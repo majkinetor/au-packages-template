6d805d55fd18fc6db386f445481cf2dff0a072db
David Gardiner <david@gardiner.net.au>
2017-05-21 21:57:51 +0930

    AU for ozcode-vs2017
    - Ignore .vsix files

--- ozcode-vs2017/tools/chocolateyinstall.ps1
+++ ozcode-vs2017/tools/chocolateyinstall.ps1
@@ -2,19 +2,27 @@
 
 $packageName  = 'ozcode-vs2017'
 $toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$fileName     = "OzCode_3.0.0.3597_FOR_VS2017.vsix"
+$fileName     = 'OzCode_3.0.0.3597_FOR_VS2017.vsix'
+$checksum     = '3a493231fa4040ad90f5384ad8f86af19a39f5f8b4ddbfb2332a970cd97669ff'
 $fileFullPath = "$toolsDir\$fileName"
 
+# Flag whether we're being invoked by AU module
+[bool] $runningAU = (Test-Path Function:\au_GetLatest)
+
+if (-not $runningAU) {
 . $toolsDir\common.ps1
+}
 
 $packageArgs = @{
   packageName   = $packageName
-  fileFullPath = $fileFullPath
+  fileFullPath  = $fileFullPath
   url           = "http://downloads.oz-code.com/files/$fileName"  #download URL, HTTPS preferrred
-  checksum      = '3A493231FA4040AD90F5384AD8F86AF19A39F5F8B4DDBFB2332A970CD97669FF'
+  checksum      = $checksum
   checksumType  = 'sha256'
 }
 
 Get-ChocolateyWebFile @packageArgs
 
-& $vsixInstaller $fileFullPath /instanceIds:$vsInstances /logfile:`"$env:TEMP\$($packageName)\$($packageName).VsixInstall.log`" /quiet
+if (-not $runningAU) {
+    & $vsixInstaller $fileFullPath /instanceIds:$vsInstances /logfile:`"$env:TEMP\$($packageName)\$($packageName).VsixInstall.log`" /quiet
+}
