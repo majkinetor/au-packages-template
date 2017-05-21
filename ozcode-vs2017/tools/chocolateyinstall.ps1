ce8d74ad69910754e23d4a8dea5f93fa719a55c1
David Gardiner <david@gardiner.net.au>
2017-05-21 21:31:48 +0930

    Working ozcode-vs2017

--- ozcode-vs2017/tools/chocolateyinstall.ps1
+++ ozcode-vs2017/tools/chocolateyinstall.ps1
@@ -5,19 +5,7 @@ $toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $fileName     = "OzCode_3.0.0.3597_FOR_VS2017.vsix"
 $fileFullPath = "$toolsDir\$fileName"
 
-# Find latest VS 2017 instance
-# We can't use Install-ChocolateyVsixPackage for 2017 yet.
-$xml = [xml] (vswhere -version "[15.0,16.0)" -latest -requires Microsoft.Net.ComponentGroup.DevelopmentPrerequisites -format xml)
-
-if (-not $xml.instances.HasChildNodes) {
-    Write-Error "No suitable instances of Visual Studio 2017 were found to install OzCode"
-}
-
-$vsixInstaller = (Join-Path $xml.instances.instance.installationPath "Common7\IDE\VSIXInstaller.exe")
-
-if (-not (Test-Path $vsixInstaller)) {
-    Write-Error "Could not find VSIXInstaller.exe"
-}
+. $toolsDir\common.ps1
 
 $packageArgs = @{
   packageName   = $packageName
@@ -29,4 +17,4 @@ $packageArgs = @{
 
 Get-ChocolateyWebFile @packageArgs
 
-& $vsixInstaller $fileFullPath /logfile:`"$env:TEMP\$($packageName)\$($packageName).VsixInstall.log`"
+& $vsixInstaller $fileFullPath /instanceIds:$vsInstances /logfile:`"$env:TEMP\$($packageName)\$($packageName).VsixInstall.log`" /quiet
