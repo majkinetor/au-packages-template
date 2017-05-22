8766ebc951b7eb753d006f45ac03674b49f57ad9
Miodrag Milic <miodrag.milic@gmail.com>
2017-05-23 01:17:27 +0200

    added all options

--- update_all.ps1
+++ update_all.ps1
@@ -5,11 +5,23 @@ param([string[]] $Name, [string] $ForcedPackages, [string] $Root = $PSScriptRoot
 if (Test-Path $PSScriptRoot/update_vars.ps1) { . $PSScriptRoot/update_vars.ps1 }
 
 $Options = [ordered]@{
+    WhatIf        = $au_WhatIf                              #WhatIf all packages
+    Force         = $false                                  #Force all packages
     Timeout       = 100                                     #Connection timeout in seconds
     UpdateTimeout = 1200                                    #Update timeout in seconds
     Threads       = 10                                      #Number of background jobs to use
     Push          = $Env:au_Push -eq 'true'                 #Push to chocolatey
     PluginPath    = ''                                      #Path to user plugins
+    
+    IgnoreOn = @(                                           #Error message parts to set the package ignore status
+    )                                  
+    RepeatOn = @(                                           #Error message parts on which to repeat package updater
+        'Unable to create secure channel'
+        'Could not establish trust relationship'
+        'Unable to connect'
+    )
+    RepeatSleep   = 0                                       #How much to sleep between repeats in seconds, by default 0
+    RepeatCount   = 1                                       #How many times to repeat on errors, by default 1
 
     Report = @{
         Type = 'markdown'                                   #Report type: markdown or text
@@ -17,7 +29,7 @@ $Options = [ordered]@{
         Params= @{                                          #Report parameters:
             Github_UserRepo = $Env:github_user_repo         #  Markdown: shows user info in upper right corner
             NoAppVeyor  = $false                            #  Markdown: do not show AppVeyor build shield
-            UserMessage = "[History](#update-history)"       #  Markdown, Text: Custom user message to show
+            UserMessage = "[History](#update-history)"      #  Markdown, Text: Custom user message to show
             NoIcons     = $false                            #  Markdown: don't show icon
             IconSize    = 32                                #  Markdown: icon size
             Title       = ''                                #  Markdown, Text: TItle of the report, by default 'Update-AUPackages'
