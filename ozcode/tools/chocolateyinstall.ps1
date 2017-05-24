712892bb05ff89d8ffa62cfdcab7b8a272b4bedc
David Gardiner <david@gardiner.net.au>
2017-05-24 17:34:21 +0930

    Add update.ps1 for ozcode
    - Add supported versions
    - Move checksum to variable

--- ozcode/tools/chocolateyinstall.ps1
+++ ozcode/tools/chocolateyinstall.ps1
@@ -2,9 +2,13 @@
 
 $packageName  = 'ozcode'
 $toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$download_dir = "$Env:TEMP\$packageName\$Env:ChocolateyPackageVersion"
+$download_dir = "$Env:TEMP\chocolatey\$packageName\$Env:ChocolateyPackageVersion".Replace("chocolatey\chocolatey", "chocolatey")
 $fileLocation = ''
 $filename = 'OzCode_2.0.0.1029.exe'
+$checksum = '10D0A0FBCE9D76A16ECF4BF49353DFC0BD65888E450FE143DA098E12F66A86A6'
+
+# Flag whether we're being invoked by AU module
+[bool] $runningAU = (Test-Path Function:\au_GetLatest)
 
 if (-not (Test-Path $download_dir)) {
     New-Item -ItemType Directory $download_dir #| Out-Null
@@ -18,12 +22,14 @@ $packageArgs = @{
   silentArgs    = "" # NSIS  
   validExitCodes= @(0)
   url           = "http://downloads.oz-code.com/files/$filename"
-  checksum      = '10D0A0FBCE9D76A16ECF4BF49353DFC0BD65888E450FE143DA098E12F66A86A6'
+  checksum      = $checksum
   checksumType  = 'sha256'
 }
 
 Get-ChocolateyWebFile @packageArgs
 
-Start-Process 'AutoHotkey' "$toolsDir\install.ahk"
+if (-not $runningAU) {
+    Start-Process 'AutoHotkey' "$toolsDir\install.ahk"
 
-Install-ChocolateyInstallPackage @packageArgs 
+    Install-ChocolateyInstallPackage @packageArgs 
+}
--- ozcode/tools/install.ahk
+++ ozcode/tools/install.ahk
@@ -1,8 +1,12 @@
 ; License agreement
+SetKeyDelay, 10, 10
+
 WinWait, OzCode Setup, License Agreement
 IfNotEqual, ErrorLevel, 0, exit 1
-ControlSend,, !a, OzCode Setup ; accept
-WinWait, OzCode Setup, Visual Studio integration, 20
+
+ControlSend,, !a, OzCode Setup
+
+WinWait, OzCode Setup, integration
 IfNotEqual, ErrorLevel, 0, exit 1
 
 ControlGet, NextEnabled, Enabled, ,Button2, OzCode Setup ; Check if Next button enabled
@@ -10,12 +14,14 @@ ControlGet, NextEnabled, Enabled, ,Button2, OzCode Setup ; Check if Next button 
 If NextEnabled = 0
 {
 	ControlSend,, {Esc}, OzCode Setup
+	
 	WinWait, OzCode Setup, Are you sure, 20
+	
 	ControlSend,, !y, OzCode Setup, Are you sure
 	exit 2
 }
 
-ControlSend,, !n, OzCode Setup ; next
+ControlSend,, !n, OzCode Setup
 
 WinWait, OzCode Setup, Prerequirements, 20
 IfNotEqual, ErrorLevel, 0, exit 1
@@ -24,6 +30,6 @@ ControlSend,, !n, OzCode Setup
 WinWait, OzCode Setup, successfully,20
 IfNotEqual, ErrorLevel, 0, exit 1
 
-ControlSend,, !f, OzCode Setup ; finish
+ControlSend,, !f, OzCode Setup
 
 WinWaitClose ; wait for dialog to close
