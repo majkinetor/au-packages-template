c89cdf9be794735921181a5c915da2c5e373c9ab
flcdrg <david@gardiner.net.au>
2017-06-28 00:05:32 +0000

    AU: 1 updated - typescript-vs2015 [skip ci] https://gist.github.com/7d00c69c2cde8309a8594abaa897a9a6/a9a6078dc61dea50585e84eb443f8b00bb979bdd

--- typescript-vs2015/tools/chocolateyinstall.ps1
+++ typescript-vs2015/tools/chocolateyinstall.ps1
@@ -2,9 +2,9 @@
 
 $packageName= 'typescript-vs2015'
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://download.microsoft.com/download/6/D/8/6D8381B0-03C1-4BD2-AE65-30FF0A4C62DA/2.4.0-TS-release-dev14update3-20170608.3/TypeScript_Dev14Full.exe'
-$checksum   = '2e225f1d5bd6f8158f7294b4fcf9676d5d67610406d6c051dbfc011589cd4bf5'
-$installVersion = '2.4.0.0 RC'
+$url        = 'https://download.microsoft.com/download/6/D/8/6D8381B0-03C1-4BD2-AE65-30FF0A4C62DA/2.4.1-TS-release-dev14update3-20170626.1/TypeScript_Dev14Full.exe'
+$checksum   = 'cbd4aa345c8f7884721176f0d151905479ae10566d1fa2bb0747accd8fcd1985'
+$installVersion = '2.4.1.0'
 $softwareName = "TypeScript Tools for Microsoft Visual Studio 2015 $installVersion"
 
 $packageArgs = @{
