a285270b0d556fa27c30dd6e5b854df41bdd8ca9
David Gardiner <david@gardiner.net.au>
2017-07-04 22:59:54 +0930

    Redgate (#10)
    Initial commit for Red Gate SQL Toolbelt
    * Add FTP fallback
    * Improve logging

---
+++ redgate-sqltoolbelt/tools/chocolateyinstall.ps1
@@ -0,0 +1,48 @@
+﻿$ErrorActionPreference = 'Stop';
+$toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+$primaryDownloadUrl = "https://download.red-gate.com/SQLToolbelt.exe"
+$secondaryDownloadUrl = "ftp://support.red-gate.com/patches/SQLToolbelt/03Jul2017/SQLToolbelt.exe"
+$packageVersionLastModified = New-Object -TypeName DateTimeOffset 2017, 7, 3, 11, 5, 0, 0 # Last modified time corresponding to this package version
+$checksum = 'A7DA6A6C96AE83C5EA392903A146D5840EFD6136DE311E73AFED0E2F6C81BACC'
+
+$pp = Get-PackageParameters
+
+if ($pp["FTP"] -ne $null -and $pp["FTP"] -ne '') { 
+
+  # FTP forced  
+    $url = $secondaryDownloadUrl
+} else {
+
+  # Red Gate have a fixed download URL, but if the binary changes we can fall back to their FTP site
+  # so the package doesn't break
+  $headers = Get-WebHeaders -url $primaryDownloadUrl
+  $lastModifiedHeader = $headers.'Last-Modified'
+
+  $lastModified = [DateTimeOffset]::Parse($lastModifiedHeader, [Globalization.CultureInfo]::InvariantCulture)
+
+  Write-Verbose "Package LastModified: $packageVersionLastModified"
+  Write-Verbose "HTTP Last Modified  : $lastModified"
+
+  if ($lastModified -ne $packageVersionLastModified) {
+    Write-Warning "The download available at $primaryDownloadUrl has changed from what this package was expecting. Falling back to FTP for version-specific URL"
+    $url = $secondaryDownloadUrl
+  } else {
+    Write-Verbose "Primary URL matches package expectation"
+    $url = $primaryDownloadUrl
+  }
+}
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  fileType      = 'exe'
+  silentArgs    = "/IAgreeToTheEula"
+  
+  validExitCodes= @(0)
+  url           = $url
+  checksum      = $checksum
+  checksumType  = 'sha256'
+  destination   = $toolsDir
+}
+
+Install-ChocolateyPackage @packageArgs
