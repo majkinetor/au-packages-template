20786770b6457ac8a5a69b864d90da5080106a12
David Gardiner <david@gardiner.net.au>
2017-07-08 11:34:33 +0930

    Use HTML Agility Pack to work around "Windows Security Warning" when using Invoke-WebRequest

--- typescript-vs2015/update.ps1
+++ typescript-vs2015/update.ps1
@@ -1,5 +1,7 @@
 import-module au
 
+Add-type -Path ..\packages\HtmlAgilityPack\lib\Net45\HtmlAgilityPack.dll
+
 $releases = 'https://www.microsoft.com/en-us/download/details.aspx?id=48593'
 
 function global:au_SearchReplace {
@@ -18,51 +20,62 @@ function global:au_SearchReplace {
 
 function global:au_GetLatest {
 
-# http://download.microsoft.com/download/6/D/8/6D8381B0-03C1-4BD2-AE65-30FF0A4C62DA/2.3.3-TS-release-dev14update3-20170519.1/TypeScript_Dev14Full.exe
-    $download_page = Invoke-WebRequest -Uri $releases #1 
+    try {        
+        # http://download.microsoft.com/download/6/D/8/6D8381B0-03C1-4BD2-AE65-30FF0A4C62DA/2.3.3-TS-release-dev14update3-20170519.1/TypeScript_Dev14Full.exe
 
-    $links = $download_page.links
-    $url32     = $links | ? href -match '.exe$' | select -First 1 -expand href
+        $htmlWeb = New-Object HtmlAgilityPack.HtmlWeb
+        $htmlWeb.AutoDetectEncoding = $true
+        $doc = $htmlWeb.Load($releases)
 
-    $releaseNotes = $links | Where-Object { $_.href.StartsWith("https://github.com/Microsoft/TypeScript/releases/tag/") } | Select-Object -First 1 -ExpandProperty Href
+        $links = $doc.DocumentNode.SelectNodes("//a[substring(@href, string-length(@href) - string-length('.exe') +1) = '.exe']")
+        $link = $links[0]
+        $url32 = $link.Attributes["href"].Value
 
-    # version in filename isn't always complete, so we search for text description before link
-    $all = $download_page.ParsedHtml.body.all
-    $a = $all | Where { $_.tagName -eq 'a' -and $_.href -eq $url32 }
-    $li = $a.parentElement
+        $li = $link.ParentNode
 
-    # Use HTTPS
-    $builder = [UriBuilder] $url32
-    $builder.Scheme = "https"
-    $builder.Port = 443
-    $url32 = $builder.Uri.ToString()
-    
-    # TypeScript 2.3.0 RC for Visual Studio 2015
-    if ($li.innerText -match "^TypeScript (?<version>(?<digits>\d+\.\d+\.\d+)( \w*)?) for Visual Studio 2015.*$") {
+        # first a should be release notes
+        $firstA = $li.SelectSingleNode("a")
+        $releaseNotes = $firstA.Attributes["href"].Value
 
-        $digits = $Matches.digits # 2.3.0
-        $installVersion = $Matches.version.Replace($digits, $digits + ".0") #2.3.0.0 RC
+        # version in filename isn't always complete, so we search for text description before link
 
-        # semver 2.3.0-RC
-        $version = $installVersion.Replace(" ", "-")
+        # Use HTTPS
+        $builder = [UriBuilder] $url32
+        $builder.Scheme = "https"
+        $builder.Port = 443
+        $url32 = $builder.Uri.ToString()
+        
+        # TypeScript 2.3.0 RC for Visual Studio 2015
+        if ($li.InnerText -match "^TypeScript (?<version>(?<digits>\d+\.\d+\.\d+)( \w*)?) for Visual Studio 2015.*$") {
 
-        $Latest = @{ 
-            URL32 = $url32; 
-            Version = $version;
-            ReleaseNotes = $releaseNotes
-            InstallVersion = $installVersion
+            $digits = $Matches.digits # 2.3.0
+            $installVersion = $Matches.version.Replace($digits, $digits + ".0") #2.3.0.0 RC
+
+            # semver 2.3.0-RC
+            $version = $installVersion.Replace(" ", "-")
+
+            Write-Host $version
+
+            $Latest = @{ 
+                URL32 = $url32; 
+                Version = $version;
+                ReleaseNotes = $releaseNotes
+                InstallVersion = $installVersion
+            }
+        } else {
+            $Latest = @{}
         }
-    } else {
-        $Latest = @{}
+    } catch {
+        Write-Error $_
+        $Latest = @{}        
     }
-    
     return $Latest
 }
 
 function global:au_AfterUpdate
 { 
     $nuspecFileName = $Latest.PackageName + ".nuspec"
-    $nu = gc $nuspecFileName -Raw -Encoding UTF8
+    $nu = Get-Content $nuspecFileName -Raw -Encoding UTF8
     $nu = $nu -replace "(?smi)(\<releaseNotes\>).*?(\</releaseNotes\>)", "`${1}$($Latest.ReleaseNotes)`$2"
 
     $Utf8NoBomEncoding = New-Object System.Text.UTF8Encoding($False)
