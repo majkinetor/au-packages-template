cf88804d11c0a1bd4000101e45d6e2cbca9be286
flcdrg <david@gardiner.net.au>
2017-12-22 17:26:00 -0800

    AU: 1 updated - typescript-vs2015 [skip ci] https://gist.github.com/7d00c69c2cde8309a8594abaa897a9a6/6c259a76e57372b4cb8eb4f12e4020656af4464b

--- typescript-vs2015/tools/chocolateyinstall.ps1
+++ typescript-vs2015/tools/chocolateyinstall.ps1
@@ -2,9 +2,9 @@
 
 $packageName= 'typescript-vs2015'
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://download.microsoft.com/download/6/D/8/6D8381B0-03C1-4BD2-AE65-30FF0A4C62DA/2.6.1-TS-release-dev14update3-20171027.2/TypeScript_Dev14Full.exe'
-$checksum   = '087583a0a4e270487348305da51af2bc16692877db25acc53fc1fc08ea1816dc'
-$installVersion = '2.6.1.0'
+$url        = 'https://download.microsoft.com/download/6/D/8/6D8381B0-03C1-4BD2-AE65-30FF0A4C62DA/2.6.2-TS-release-dev14update3-20171121.4/TypeScript_Dev14Full.exe'
+$checksum   = 'f656f55f0d91d316afb973718d9c7db4dfbb0df2d1a8589a4f623eef76e7c930'
+$installVersion = '2.6.2.0'
 $softwareName = "TypeScript Tools for Microsoft Visual Studio 2015 $installVersion"
 
 $packageArgs = @{
--- typescript-vs2015/tools/chocolateyuninstall.ps1
+++ typescript-vs2015/tools/chocolateyuninstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop';
 
 $packageName = 'typescript-vs2015'
-$installVersion = '2.6.1.0'
+$installVersion = '2.6.2.0'
 $softwareName = "TypeScript Tools for Microsoft Visual Studio 2015 $installVersion"
  
 $installerType = 'EXE' 
