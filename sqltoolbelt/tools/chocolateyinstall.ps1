f51201eda8d7d95f76693488614f7149e2dfa630
flcdrg <david@gardiner.net.au>
2018-01-12 16:08:22 -0800

    AU: 1 updated - sqltoolbelt [skip ci] https://gist.github.com/7d00c69c2cde8309a8594abaa897a9a6/d6796e9645812ea6ee554306b272b7bf19953cb5

--- sqltoolbelt/tools/chocolateyinstall.ps1
+++ sqltoolbelt/tools/chocolateyinstall.ps1
@@ -2,9 +2,9 @@
 $toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 $primaryDownloadUrl = "https://download.red-gate.com/SQLToolbelt.exe"
-$secondaryDownloadUrl = 'ftp://support.red-gate.com/patches/SQLToolbelt/04Jan2018/SQLToolbelt.exe'
-$packageVersionLastModified = New-Object -TypeName DateTimeOffset 2018, 1, 4, 13, 31, 18, 0 # Last modified time corresponding to this package version
-$checksum = '855940C0F505D9B999BA2B01E02DB4545C37DB8379B41B0CF461DDF18D8F6D3D'
+$secondaryDownloadUrl = 'ftp://support.red-gate.com/patches/SQLToolbelt/12Jan2018/SQLToolbelt.exe'
+$packageVersionLastModified = New-Object -TypeName DateTimeOffset 2018, 1, 12, 13, 29, 43, 0 # Last modified time corresponding to this package version
+$checksum = '98BA19F05F7C3E5E4EC4760965344BD1ACA8AED66C5A62A331E5597AE99DC211'
 
 $pp = Get-PackageParameters
 
