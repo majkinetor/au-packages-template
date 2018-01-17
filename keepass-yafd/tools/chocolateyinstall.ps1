21359936c98f31daa2265a39b351d1b97e8c8ee1
David Gardiner <david@gardiner.net.au>
2018-01-18 10:14:25 +1030

    Added keepass-yafd (Yet another favicon downloader plugin)

---
+++ keepass-yafd/tools/chocolateyinstall.ps1
@@ -0,0 +1,15 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$fileLocation = Join-Path $toolsDir 'YetAnotherFaviconDownloader.plgx'
+
+if (Test-Path env:"ProgramFiles(x86)")
+{
+  $fileFullPath = ${env:ProgramFiles(x86)}
+} else {
+  $fileFullPath = $env:ProgramFiles
+}
+
+$fileFullPath = [IO.Path]::Combine($fileFullPath, "KeePass Password Safe 2\Plugins\YetAnotherFaviconDownloader.plgx")
+
+Copy-Item -Path $fileLocation -Destination $fileFullPath
---
+++ keepass-yafd/tools/chocolateyuninstall.ps1
@@ -0,0 +1,14 @@
+﻿$ErrorActionPreference = 'Stop';
+
+if (Test-Path env:"ProgramFiles(x86)")
+{
+  $fileFullPath = ${env:ProgramFiles(x86)}
+} else {
+  $fileFullPath = $env:ProgramFiles
+}
+
+$fileFullPath = [IO.Path]::Combine($fileFullPath, "KeePass Password Safe 2\Plugins\YetAnotherFaviconDownloader.plgx")
+
+if (Test-Path $fileFullPath) {
+  Remove-Item $fileFullPath -Force
+}
