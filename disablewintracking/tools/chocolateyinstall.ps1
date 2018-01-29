d0cfeab97f6fd4ca5da5882e80847ef02c370928
Chocolatey <chocolatey@realdimensions.net>
2018-01-29 13:01:37 -0800

    AU: 2 updated - disablewintracking foldersizes [skip ci] https://gist.github.com/03903cd565061a825f153ce2f9d6227c/3e9f109e17bb9b61922eb63de8ca73a9324dd6fb

--- disablewintracking/tools/chocolateyinstall.ps1
+++ disablewintracking/tools/chocolateyinstall.ps1
@@ -1,8 +1,8 @@
 ﻿$packageArgs = @{
   packageName            = "$env:chocolateyPackageName"
-  url                    = 'https://github.com/10se1ucgo/DisableWinTracking/releases/download/v3.0.1/dwt-3.0.1-cp27-win_x86.zip'
+  url                    = 'https://github.com/10se1ucgo/DisableWinTracking/releases/download/v3.2.1/dwt-3.2.1-cp27-win_x86.zip'
   UnzipLocation          = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
-  checksum               = 'd4c00704e0888c2b630b9fc61ca93ca222ad30767c6f998f219e96075a12ebfe'
+  checksum               = 'd4c31d005c1a676e9867f4a2a553299e55aa0857626d25a1d7dd84b00e505757'
   checksumType           = 'sha256'
 }
 Install-ChocolateyZipPackage @packageArgs
