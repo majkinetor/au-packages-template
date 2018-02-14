ccfd8e845c091702ab8c48939be25efe8481641e
Alexander Georgievskiy <galeksandrp@gmail.com>
2018-02-15 02:05:32 +0400

    Fix a little

--- sprut-sms-server/update.ps1
+++ sprut-sms-server/update.ps1
@@ -17,7 +17,7 @@ function global:au_GetLatest {
 	
 	@{
         URL32   = $URL32
-        Version = (Get-Item "$env:TMP\sms_server_setup.exe").VersionInfo.ProductVersion
+        Version = (Get-Item "$env:TMP\sms_server_setup.exe").VersionInfo.ProductVersion.trim()
     }
 }
 
