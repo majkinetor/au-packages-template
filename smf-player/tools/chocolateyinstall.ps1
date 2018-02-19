d0604945f4a581efda72bd383306cac01cfd5362
Alexander Georgievskiy <galeksandrp@gmail.com>
2018-02-19 13:53:38 +0300

    [ci skip] Upd

---
+++ smf-player/tools/chocolateyinstall.ps1
@@ -0,0 +1,8 @@
+﻿$packageArgs = @{
+  packageName            = "$env:chocolateyPackageName"
+  FileFullPath           = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)\UpdSMFJ.exe"
+  url                    = 'https://static.roland.com/jp/media/exe/UpdSMFJ.exe'
+  checksum               = 'f5d696315286afd4b75b3d384ef671256e254789f22612fae409c1625921481a'
+  checksumType           = 'sha256'
+}
+Get-ChocolateyWebFile @packageArgs
