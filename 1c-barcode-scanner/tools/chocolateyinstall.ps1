86568de95fef8e6d89e06ad6caa618aea647a770
Alexander Georgievskiy <galeksandrp@gmail.com>
2018-02-21 17:00:47 +0300

    Upd

---
+++ 1c-barcode-scanner/tools/chocolateyinstall.ps1
@@ -0,0 +1,9 @@
+﻿$packageArgs = @{
+  packageName            = "$env:chocolateyPackageName"
+  FileType               = 'exe'
+  SilentArgs             = '/s /sp"/s /v"/qn /norestart""'
+  url                    = 'https://its.1c.ru/db/files/1CITS/EXE/TradeWare/1C/ScanOPOS/ScanOPOS.exe'
+  checksum               = '11f98ee88669923a8d19e58e955b8b7268abb6b2977ff687a714462e68aeba34'
+  checksumType           = 'sha256'
+}
+Install-ChocolateyPackage @packageArgs
---
+++ 1c-barcode-scanner/update.ps1
@@ -0,0 +1,24 @@
+import-module au
+import-module $env:ChocolateyInstall\helpers\chocolateyInstaller.psm1
+
+function global:au_SearchReplace {
+   @{
+        ".\tools\chocolateyInstall.ps1" = @{
+            "(?i)(^\s*url\s*=\s*)('.*')"        = "`$1'$($Latest.URL32)'"
+            "(?i)(^\s*checksum\s*=\s*)('.*')"   = "`$1'$($Latest.Checksum32)'"
+        }
+    }
+}
+
+function global:au_GetLatest {
+	$URL32 = 'https://its.1c.ru/db/files/1CITS/EXE/TradeWare/1C/ScanOPOS/ScanOPOS.exe'
+    Get-ChocolateyWebFile '1c-barcode-scanner' "$env:TMP\1c-barcode-scanner.exe" $URL32
+    Get-ChocolateyUnzip "$env:TMP\1c-barcode-scanner.exe" "$env:TMP" -SpecificFolder "ScanOPOS.exe"
+	
+	@{
+        URL32   = $URL32
+        Version = (Get-Item "$env:TMP\ScanOPOS.exe").VersionInfo.ProductVersion.trim()
+    }
+}
+
+update
