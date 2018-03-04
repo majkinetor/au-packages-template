c1a96d140f4816dc91cf12014eeaf2289f3531fe
Alexander Georgievskiy <galeksandrp@gmail.com>
2018-03-04 16:17:40 +0400

    [ci skip] Upd

--- 1c-barcode-scanner/tools/chocolateyinstall.ps1
+++ 1c-barcode-scanner/tools/chocolateyinstall.ps1
@@ -3,7 +3,7 @@
   FileType               = 'exe'
   SilentArgs             = '/s /sp"/s /v"/qn /norestart""'
   url                    = 'https://its.1c.ru/db/files/1CITS/EXE/TradeWare/1C/ScanOPOS/ScanOPOS.exe'
-  checksum               = '11f98ee88669923a8d19e58e955b8b7268abb6b2977ff687a714462e68aeba34'
+  checksum               = '80170acc844e4ceb55c839881988b54f36fac4ff5a319b1c02fcee5a870b40ef'
   checksumType           = 'sha256'
 }
 Install-ChocolateyPackage @packageArgs
