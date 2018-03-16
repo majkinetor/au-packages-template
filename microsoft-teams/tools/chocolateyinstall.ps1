c6eedc909ab2fd685c15a4426434ac69dcbe0edb
flcdrg <david@gardiner.net.au>
2018-03-15 17:08:38 -0700

    AU: 2 updated - microsoft-teams typescript-vs2015 [skip ci] https://gist.github.com/7d00c69c2cde8309a8594abaa897a9a6/7223781ff5a15b33324f0f05c8389a9f86cbc8e9

--- microsoft-teams/tools/chocolateyinstall.ps1
+++ microsoft-teams/tools/chocolateyinstall.ps1
@@ -2,10 +2,10 @@
 
 $packageName= 'microsoft-teams'
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url32      = 'https://statics.teams.microsoft.com/production-windows/1.1.00.5855/Teams_windows.exe'
-$url64      = 'https://statics.teams.microsoft.com/production-windows-x64/1.1.00.5855/Teams_windows_x64.exe'
-$checksum32 = '34f02df763fae231cce3a9256aba5a1eb652cbf15e83452cc5be697de0933513'
-$checksum64 = '5ee30fbb006ea9eec10423d3d22b623b02db34caecbf97aefacd8f8cb1f3a89f'
+$url32      = 'https://statics.teams.microsoft.com/production-windows/1.1.00.5963/Teams_windows.exe'
+$url64      = 'https://statics.teams.microsoft.com/production-windows-x64/1.1.00.5963/Teams_windows_x64.exe'
+$checksum32 = '7eb40211516f7d7e2f4914df818fd4aaf3de28a04a66b811561a7b092051b6a7'
+$checksum64 = '9fbaa18e39b5437be49b02e96f12269f82417a2b31b297e821c132cccb5c261b'
 
 $packageArgs = @{
   packageName   = $packageName
--- typescript-vs2015/tools/chocolateyinstall.ps1
+++ typescript-vs2015/tools/chocolateyinstall.ps1
@@ -2,9 +2,9 @@
 
 $packageName= 'typescript-vs2015'
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://download.microsoft.com/download/6/D/8/6D8381B0-03C1-4BD2-AE65-30FF0A4C62DA/2.7.2-TS-release-dev14update3-20180212.1/TypeScript_Dev14Full.exe'
-$checksum   = '8ffe8c84cc4b4ae3bf98cd71b66c59586b56b7cfd8acfa3cd519e275cbaf5241'
-$installVersion = '2.7.2.0'
+$url        = 'https://download.microsoft.com/download/6/D/8/6D8381B0-03C1-4BD2-AE65-30FF0A4C62DA/2.8.0-TS-release-dev14update3-20180313.3/TypeScript_Dev14Full.exe'
+$checksum   = '017b92a7aa5dc6de562f089b2b8cb8ad6241b115344f0cc726b4c2b418d878fb'
+$installVersion = '2.8.0.0 RC'
 $softwareName = "TypeScript Tools for Microsoft Visual Studio 2015 $installVersion"
 
 $packageArgs = @{
