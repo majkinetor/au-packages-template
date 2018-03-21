f055f11e3ad55e4416837f300f4a43ce431a95c5
Alexander Georgievskiy <galeksandrp@gmail.com>
2018-03-22 02:14:53 +0400

    [ci skip] Upd

--- dws.portable/tools/chocolateyinstall.ps1
+++ dws.portable/tools/chocolateyinstall.ps1
@@ -1,8 +1,8 @@
 ﻿$packageArgs = @{
   packageName            = "$env:chocolateyPackageName"
-  FileFullPath                = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)\DWS.exe"
-  url                    = 'https://github.com/Nummer/Destroy-Windows-10-Spying/releases/download/1.7.1.0/DWS.exe'
-  checksum               = '2bfc72791f0e222a790e88895a143aac02c1c67ffc3f663cfb5c2461683d96b4'
+  FileFullPath           = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)\DWS.exe"
+  url                    = 'https://github.com/Nummer/Destroy-Windows-10-Spying/releases/download/2.2.2.2/DWS.exe'
+  checksum               = '2f50ea4dd2b4a90622d1ba9d9b68dd40f1941f3a913b5d449100055a21552bf4'
   checksumType           = 'sha256'
 }
 Get-ChocolateyWebFile @packageArgs
