d9a5aa02d19bc6acce44b9b4cb349cf9a2f60a6f
Alexander Georgievskiy <galeksandrp@gmail.com>
2018-03-22 16:18:26 +0300

    Upd

---
+++ cspclean/tools/chocolateyinstall.ps1
@@ -0,0 +1,8 @@
+﻿$packageArgs = @{
+  packageName            = "$env:chocolateyPackageName"
+  FileFullPath           = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)\cspclean.exe"
+  url                    = 'https://www.cryptopro.ru/sites/default/files/public/cspclean.exe'
+  checksum               = '5185d970e2e4f4747ec691d7db35ef75683f4612f74cddc070afe4b84e9f372d'
+  checksumType           = 'sha256'
+}
+Get-ChocolateyWebFile @packageArgs
---
+++ cspclean/update.ps1
@@ -0,0 +1,23 @@
+import-module au
+import-module $env:ChocolateyInstall\helpers\chocolateyInstaller.psm1
+
+function global:au_SearchReplace {
+   @{
+        ".\tools\chocolateyInstall.ps1" = @{
+            "(?i)(^\s*url\s*=\s*)('.*')"        = "`$1'$($Latest.URL32)'"
+            "(?i)(^\s*checksum\s*=\s*)('.*')"   = "`$1'$($Latest.Checksum32)'"
+        }
+    }
+}
+
+function global:au_GetLatest {
+	$URL32 = 'https://www.cryptopro.ru/sites/default/files/public/cspclean.exe'
+    Get-ChocolateyWebFile 'cspclean' $URL32 -FileFullPath "$env:TMP\cspclean.exe"
+	
+	@{
+        URL32   = $URL32
+        Version = (Get-Item "$env:TMP\cspclean.exe").VersionInfo.ProductVersion.trim()
+    }
+}
+
+update
