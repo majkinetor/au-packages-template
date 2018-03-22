cb8473d77db4fb98f103671442e79d38dce39143
Alexander Georgievskiy <galeksandrp@gmail.com>
2018-03-22 17:27:11 +0300

    Upd

---
+++ fuse-nfs/tools/chocolateyinstall.ps1
@@ -0,0 +1,11 @@
+﻿$packageArgs = @{
+  packageName            = "$env:chocolateyPackageName"
+  UnzipLocation          = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
+  url                    = 'https://github.com/Daniel-Abrecht/fuse-nfs-crossbuild-scripts/releases/download/libnfs%3Dlibnfs-2.0.0_fuse-nfs%3Dbdd4e2bfede5ced1e1d1ab21c7652e9905b7f668_dokany%3Dv1.1.0/i686-w64-mingw32.zip'
+  url64                  = 'https://github.com/Daniel-Abrecht/fuse-nfs-crossbuild-scripts/releases/download/libnfs%3Dlibnfs-2.0.0_fuse-nfs%3Dbdd4e2bfede5ced1e1d1ab21c7652e9905b7f668_dokany%3Dv1.1.0/x86_64-w64-mingw32.zip'
+  checksum               = 'ac68c8845a869fabf90dc16e3bc309130fffd337493fed25eb41d1f45f7528dd'
+  checksum64             = 'bb1e7a1b97f37ff0abe841c8e7a6470a5ddd456350e1e018d3edfc1f5b899c15'
+  checksumType           = 'sha256'
+  checksumType64         = 'sha256'
+}
+Install-ChocolateyZipPackage @packageArgs
---
+++ fuse-nfs/update.ps1
@@ -0,0 +1,24 @@
+import-module au
+
+function global:au_SearchReplace {
+   @{
+        ".\tools\chocolateyInstall.ps1" = @{
+            "(?i)(^\s*url\s*=\s*)('.*')"        = "`$1'$($Latest.URL)'"
+            "(?i)(^\s*url64bit\s*=\s*)('.*')"   = "`$1'$($Latest.URL64)'"
+            "(?i)(^\s*checksum\s*=\s*)('.*')"   = "`$1'$($Latest.Checksum)'"
+            "(?i)(^\s*checksum64\s*=\s*)('.*')" = "`$1'$($Latest.Checksum64)'"
+        }
+    }
+}
+
+function global:au_GetLatest {
+    $release = curl https://api.github.com/repos/Daniel-Abrecht/fuse-nfs-crossbuild-scripts/releases/latest | ConvertFrom-Json
+	
+	@{
+		URL32   = ($release.assets -match 'i686-w64-mingw32.zip').browser_download_url
+        URL64   = ($release.assets -match 'x86_64-w64-mingw32.zip').browser_download_url
+        Version = ((($release.tag_name -split '_')[0] -split '=')[1] -split '-')[1]
+    }
+}
+
+update
