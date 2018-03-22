213088f7b98c862739dc71050c1f01919d1a3aec
Alexander Georgievskiy <galeksandrp@gmail.com>
2018-03-22 18:35:23 +0300

    Upd

---
+++ shaman-dokan-warc/tools/chocolateyinstall.ps1
@@ -0,0 +1,8 @@
+﻿$packageArgs = @{
+  packageName            = "$env:chocolateyPackageName"
+  url                    = 'https://github.com/antiufo/Shaman.Dokan.Warc/releases/download/1.0.1/Shaman.Dokan.Warc.zip'
+  UnzipLocation          = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
+  checksum               = 'bf891b790a6a4600946d400fa51ab84db983c71284ae08e292a2139b1dee3192'
+  checksumType           = 'sha256'
+}
+Install-ChocolateyZipPackage @packageArgs
---
+++ shaman-dokan-warc/update.ps1
@@ -0,0 +1,21 @@
+import-module au
+
+function global:au_SearchReplace {
+   @{
+        ".\tools\chocolateyInstall.ps1" = @{
+            "(?i)(^\s*url\s*=\s*)('.*')"        = "`$1'$($Latest.URL)'"
+            "(?i)(^\s*checksum\s*=\s*)('.*')"   = "`$1'$($Latest.Checksum)'"
+        }
+    }
+}
+
+function global:au_GetLatest {
+    $release = curl "https://api.github.com/repos/antiufo/Shaman.Dokan.Warc/releases/latest?client_id=$env:GITHUB_CLIENT_ID&client_secret=$env:GITHUB_CLIENT_SECRET" | ConvertFrom-Json
+	
+	@{
+        URL32   = $release.assets.browser_download_url
+        Version = $release.tag_name
+    }
+}
+
+update
