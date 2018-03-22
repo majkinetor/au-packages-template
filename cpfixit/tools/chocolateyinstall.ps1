557405b1bad11a33f53e57836feb3d8ca27a3f44
Alexander Georgievskiy <galeksandrp@gmail.com>
2018-03-22 16:14:16 +0300

    Upd

---
+++ cpfixit/tools/chocolateyinstall.ps1
@@ -0,0 +1,8 @@
+﻿$packageArgs = @{
+  packageName            = "$env:chocolateyPackageName"
+  FileFullPath           = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)\cpfixit.exe"
+  url                    = 'https://www.cryptopro.ru/sites/default/files/public/cpfixit.exe'
+  checksum               = 'b78f9f11461764e15ba5e167cea20ce67e0be4236d20e51b7459dd4192e929f5'
+  checksumType           = 'sha256'
+}
+Get-ChocolateyWebFile @packageArgs
---
+++ cpfixit/update.ps1
@@ -0,0 +1,23 @@
+import-module au
+import-module $env:ChocolateyInstall\helpers\chocolateyInstaller.psm1
+
+function global:au_SearchReplace {
+   @{
+        ".\tools\chocolateyInstall.ps1" = @{
+            "(?i)(^\s*url\s*=\s*)('.*')"        = "`$1'$($Latest.URL32)'"
+            "(?i)(^\s*checksum\s*=\s*)('.*')"   = "`$1'$($Latest.Checksum32)'"
+        }
+    }
+}
+
+function global:au_GetLatest {
+	$URL32 = 'https://www.cryptopro.ru/sites/default/files/public/cpfixit.exe'
+    Get-ChocolateyWebFile 'cpfixit' $URL32 -FileFullPath "$env:TMP\cpfixit.exe"
+	
+	@{
+        URL32   = $URL32
+        Version = (Get-Item "$env:TMP\cpfixit.exe").VersionInfo.FileVersion.trim()
+    }
+}
+
+update
