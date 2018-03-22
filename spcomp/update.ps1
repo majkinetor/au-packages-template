27269c443c2516238e7302fe98d3bf2bdff1c49b
Alexander Georgievskiy <galeksandrp@gmail.com>
2018-03-22 19:48:29 +0300

    Upd

--- spcomp/update.ps1
+++ spcomp/update.ps1
@@ -3,20 +3,26 @@ import-module au
 function global:au_SearchReplace {
    @{
         ".\tools\chocolateyInstall.ps1" = @{
-            "(?i)(^\s*url\s*=\s*)('.*')"        = "`$1'$($Latest.URL)'"
-            "(?i)(^\s*checksum\s*=\s*)('.*')"   = "`$1'$($Latest.Checksum)'"
+            "(?i)(^\s*url\s*=\s*)('.*')"        = "`$1'$($Latest.URL32)'"
+            "(?i)(^\s*checksum\s*=\s*)('.*')"   = "`$1'$($Latest.Checksum32)'"
         }
     }
 }
 
 function global:au_GetLatest {
     $release = ((curl 'https://sm.alliedmods.net/smdrop/1.8/').Links -match '-windows.zip').href | select -last 1
-	$major = ($release -split '-')[1]
-	$minor = ($release -split '-')[2] -replace '^git',''
+	$version = [version]($release -split '-')[1]
+	$revision = ($release -split '-')[2] -replace '^git',''
+	
+	$build = ''
+	
+	if ($version.build) {
+		$build = $version.build
+	}
 	
 	@{
         URL32   = "https://sm.alliedmods.net/smdrop/1.8/$release"
-        Version = "$major.$minor"
+        Version = ($version.major, $version.minor -join '.'), ($build, $revision -join '') -join '.'
     }
 }
 
