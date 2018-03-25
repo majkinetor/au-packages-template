a57eba0314a54947c57597745f5bd9be2f97247c
Chocolatey <chocolatey@realdimensions.net>
2018-03-25 13:41:05 -0700

    AU: 1 updated - rutracker-proxy [skip ci] https://gist.github.com/03903cd565061a825f153ce2f9d6227c/1ca3a3bbdf9f50b293b7403b0113904bfb24af35

--- rutracker-proxy/tools/chocolateyinstall.ps1
+++ rutracker-proxy/tools/chocolateyinstall.ps1
@@ -2,8 +2,8 @@
   packageName            = "$env:chocolateyPackageName"
   FileType               = 'exe'
   SilentArgs             = '/S'
-  url                    = 'https://github.com/RutrackerOrg/rutracker-proxy/releases/download/v0.1.3/rto-proxy-installer-WINDOWS.exe'
-  checksum               = 'f8009d7e59c51fb209b624a333893a8a1eb9c4be51ee23a257bd7681439fbd36'
+  url                    = 'https://github.com/RutrackerOrg/rutracker-proxy/releases/download/v0.2.3/rto-proxy-setup-0.2.3.exe'
+  checksum               = 'a1711839fe853a91e77b6a794f24c624541b00dd8bb2790bb20e450de84c31a5'
   checksumType           = 'sha256'
 }
 Install-ChocolateyPackage @packageArgs
