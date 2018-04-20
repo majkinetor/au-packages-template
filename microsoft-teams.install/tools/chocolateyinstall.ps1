1ca8398daf9c1e1731e153c02ad915521dd582a2
David Gardiner <david@gardiner.net.au>
2018-04-21 08:37:52 +1000

    Microsoft teams - (MSI )

---
+++ microsoft-teams.install/tools/chocolateyinstall.ps1
@@ -0,0 +1,28 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageName= 'microsoft-teams.install'
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url32      = 'https://statics.teams.microsoft.com/production-windows/1.1.00.5966/Teams_windows.msi'
+$url64      = 'https://statics.teams.microsoft.com/production-windows-x64/1.1.00.5966/Teams_windows_x64.msi'
+$checksum32 = '4421A76A76ABD0F6AE07769A64BF9DBA2CF015627FBDD7C54509EAD660AD64FE'
+$checksum64 = '3142245F681B1D36EC1539FA0DF0D20B8ED6FC4367E75478F6E48E508FF8293A'
+
+$packageArgs = @{
+  packageName   = $packageName
+  unzipLocation = $toolsDir
+  fileType      = 'MSI'
+  url           = $url32
+  url64bit      = $url64
+
+  softwareName  = 'Teams Machine-Wide Installer'
+
+  checksum      = $checksum32
+  checksumType  = 'sha256'
+  checksum64    = $checksum64
+  checksumType64= 'sha256'
+
+  silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`"" # ALLUSERS=1 DISABLEDESKTOPSHORTCUT=1 ADDDESKTOPICON=0 ADDSTARTMENU=0
+  validExitCodes= @(0, 3010, 1641)
+}
+
+Install-ChocolateyPackage @packageArgs
