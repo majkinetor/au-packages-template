9edc74e06094f479e5eb2a528c86ebbc1c3265e3
David Gardiner <david@gardiner.net.au>
2018-04-22 17:02:33 +1000

    dotMemory

---
+++ dotMemory/tools/chocolateyinstall.ps1
@@ -0,0 +1,18 @@
+﻿$ErrorActionPreference = 'Stop'; # stop on all errors
+
+$filename = "JetBrains.ReSharperUltimate.$($env:ChocolateyPackageVersion).exe"
+
+$platformPackageName = 'resharper-platform'
+
+$scriptPath = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
+$commonPath = $(Split-Path -parent $(Split-Path -parent $scriptPath))
+$installPath = Join-Path  (Join-Path $commonPath $platformPackageName) $filename
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  fileType      = 'exe'
+  file          = $installPath
+  silentArgs    = "/Silent=True /SpecificProductNames=$($env:ChocolateyPackageName) /VsVersion=*"
+  validExitCodes= @(0)
+  softwareName  = $env:ChocolateyPackageName
+}
+Install-ChocolateyInstallPackage @packageArgs
