db569d940c44d8c5c0cc79faff38d955d2302b24
Alexander Georgievskiy <galeksandrp@gmail.com>
2018-04-27 23:43:33 +0400

    [ci skippush] Init lesegais

---
+++ lesegais/tools/chocolateyinstall.ps1
@@ -0,0 +1,9 @@
+﻿$packageArgs = @{
+  packageName            = "$env:chocolateyPackageName"
+  FileType               = 'exe'
+  SilentArgs             = '/S'
+  url                    = 'http://www.lesegais.ru/upload/EGAISCryptoSetup.exe'
+  checksum               = '96b3843571b8bf669852734656f262bc346c1b9c85e02735dbaa244e03d5306b'
+  checksumType           = 'sha256'
+}
+Install-ChocolateyPackage @packageArgs
