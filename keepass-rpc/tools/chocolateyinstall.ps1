fca555d4f9886e964071a5b48b9d0a787b1b76ac
David Gardiner <david@gardiner.net.au>
2018-05-02 11:10:02 +0930

    keepass-rpc 1.8

--- keepass-rpc/tools/chocolateyinstall.ps1
+++ keepass-rpc/tools/chocolateyinstall.ps1
@@ -11,8 +11,8 @@ $fileFullPath = [IO.Path]::Combine($fileFullPath, "KeePass Password Safe 2\Plugi
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
-  url           = "https://github.com/kee-org/keepassrpc/releases/download/v1.7.3.1/KeePassRPC.plgx"
-  checksum      = '3355834E4CE9D09D58E3F19F60505B0B06332FCE1F63B5EE908A1DF170192BF9'
+  url           = "https://github.com/kee-org/keepassrpc/releases/download/v1.8.0/KeePassRPC.plgx"
+  checksum      = '8D9D5E390FC4A3B8D6D8F24DD26A712DC032C4FF49708C8EC32C95A2E27594B5'
   checksumType  = 'sha256'
   FileFullPath   = $fileFullPath
 }
