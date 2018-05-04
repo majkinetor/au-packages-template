0fcf842ef3680a985d0defa711067fd898707f15
Alexander Georgievskiy <galeksandrp@gmail.com>
2018-05-04 22:51:26 +0300

    [ci skip] Fix 3proxy
    * Fix 32bit url detection
    * Add lite version (Vista / Server 2003 lower OS)

--- 3proxy/tools/chocolateyinstall.ps1
+++ 3proxy/tools/chocolateyinstall.ps1
@@ -1,9 +1,45 @@
-﻿$packageArgs = @{
+﻿# According to https://stackoverflow.com/q/7330187
+ 
+# [System.Environment]::OSVersion.Version
+# - does not show [Windows 10 release](https://en.wikipedia.org/wiki/Windows_10_version_history)
+# - does not differentiate between Windows 8.1 (6.3.9600) and Windows 8 (6.2.9200)
+# - [deprecated](https://blogs.technet.microsoft.com/heyscriptingguy/2014/04/25/use-powershell-to-find-operating-system-version/)
+ 
+# [Environment]::OSVersion is same as previous
+ 
+# Get-WmiObject Win32_OperatingSystem
+# - does not show [Windows 10 release](https://en.wikipedia.org/wiki/Windows_10_version_history)
+# - [deprecated](https://blogs.technet.microsoft.com/heyscriptingguy/2014/04/25/use-powershell-to-find-operating-system-version/)
+ 
+# Get-CimInstance Win32_OperatingSystem
+# - does not show [Windows 10 release](https://en.wikipedia.org/wiki/Windows_10_version_history)
+ 
+# Get-ComputerInfo is PowerShell 5+
+
+$url = 'https://github.com/z3APA3A/3proxy/releases/download/0.8.11/3proxy-0.8.11.zip'
+$urlVista2003 = 'https://github.com/z3APA3A/3proxy/releases/download/0.8.11/3proxy-0.8.11-lite.zip'
+$checksum = '2c83da8290afd11313ee22709d6e0c68656b2ac87e421e7ca7e5204fd00b44ac'
+$checksumVista2003 = '7f7cdbac89247cf158b3d5f7e67a7c68aaefcf6224f5ec63d91cae6c7263f4ed'
+
+function getFile{
+    $windowsVersion = Get-ItemProperty "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion"
+	if ([double]$windowsVersion.CurrentVersion -ge 5.2) {
+		return @{url = $url
+		checksum = $checksum}
+	} else {
+		return @{url = $urlVista2003
+		checksum = $checksumVista2003}
+	}
+}
+
+$file = getFile
+
+$packageArgs = @{
   packageName            = "$env:chocolateyPackageName"
-  url                    = 'https://github.com/z3APA3A/3proxy/releases/download/0.8.11/3proxy-0.8.11.zip'
+  url                    = $file.url
   url64bit               = 'https://github.com/z3APA3A/3proxy/releases/download/0.8.11/3proxy-0.8.11-x64.zip'
   UnzipLocation          = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
-  checksum               = '2c83da8290afd11313ee22709d6e0c68656b2ac87e421e7ca7e5204fd00b44ac'
+  checksum               = $file.checksum
   checksum64             = '9310ba8233ce77d31f27878d11509f1b613edfedbfef735e2cb76c0619705fb4'
   checksumType           = 'sha256'
   checksumType64         = 'sha256'
--- 3proxy/update.ps1
+++ 3proxy/update.ps1
@@ -3,10 +3,12 @@ import-module au
 function global:au_SearchReplace {
    @{
         ".\tools\chocolateyInstall.ps1" = @{
-            "(?i)(^\s*url\s*=\s*)('.*')"        = "`$1'$($Latest.URL32)'"
-            "(?i)(^\s*url64bit\s*=\s*)('.*')"   = "`$1'$($Latest.URL64)'"
-            "(?i)(^\s*checksum\s*=\s*)('.*')"   = "`$1'$($Latest.Checksum32)'"
-            "(?i)(^\s*checksum64\s*=\s*)('.*')" = "`$1'$($Latest.Checksum64)'"
+            "(?i)(^\s*\`$url\s*=\s*)('.*')"                 = "`$1'$($Latest.URL32)'"
+            "(?i)(^\s*url64bit\s*=\s*)('.*')"              = "`$1'$($Latest.URL64)'"
+			"(?i)(^\s*\`$urlVista2003\s*=\s*)('.*')"        = "`$1'$($Latest.URLLite)'"
+            "(?i)(^\s*\`$checksum\s*=\s*)('.*')"            = "`$1'$($Latest.Checksum32)'"
+            "(?i)(^\s*checksum64\s*=\s*)('.*')"            = "`$1'$($Latest.Checksum64)'"
+			"(?i)(^\s*\`$checksumVista2003\s*=\s*)('.*')"   = "`$1'$($Latest.ChecksumVista2003)'"
         }
     }
 }
@@ -14,11 +16,20 @@ function global:au_SearchReplace {
 function global:au_GetLatest {
     $release = curl "https://api.github.com/repos/z3APA3A/3proxy/releases/latest?client_id=$env:GITHUB_CLIENT_ID&client_secret=$env:GITHUB_CLIENT_SECRET" | ConvertFrom-Json
 	
-	@{
-        URL32   = ($release.assets | where {!$_.name.EndsWith("-lite.zip")} | where {$_.name.EndsWith("-x64.zip")}).browser_download_url
-		URL64   = ($release.assets | where {$_.name.EndsWith("-x64.zip")}).browser_download_url
-        Version = $release.tag_name
+	$version = $release.tag_name
+	
+	$Latest = @{
+		URL32   = ($release.assets -match "3proxy-$version.zip").browser_download_url
+		URL64   = ($release.assets -match "3proxy-$version-x64.zip").browser_download_url
+		URLLite = ($release.assets -match "3proxy-$version-lite.zip").browser_download_url
+        Version = $version
     }
+	
+	$Latest.Checksum32 = Get-RemoteChecksum $Latest.URL32
+	$Latest.Checksum64 = Get-RemoteChecksum $Latest.URL64
+	$Latest.ChecksumVista2003 = Get-RemoteChecksum $Latest.URLLite
+	
+	$Latest
 }
 
-update
+update -NoCheckChocoVersion
