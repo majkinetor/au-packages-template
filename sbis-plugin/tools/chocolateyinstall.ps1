0d2783bae332b3b9d5a6513b8f39bb3e051469cc
Chocolatey <chocolatey@realdimensions.net>
2018-05-04 03:59:33 -0700

    AU: 2 updated - sbis-plugin uefitool [skip ci] https://gist.github.com/03903cd565061a825f153ce2f9d6227c/785a6e437bdf910b78db7db6d04f228b8afcc4eb

--- sbis-plugin/tools/chocolateyinstall.ps1
+++ sbis-plugin/tools/chocolateyinstall.ps1
@@ -1,9 +1,9 @@
-$packageArgs = @{
+﻿$packageArgs = @{
   packageName            = "$env:chocolateyPackageName"
   FileType               = 'exe'
   SilentArgs             = '/verysilent /SUPPRESSMSGBOXES /NORESTART /notShowInstallInProcessMessage'
   url                    = 'https://update.sbis.ru/SbisPlugin/master/SbisPlugin.exe'
-  checksum               = 'dddbf5512b96f4576e0f554a9c6a992f31c7fcdf58e352387a3b63141a6374c5'
+  checksum               = '463dc406e2ba0285bff7f343d7906fd7ad2069f239419cac871cff5ab74c27d3'
   checksumType           = 'sha256'
   ValidExitCodes         = @(0,1)
 }
--- uefitool/tools/chocolateyinstall.ps1
+++ uefitool/tools/chocolateyinstall.ps1
@@ -1,8 +1,8 @@
 ﻿$packageArgs = @{
   packageName            = "$env:chocolateyPackageName"
-  url                    = 'https://github.com/LongSoft/UEFITool/releases/download/0.22.3/UEFITool_0.22.3_win.zip'
+  url                    = 'https://github.com/LongSoft/UEFITool/releases/download/0.23.0/UEFITool_0.23.0_mac.zip'
   UnzipLocation          = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
-  checksum               = 'bf962fb0bd9cb733d15ed334be3b17cac81f10063205bb6cf88ff32f3429a80c'
+  checksum               = '0031c25d4f86e8a98a3070c9a8cc199bb8cf10aba7bb182fe6bdeaea5ab27725'
   checksumType           = 'sha256'
 }
 Install-ChocolateyZipPackage @packageArgs
