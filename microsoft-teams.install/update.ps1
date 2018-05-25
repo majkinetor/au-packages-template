e732b04beb12b3c63fc164e9ee3db83137108743
David Gardiner <david@gardiner.net.au>
2018-05-25 19:53:00 +0930

    Handle extra redirection

--- microsoft-teams.install/update.ps1
+++ microsoft-teams.install/update.ps1
@@ -13,14 +13,31 @@ function global:au_SearchReplace {
 
 function global:au_GetLatest {
     # 32bit
-    $response = Invoke-WebRequest -Uri "http://aka.ms/teams32bitmsi" -Method Head -MaximumRedirection 0 -ErrorAction Ignore
-    $url32 = $response.Headers.Location
+    
+    $url32 = "http://aka.ms/teams32bitmsi"
+    for ($i = 0; ($i -lt 4) -and (-not ($url32.EndsWith(".msi"))); $i++) {
+        $response = Invoke-WebRequest -Uri $url32 -Method Head -MaximumRedirection 0 -ErrorAction Ignore
+
+        $url32 = $response.Headers.Location
+    }
+
+    if ($i -ge 3) {
+        return @{}
+    }
 
     #https://statics.teams.microsoft.com/production-windows-x64/1.1.00.7855/Teams_windows.msi
 
-    $response = Invoke-WebRequest -Uri "http://aka.ms/teams64bitmsi" -Method Head -MaximumRedirection 0 -ErrorAction Ignore
-    $url64 = $response.Headers.Location
+    $url64 = "http://aka.ms/teams64bitmsi"
+    for ($i = 0; ($i -lt 4) -and (-not ($url64.EndsWith(".msi"))); $i++) {
+        $response = Invoke-WebRequest -Uri $url64 -Method Head -MaximumRedirection 0 -ErrorAction Ignore
 
+        $url64 = $response.Headers.Location
+    }
+
+    if ($i -ge 3) {
+        return @{}
+    }
+    
     $url32 -match ".*\/(?<version>\d+\.\d+\.\d+\.\d+)\/Teams_windows\.msi"   
 
     $version = $Matches.version
