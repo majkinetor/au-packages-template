61db98e8e3aa42a0bef5f55ab38f14eb0f61efdc
Alexander Georgievskiy <galeksandrp@gmail.com>
2018-05-29 12:42:18 +0300

    [AU gogs] Init gogs

---
+++ gogs/tools/chocolateyinstall.ps1
@@ -0,0 +1,11 @@
+﻿$packageArgs = @{
+  packageName            = "$env:chocolateyPackageName"
+  url                    = 'https://github.com/gogs/gogs/releases/download/v0.11.34/windows_386.zip'
+  url64bit               = 'https://github.com/gogs/gogs/releases/download/v0.11.34/windows_amd64.zip'
+  UnzipLocation          = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
+  checksum               = '456fcae619224bc98ee17298899ef8db95bd34c125f9400bbfc10a91744af249'
+  checksum64             = '8d1eecc23162627ade4207c20f359ff8ea1f90661aeae4be88d571151491819f'
+  checksumType           = 'sha256'
+  checksumType64         = 'sha256'
+}
+Install-ChocolateyZipPackage @packageArgs
---
+++ gogs/update.ps1
@@ -0,0 +1,24 @@
+import-module au
+
+function global:au_SearchReplace {
+   @{
+        ".\tools\chocolateyInstall.ps1" = @{
+            "(?i)(^\s*url\s*=\s*)('.*')"        = "`$1'$($Latest.URL32)'"
+            "(?i)(^\s*url64bit\s*=\s*)('.*')"   = "`$1'$($Latest.URL64)'"
+            "(?i)(^\s*checksum\s*=\s*)('.*')"   = "`$1'$($Latest.Checksum32)'"
+            "(?i)(^\s*checksum64\s*=\s*)('.*')" = "`$1'$($Latest.Checksum64)'"
+        }
+    }
+}
+
+function global:au_GetLatest {
+    $release = curl "https://api.github.com/repos/gogs/gogs/releases/latest?client_id=$env:GITHUB_CLIENT_ID&client_secret=$env:GITHUB_CLIENT_SECRET" | ConvertFrom-Json
+	
+	@{
+        URL32   = $release.assets.browser_download_url -match 'windows_386.zip'
+		URL64   = $release.assets.browser_download_url -match 'windows_amd64.zip'
+        Version = $release.tag_name -replace '^v',''
+    }
+}
+
+update
