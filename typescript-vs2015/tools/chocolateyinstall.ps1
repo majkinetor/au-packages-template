a621150776f5eb670d1a043e5396b7b29db871ff
flcdrg <david@gardiner.net.au>
2018-06-01 00:09:53 +0000

    AU: 7 updated - dotMemory dotPeek dotTrace ReSharper resharper-clt resharper-ultimate-all typescript-vs2015 [skip ci] https://gist.github.com/7d00c69c2cde8309a8594abaa897a9a6/c370fc46ca1fe808b1a82e96778d797e0029c073

--- typescript-vs2015/tools/chocolateyinstall.ps1
+++ typescript-vs2015/tools/chocolateyinstall.ps1
@@ -2,9 +2,9 @@
 
 $packageName= 'typescript-vs2015'
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://download.microsoft.com/download/6/D/8/6D8381B0-03C1-4BD2-AE65-30FF0A4C62DA/2.9.0-TS-release-dev14update3-20180514.1/TypeScript_Dev14Full.exe'
-$checksum   = '6204a8e5e4f3eca98ec9541b7c76825871c776c6e262409a7633e2ac5effd0e9'
-$installVersion = '2.9.0.0 RC'
+$url        = 'https://download.microsoft.com/download/6/D/8/6D8381B0-03C1-4BD2-AE65-30FF0A4C62DA/2.9.1-TS-release-dev14update3-20180525.2/TypeScript_Dev14Full.exe'
+$checksum   = '0720ba5a63e3253a511b8e9fe86be630c070cccf68c93a00cdc14bae9a4ca21d'
+$installVersion = '2.9.1.0'
 $softwareName = "TypeScript Tools for Microsoft Visual Studio 2015 $installVersion"
 
 $packageArgs = @{
