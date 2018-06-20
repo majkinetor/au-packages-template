8569fa1d3f3b990ea9266ac91cd7f7689fc81a94
Chocolatey <chocolatey@realdimensions.net>
2018-06-20 04:58:58 -0700

    AU: 1 updated - grafana [skip ci] https://gist.github.com/03903cd565061a825f153ce2f9d6227c/08b4edf7dddd1582008621db32cbb0e71e367ee6

--- grafana/tools/chocolateyinstall.ps1
+++ grafana/tools/chocolateyinstall.ps1
@@ -1,8 +1,8 @@
 ﻿$packageArgs = @{
   packageName            = "$env:chocolateyPackageName"
-  url                    = 'https://s3-us-west-2.amazonaws.com/grafana-releases/release/grafana-5.1.3.windows-x64.zip'
+  url                    = 'https://s3-us-west-2.amazonaws.com/grafana-releases/release/grafana-5.1.4.windows-x64.zip'
   UnzipLocation          = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
-  checksum               = '6b93d063c693a730fd38a6d024fdf5ff9d49dd43f5698c9be931272ccfb4b074'
+  checksum               = '529a4ead110e541a4c6d88cbedcbfee7b7e0153d725e8b13b9438a7b2a8c7be9'
   checksumType           = 'sha256'
 }
 Install-ChocolateyZipPackage @packageArgs
