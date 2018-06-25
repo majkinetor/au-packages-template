45bccebdd851f86ac8cbef1841af240fc515cb0e
Chocolatey <chocolatey@realdimensions.net>
2018-06-25 05:01:48 -0700

    AU: 1 updated - uefitool [skip ci] https://gist.github.com/03903cd565061a825f153ce2f9d6227c/9bad91316c615cefcee77def320f2d3a717ec814

--- uefitool/tools/chocolateyinstall.ps1
+++ uefitool/tools/chocolateyinstall.ps1
@@ -1,8 +1,8 @@
 ﻿$packageArgs = @{
   packageName            = "$env:chocolateyPackageName"
-  url                    = 'https://github.com/LongSoft/UEFITool/releases/download/0.25.0/UEFITool_0.25.0_win32.zip'
+  url                    = 'https://github.com/LongSoft/UEFITool/releases/download/0.25.1/UEFITool_0.25.1_win32.zip'
   UnzipLocation          = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
-  checksum               = 'c92bec82c3d56932e1c7d07c0d13c94b0308d3de26d5ef984eb7549cf546790b'
+  checksum               = '4ee26b66de617c4a5c78c8dd47fd4f1a304318da9940a92587d9bc30c6677414'
   checksumType           = 'sha256'
 }
 Install-ChocolateyZipPackage @packageArgs
