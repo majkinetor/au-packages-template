a5edfc0c76bc8635891e51b5a79b09bc9c2f1cb4
Chocolatey <chocolatey@realdimensions.net>
2018-06-27 05:03:36 -0700

    AU: 1 updated - foldersizes [skip ci] https://gist.github.com/03903cd565061a825f153ce2f9d6227c/b443577dfee9511de65af5187ead8a88f9fbe4b2

--- foldersizes/tools/chocolateyinstall.ps1
+++ foldersizes/tools/chocolateyinstall.ps1
@@ -3,7 +3,7 @@
   FileType               = 'exe'
   SilentArgs             = '/qn'
   url                    = 'https://s3.amazonaws.com/KeyMetricSoft/FolderSizes/fs8-setup.exe'
-  checksum               = '4705855d459a47a7e26e9529ed83866045995090f96b9ffcc76cbec1ab2e86ee'
+  checksum               = '1b80c3b072e2944089d16b8fa294f4d27e70067ef4b870e9e1f65074a5c5d0ad'
   checksumType           = 'sha256'
 }
 Install-ChocolateyPackage @packageArgs
