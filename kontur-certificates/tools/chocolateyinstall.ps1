cf5b5c2fbd7fdff1a8e3ef25c738aa8082f4991f
Chocolatey <chocolatey@realdimensions.net>
2018-07-02 05:05:40 -0700

    AU: 1 updated - kontur-certificates [skip ci] https://gist.github.com/03903cd565061a825f153ce2f9d6227c/9877906ec9ba84a45bf942bd1caf7f3838700627

--- kontur-certificates/tools/chocolateyinstall.ps1
+++ kontur-certificates/tools/chocolateyinstall.ps1
@@ -3,7 +3,7 @@
   FileType               = 'exe'
   SilentArgs             = '/nowait'
   url                    = 'https://help.kontur.ru/content/components/Certificates_Kontur_Admin.exe'
-  checksum               = 'a9c99ea1e42f05d0ffc3229f35f880391277d08c072a36f4fd0640bf9f60a9bf'
+  checksum               = '82cc54c5abc72aef1dbe8ae58e0cf82ab6d46eeb5a3ffdeae39700ddb60db557'
   checksumType           = 'sha256'
 }
 Install-ChocolateyPackage @packageArgs
