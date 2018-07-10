0f8d00fb92d7ecdafa2007e78855097675f5c16a
Alexander Georgievskiy <galeksandrp@gmail.com>
2018-07-10 17:22:25 +0300

    [AU letsencrypt-win-simple] Init letsencrypt-win-simple

---
+++ letsencrypt-win-simple/tools/chocolateyinstall.ps1
@@ -0,0 +1,8 @@
+﻿$packageArgs = @{
+  packageName            = "$env:chocolateyPackageName"
+  url                    = 'https://github.com/PKISharp/win-acme/releases/download/v1.9.8.4/letsencrypt-win-simple.v1.9.8.4.zip'
+  UnzipLocation          = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
+  checksum               = '6b5d3899df25b6a7b6b439bbb10c5ce05f77108e443b3bea8286d37721a82c56'
+  checksumType           = 'sha256'
+}
+Install-ChocolateyZipPackage @packageArgs
---
+++ letsencrypt-win-simple/update.ps1
@@ -0,0 +1,21 @@
+import-module au
+
+function global:au_SearchReplace {
+   @{
+        ".\tools\chocolateyInstall.ps1" = @{
+            "(?i)(^\s*url\s*=\s*)('.*')"        = "`$1'$($Latest.URL32)'"
+            "(?i)(^\s*checksum\s*=\s*)('.*')"   = "`$1'$($Latest.Checksum32)'"
+        }
+    }
+}
+
+function global:au_GetLatest {
+    $release = curl "https://api.github.com/repos/PKISharp/win-acme/releases/latest?client_id=$env:GITHUB_CLIENT_ID&client_secret=$env:GITHUB_CLIENT_SECRET" | ConvertFrom-Json
+	
+	@{
+        URL32   = $release.assets.browser_download_url
+        Version = $release.tag_name -replace '^v',''
+    }
+}
+
+update
