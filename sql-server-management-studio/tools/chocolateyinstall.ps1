076080ff4a1edef021ba8dfd560a0248aaa102bb
flcdrg <david@gardiner.net.au>
2018-07-10 02:11:39 +0000

    AU: 1 updated - sql-server-management-studio [skip ci] https://gist.github.com/7d00c69c2cde8309a8594abaa897a9a6/b6e9f2ce74f9a6d11061ab3226e3227ffc44d0d6

--- sql-server-management-studio/tools/chocolateyinstall.ps1
+++ sql-server-management-studio/tools/chocolateyinstall.ps1
@@ -3,13 +3,13 @@
 $packageName= 'SQL Server Management Studio'
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-$fullUrl = 'https://download.microsoft.com/download/0/D/2/0D26856F-E602-4FB6-8F12-43D2559BDFE4/SSMS-Setup-ENU.exe'
-$fullChecksum = '78591C090A91256CCBEBD871DD77EC1677436FC5CEB87560C914C6921D176FBD'
+$fullUrl = 'https://download.microsoft.com/download/0/5/B/05B2AF8F-906F-4C57-A58E-5780F64F9D62/SSMS-Setup-ENU.exe'
+$fullChecksum = '0A822A542E62A944D1D60D52E4501BA48588F20DED6DCFA33A119649171A7050'
 
-$upgradeUrl = 'https://download.microsoft.com/download/0/D/2/0D26856F-E602-4FB6-8F12-43D2559BDFE4/SSMS-Setup-ENU-Upgrade.exe'
-$upgradeChecksum = 'B841B8B4EAAF01FC9860E6D5817A5EF9F8B477C6E14362B03EE5276CDB47D223'
+$upgradeUrl = 'https://download.microsoft.com/download/0/5/B/05B2AF8F-906F-4C57-A58E-5780F64F9D62/SSMS-Setup-ENU-Upgrade.exe'
+$upgradeChecksum = 'DBF4B2AC747A976203A6A9D583CF823B5E301585FA1C2091C084D5058FD6F7B2'
 
-$release = '17.7'
+$release = '17.8.1'
 
 # Check if 17.0 is installed so we can get upgrade package instead of full package
 $version17 = [version]"14.0.17099.0"
