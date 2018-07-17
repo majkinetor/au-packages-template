b3851f8e43cd75457b89acfca29f46677f6d8c38
Alex Dove <adove@practicefusion.com>
2018-07-16 20:22:32 -0700

    Allow for users to specify the products they want to install. * Update documentation. * Update last modified version so that package is actually downloaded from redgate instead of FTP.

--- sqltoolbelt/tools/chocolateyinstall.ps1
+++ sqltoolbelt/tools/chocolateyinstall.ps1
@@ -6,7 +6,43 @@ $secondaryDownloadUrl = 'ftp://support.red-gate.com/patches/SQLToolbelt/18Jul201
 $packageVersionLastModified = New-Object -TypeName DateTimeOffset 2018, 7, 18, 15, 13, 16, 0 # Last modified time corresponding to this package version
 $checksum = '76799F80DE7BE4F85EDE2A968BC8AD99518CDA14DA4180BE5C0D8A716202E020'
 
+$validProductPackageNames = @(
+  "SQL Compare",
+  "SQL Data Compare",
+  "SQL Source Control",
+  "SQL Prompt",
+  "SQL Search",
+  "SQL Data Generator",
+  "SQL Doc",
+  "SQL Test",
+  "DLM Dashboard",
+  "SQL Multi Script",
+  "SQL Dependency Tracker",
+  "SQL Monitor",
+  "SQL Backup",
+  "SSMS Integration Pack",
+  "SQL Change Automation Powershell",
+  "SQL Change Automation" )
+
 $pp = Get-PackageParameters
+
+$commandArgs = ""
+if ($pp["products"] -ne $null -and $pp["products"] -ne ''){
+
+  $products = $pp["products"].Split(",")
+  foreach($product in $products){
+    if(!$validProductPackageNames.Contains($product.Trim())){
+      throw "Invalid product package name '$product', exiting installer."
+    }
+  }
+
+  $productCommand = "products ""$($pp["products"])"""
+  $commandArgs += "$productCommand "
+} else {
+
+  $productCommand = "all products"
+}
+$commandArgs += "/IAgreeToTheEula"
 
 if ($pp["FTP"] -ne $null -and $pp["FTP"] -ne '') { 
 
@@ -14,7 +50,7 @@ if ($pp["FTP"] -ne $null -and $pp["FTP"] -ne '') {
     $url = $secondaryDownloadUrl
 } else {
 
-  # Red Gate have a fixed download URL, but if the binary changes we can fall back to their FTP site
+  # Red Gate has a fixed download URL, but if the binary changes we can fall back to their FTP site
   # so the package doesn't break
   $headers = Get-WebHeaders -url $primaryDownloadUrl
   $lastModifiedHeader = $headers.'Last-Modified'
@@ -35,14 +71,14 @@ if ($pp["FTP"] -ne $null -and $pp["FTP"] -ne '') {
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
-  fileType      = 'exe'
-  silentArgs    = "/IAgreeToTheEula"
+  fileFullPath  = "$toolsDir\SQLToolbelt.exe"
   
-  validExitCodes= @(0)
   url           = $url
   checksum      = $checksum
   checksumType  = 'sha256'
-  destination   = $toolsDir
 }
 
-Install-ChocolateyPackage @packageArgs
+$executable = Get-ChocolateyWebFile @packageArgs
+
+Write-Host "Installing $productCommand from $env:ChocolateyPackageName"
+Start-Process $executable -ArgumentList $commandArgs -Wait
