fa0abbf1fcd8a125de81532e8749764d9cbce1ec
David Gardiner <david@gardiner.net.au>
2018-07-30 02:21:45 +0930

    teamcityaddin

---
+++ teamCityAddin/tools/chocolateyinstall.ps1
@@ -0,0 +1,16 @@
+﻿$platformPackageName = 'resharper-platform'
+$packageName = 'teamCityAddin'
+$scriptPath = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
+$commonPath = $(Split-Path -parent $(Split-Path -parent $scriptPath))
+$filename = 'JetBrains.ReSharperUltimate.2018.1.3.exe'
+
+$installPath = Join-Path  (Join-Path $commonPath $platformPackageName) $filename
+$packageArgs = @{
+  packageName   = $packageName
+  fileType      = 'exe'
+  file          = $installPath
+  silentArgs    = "/Silent=True /SpecificProductNames=teamCityAddin /VsVersion=*"
+  validExitCodes= @(0)
+  softwareName  = 'TeamCity Add-in'
+}
+Install-ChocolateyInstallPackage @packageArgs
---
+++ teamCityAddin/tools/chocolateyuninstall.ps1
@@ -0,0 +1,8 @@
+﻿$platformPackageName = 'resharper-platform'
+$packageName = 'teamCityAddin'
+$scriptPath = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
+$commonPath = $(Split-Path -parent $(Split-Path -parent $scriptPath))
+$filename = 'JetBrains.ReSharperUltimate.2018.1.3.exe'
+
+$installPath = Join-Path  (Join-Path $commonPath $platformPackageName) $filename
+Uninstall-ChocolateyPackage $packageName 'exe' '/Silent=True /SpecificProductNamesToRemove=teamCityAddin /VsVersion=*' $installPath
