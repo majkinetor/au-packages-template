f9cd52b1293f17ad95b2ce2c333b3e3836696c3b
David Gardiner <david@gardiner.net.au>
2018-07-30 01:38:31 +0930

    ReSharper C++

---
+++ ReSharperCpp/update.ps1
@@ -0,0 +1,23 @@
+import-module au
+
+function global:au_SearchReplace {
+    @{
+        'ReSharperCpp.nuspec' = @{
+            "(^.+version=`")(\[.*\])(`".+$)"   = "`$1[$($Latest.PlatformVersion)]`$3"
+        }
+        'tools\chocolateyInstall.ps1' = @{
+            "(^[$]filename\s*=\s*)([`"'].*[`"'])"      = "`$1'$($Latest.Filename)'"
+        }
+        'tools\chocolateyUninstall.ps1' = @{
+            "(^[$]filename\s*=\s*)([`"'].*[`"'])"      = "`$1'$($Latest.Filename)'"
+        }
+     }
+}
+
+. ..\resharper-platform\common.ps1
+
+function global:au_GetLatest {
+    return GetJetbrainsProductLatest
+}
+
+update -ChecksumFor none
