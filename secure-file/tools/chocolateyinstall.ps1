2d54fb0778401a9d50e3407ed86ed38857d668e1
Alexander Georgievskiy <galeksandrp@gmail.com>
2018-07-31 21:07:05 +0300

    [AU secure-file:1.0.0] Init secure-file

---
+++ secure-file/tools/chocolateyinstall.ps1
@@ -0,0 +1,8 @@
+﻿$packageArgs = @{
+  packageName            = "$env:chocolateyPackageName"
+  url                    = 'https://github.com/appveyor/secure-file/releases/download/1.0.0/secure-file.zip'
+  UnzipLocation          = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
+  checksum               = '85fbbe94d0461d7e0283b3c69fddaa0a36e0ec7ab272b62f733c5c9eb9969f45'
+  checksumType           = 'sha256'
+}
+Install-ChocolateyZipPackage @packageArgs
