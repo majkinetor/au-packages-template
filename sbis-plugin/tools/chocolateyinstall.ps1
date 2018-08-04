1c271467f1f0b7221bc5453c30bf4727a770af07
Chocolatey <chocolatey@realdimensions.net>
2018-08-04 05:18:56 -0700

    AU: 2 updated - sbis-plugin stunnel-msspi [skip ci] https://gist.github.com/03903cd565061a825f153ce2f9d6227c/998e93dfa5a46cc42988eb2a8b2c9315f62d5769

--- sbis-plugin/tools/chocolateyinstall.ps1
+++ sbis-plugin/tools/chocolateyinstall.ps1
@@ -3,7 +3,7 @@
   FileType               = 'exe'
   SilentArgs             = '/verysilent /SUPPRESSMSGBOXES /NORESTART /notShowInstallInProcessMessage'
   url                    = 'https://update.sbis.ru/SbisPlugin/master/SbisPlugin.exe'
-  checksum               = '38396af42d3c467b295b9ab8026490ae1ef79cb32d4922de2c3200213afafd29'
+  checksum               = '42ba6270f64c021e0c666bb333c359fda6fb5eb99257e4f1d0c6d40924ca28d3'
   checksumType           = 'sha256'
   ValidExitCodes         = @(0,1)
 }
--- stunnel-msspi/tools/chocolateyinstall.ps1
+++ stunnel-msspi/tools/chocolateyinstall.ps1
@@ -1,10 +1,10 @@
 ﻿$packageArgs = @{
   packageName            = "$env:chocolateyPackageName"
-  url                    = 'https://github.com/deemru/stunnel/releases/download/stunnel-5.44-msspi-0.99/stunnel-5.44-msspi-0.99_windows-386.zip'
-  url64bit               = 'https://github.com/deemru/stunnel/releases/download/stunnel-5.44-msspi-0.99/stunnel-5.44-msspi-0.99_windows-amd64.zip'
+  url                    = 'https://github.com/deemru/stunnel/releases/download/stunnel-5.48-msspi-0.106/stunnel-5.48-msspi-0.106_windows-386.zip'
+  url64bit               = 'https://github.com/deemru/stunnel/releases/download/stunnel-5.48-msspi-0.106/stunnel-5.48-msspi-0.106_windows-amd64.zip'
   UnzipLocation          = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
-  checksum               = '007a9819bbb52596e4b046f20647d9cd91f9f1444ff8ff81589b2cd25126842d'
-  checksum64             = '98b933d3875d4e7993c5b422e3e815722b2c1d1e82786540ba336abf92568e1e'
+  checksum               = '04a3ed06985a82bc49bb850370e7f82daae9dbc76e6b48b6e8fce26d114873d3'
+  checksum64             = 'e3855c3056d70cb22b69638f25a89358a2d159308770853e6b1fb63a8250813c'
   checksumType           = 'sha256'
   checksumType64         = 'sha256'
 }
