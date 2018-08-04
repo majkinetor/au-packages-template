400af98f5029395845ba81107fc6c5da03311ad5
David Gardiner <david@gardiner.net.au>
2018-08-04 11:33:41 +0930

    Add PingPlotter

---
+++ pingplotter/tools/chocolateyinstall.ps1
@@ -0,0 +1,17 @@
+﻿$ErrorActionPreference = 'Stop';
+$toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  softwareName  = 'PingPlotter*'
+  fileType      = 'exe'
+  silentArgs    = "/quiet"
+  
+  validExitCodes= @(0)
+  url           = "https://www.pingplotter.com/downloads/pingplotter_install.exe"
+  checksum      = 'F292E61CCD42FA0E30521B7A682EE8FAF28D8A692A00444C93CCE816C2AF0827'
+  checksumType  = 'sha256'
+  destination   = $toolsDir
+}
+
+Install-ChocolateyPackage @packageArgs
