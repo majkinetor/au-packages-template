330de56f09f2023e8df00ca89bb7770c02a59220
Alexander Georgievskiy <galeksandrp@gmail.com>
2018-08-10 19:17:16 +0300

    [AU nsq\1.1:1.1.0] Migrate nsq to AU Streams

--- nsq/update.ps1
+++ nsq/update.ps1
@@ -3,19 +3,59 @@ import-module au
 function global:au_SearchReplace {
    @{
         ".\tools\chocolateyInstall.ps1" = @{
-            "(?i)(^\s*url\s*=\s*)('.*')"        = "`$1'$($Latest.URL32)'"
-            "(?i)(^\s*checksum\s*=\s*)('.*')"   = "`$1'$($Latest.Checksum32)'"
+            "(?i)(^\s*url\s*=\s*)('.*')"                   = "`$1'$($Latest.URL32)'"
+            "(?i)(^\s*checksum\s*=\s*)('.*')"              = "`$1'$($Latest.Checksum32)'"
+			"(?i)(^\s*FileFullPath\s*=\s*)(`".*`")"        = "`$1`"$($Latest.FileFullPath)`""
         }
     }
 }
 
 function global:au_GetLatest {
-    $release = curl "https://api.github.com/repos/nsqio/nsq/releases/latest?client_id=$env:GITHUB_CLIENT_ID&client_secret=$env:GITHUB_CLIENT_SECRET" | ConvertFrom-Json
-	
-	@{
-        URL32   = $release.assets.browser_download_url -match 'windows-amd64'
-        Version = $release.tag_name -replace '^v',''
-    }
+	$versions = [ordered]@{}
+
+	(curl "https://api.github.com/repos/nsqio/nsq/releases?client_id=$env:GITHUB_CLIENT_ID&client_secret=$env:GITHUB_CLIENT_SECRET" | ConvertFrom-Json) | ? { #filter out nuspec-noncompatible versions. Filter is applied here, and not in foreach because it is needed immediately in next sort
+		try {
+			[version]($_.tag_name -replace '^v','')
+		}
+		catch {
+		}
+	} | % { # sort can't be used there because
+        # Order      $versions         releases
+        # -----      ---------         --------
+        # Descending in order          [x] older release overwrite newer
+        # Asscending [x] reverse order newer release overwrite older
+		$version = [version]($_.tag_name -replace '^v','')
+		$version = $version.major, $version.minor -join '.'
+		if (!$versions[$version]) {
+		
+		$url = $_.assets.browser_download_url -match 'windows-amd64'
+
+		if ($url) { $versions[$version] = @{ # append to external $versions variable since powershell does not reduce well, because in powershell it is possible for hashtable to have items with same names
+			# foreach create hashtables like
+			# Name                           Value                                                                                                 
+			# ----                           -----                                                                                                 
+			# 0.25                           {URL32, Version}                                                                                      
+			# 0.25                           {URL32, Version}                                                                                      
+			# 0.24                           {URL32, Version}                                                                                      
+			# 0.23                           {URL32, Version}                                                                                      
+			# 0.22                           {URL32, Version}                                                                                      
+			# 0.22                           {URL32, Version}                                                                                      
+			# 0.22                           {URL32, Version}                                                                                      
+			# 0.21                           {URL32, Version}
+			# with nested values
+			# Name                           Value                                                                                                 
+			# ----                           -----                                                                                                 
+			# URL32                          0.25.1                                                                                                
+			# Version                        https://github.com/LongSoft/UEFITool/releases/download/0.25.1/UEFITool_0.25.1_win32.zip               
+			# URL32                          0.25.1                                                                                                
+			# Version                        https://github.com/LongSoft/UEFITool/releases/download/0.25.1/UEFITool_0.25.1_win32.zip
+			URL32   = $url
+			Version = $_.tag_name -replace '^v',''
+			FileFullPath = "`$env:TMP\$(($url -split '/')[-1] -replace '\.gz$')"
+		} } }
+	}
+
+	@{Streams = $versions}
 }
 
 update
