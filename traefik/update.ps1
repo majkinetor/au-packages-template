89bf13b50c21e77f616ccffe4567d6f913c0c525
Alexander Georgievskiy <galeksandrp@gmail.com>
2018-08-10 12:46:19 +0300

    [AU traefik\1.6:1.6.6] Migrate traefik to AU Streams

--- traefik/update.ps1
+++ traefik/update.ps1
@@ -16,13 +16,52 @@ function global:au_BeforeUpdate() {
 }
 
 function global:au_GetLatest {
-    $release = curl "https://api.github.com/repos/containous/traefik/releases/latest?client_id=$env:GITHUB_CLIENT_ID&client_secret=$env:GITHUB_CLIENT_SECRET" | ConvertFrom-Json
-	
-	@{
-        URL32   = $release.assets.browser_download_url -match 'traefik_windows-386.exe'
-		URL64   = $release.assets.browser_download_url -match 'traefik_windows-amd64.exe'
-        Version = $release.tag_name -replace '^v',''
-    }
+	$versions = [ordered]@{}
+
+	(curl "https://api.github.com/repos/containous/traefik/releases?client_id=$env:GITHUB_CLIENT_ID&client_secret=$env:GITHUB_CLIENT_SECRET" | ConvertFrom-Json) | ? { #filter out nuspec-noncompatible versions. Filter is applied here, and not in foreach because it is needed immediately in next sort
+		try {
+			[version]($_.tag_name -replace '^v','')
+		}
+		catch {
+		}
+	} | % { # sort can't be used there because
+        # Order      $versions         releases
+        # -----      ---------         --------
+        # Descending in order          [x] older release overwrite newer
+        # Asscending [x] reverse order newer release overwrite older
+		$version = [version]($_.tag_name -replace '^v','')
+		$version = $version.major, $version.minor -join '.'
+		if (!$versions[$version]) {
+		
+		$url = $_.assets.browser_download_url -match 'traefik_windows-386.exe'
+		$url64 = $_.assets.browser_download_url -match 'traefik_windows-amd64.exe'
+
+		if ($url -and $url64) { $versions[$version] = @{ # append to external $versions variable since powershell does not reduce well, because in powershell it is possible for hashtable to have items with same names
+			# foreach create hashtables like
+			# Name                           Value                                                                                                 
+			# ----                           -----                                                                                                 
+			# 0.25                           {URL32, Version}                                                                                      
+			# 0.25                           {URL32, Version}                                                                                      
+			# 0.24                           {URL32, Version}                                                                                      
+			# 0.23                           {URL32, Version}                                                                                      
+			# 0.22                           {URL32, Version}                                                                                      
+			# 0.22                           {URL32, Version}                                                                                      
+			# 0.22                           {URL32, Version}                                                                                      
+			# 0.21                           {URL32, Version}
+			# with nested values
+			# Name                           Value                                                                                                 
+			# ----                           -----                                                                                                 
+			# URL32                          0.25.1                                                                                                
+			# Version                        https://github.com/LongSoft/UEFITool/releases/download/0.25.1/UEFITool_0.25.1_win32.zip               
+			# URL32                          0.25.1                                                                                                
+			# Version                        https://github.com/LongSoft/UEFITool/releases/download/0.25.1/UEFITool_0.25.1_win32.zip
+			URL32 = $url
+			URL64 = $url64
+			Version = $_.tag_name -replace '^v',''
+		} } }
+	}
+
+	@{Streams = $versions}
 }
 
 update
