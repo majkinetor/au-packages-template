4473dce0c62fccdd8e6215e7fdd571ce988773b5
Alexander Georgievskiy <galeksandrp@gmail.com>
2018-08-12 00:15:31 +0300

    [ci skip] Upd acme-sh

--- acme-sh/tools/chocolateyinstall.ps1
+++ acme-sh/tools/chocolateyinstall.ps1
@@ -1,10 +1,8 @@
 ﻿$packageArgs = @{
   packageName            = "$env:chocolateyPackageName"
-  FileFullPath           = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)\acme.sh"
-  url                    = 'https://raw.githubusercontent.com/Neilpang/acme.sh/2.7.9/acme.sh'
-  checksum               = '4321b2e53be435f6d17c479a2212257e9c895367ce3a653aa33f25d80760720b'
+  url                    = 'https://github.com/Neilpang/acme.sh/archive/2.7.9.zip'
+  UnzipLocation          = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
+  checksum               = 'bb975a3f943e579b559e584227966bb16b93bec2cf1dc46421906e859a34504a'
   checksumType           = 'sha256'
 }
-
-(New-Object System.Net.WebClient).DownloadFile($packageArgs.url, $packageArgs.FileFullPath)
-Get-ChecksumValid -File $packageArgs.FileFullPath -CheckSum $packageArgs.checksum -ChecksumType $packageArgs.sha256
+Install-ChocolateyZipPackage @packageArgs
