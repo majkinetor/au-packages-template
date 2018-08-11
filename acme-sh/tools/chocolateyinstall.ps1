815de5bf738b97e4e88121b9329a270ef26f0df0
Alexander Georgievskiy <galeksandrp@gmail.com>
2018-08-11 16:34:59 +0300

    [AU acme-sh] Init acme-sh

---
+++ acme-sh/tools/chocolateyinstall.ps1
@@ -0,0 +1,10 @@
+﻿$packageArgs = @{
+  packageName            = "$env:chocolateyPackageName"
+  FileFullPath           = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)\acme.sh"
+  url                    = 'https://raw.githubusercontent.com/Neilpang/acme.sh/2.7.9/acme.sh'
+  checksum               = '4321b2e53be435f6d17c479a2212257e9c895367ce3a653aa33f25d80760720b'
+  checksumType           = 'sha256'
+}
+
+(New-Object System.Net.WebClient).DownloadFile($packageArgs.url, $packageArgs.FileFullPath)
+Get-ChecksumValid -File $packageArgs.FileFullPath -CheckSum $packageArgs.checksum -ChecksumType $packageArgs.sha256
---
+++ acme-sh/update.ps1
@@ -0,0 +1,60 @@
+import-module au
+
+function global:au_SearchReplace {
+   @{
+        ".\tools\chocolateyInstall.ps1" = @{
+            "(?i)(^\s*url\s*=\s*)('.*')"        = "`$1'$($Latest.URL32)'"
+            "(?i)(^\s*checksum\s*=\s*)('.*')"   = "`$1'$($Latest.Checksum32)'"
+        }
+    }
+}
+
+function global:au_GetLatest {
+	$versions = [ordered]@{}
+
+	(curl "https://api.github.com/repos/Neilpang/acme.sh/releases?client_id=$env:GITHUB_CLIENT_ID&client_secret=$env:GITHUB_CLIENT_SECRET" | ConvertFrom-Json) | ? { #filter out nuspec-noncompatible versions. Filter is applied here, and not in foreach because it is needed immediately in next sort
+		try {
+			[version]$_.tag_name
+		}
+		catch {
+		}
+	} | % { # sort can't be used there because
+        # Order      $versions         releases
+        # -----      ---------         --------
+        # Descending in order          [x] older release overwrite newer
+        # Asscending [x] reverse order newer release overwrite older
+		$version = [version]$_.tag_name
+		$version = $version.major, $version.minor -join '.'
+		if (!$versions[$version]) {
+		
+        $url = "https://raw.githubusercontent.com/Neilpang/acme.sh/$($_.tag_name)/acme.sh"
+		if ([version]$_.tag_name -lt [version]'2.2.9') {$url = "https://raw.githubusercontent.com/Neilpang/acme.sh/$($_.tag_name)/le.sh"}
+
+		if ($url) { $versions[$version] = @{ # append to external $versions variable since powershell does not reduce well, because in powershell it is possible for hashtable to have items with same names
+			# foreach create hashtables like
+			# Name                           Value                                                                                                 
+			# ----                           -----                                                                                                 
+			# 0.25                           {URL32, Version}                                                                                      
+			# 0.25                           {URL32, Version}                                                                                      
+			# 0.24                           {URL32, Version}                                                                                      
+			# 0.23                           {URL32, Version}                                                                                      
+			# 0.22                           {URL32, Version}                                                                                      
+			# 0.22                           {URL32, Version}                                                                                      
+			# 0.22                           {URL32, Version}                                                                                      
+			# 0.21                           {URL32, Version}
+			# with nested values
+			# Name                           Value                                                                                                 
+			# ----                           -----                                                                                                 
+			# URL32                          0.25.1                                                                                                
+			# Version                        https://github.com/LongSoft/UEFITool/releases/download/0.25.1/UEFITool_0.25.1_win32.zip               
+			# URL32                          0.25.1                                                                                                
+			# Version                        https://github.com/LongSoft/UEFITool/releases/download/0.25.1/UEFITool_0.25.1_win32.zip
+			URL32   = $url
+			Version = $_.tag_name
+		} } }
+	}
+
+	@{Streams = $versions}
+}
+
+update
