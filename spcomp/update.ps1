13be1be8462708308fa012422ea349adf1a63e63
Alexander Georgievskiy <galeksandrp@gmail.com>
2018-08-15 17:26:04 +0300

    [AU spcomp] Add 1.7 branch

--- spcomp/update.ps1
+++ spcomp/update.ps1
@@ -11,7 +11,7 @@ function global:au_SearchReplace {
 
 function global:au_GetLatest {
 	function getSourcemodLatestRelease {
-		param($versionMajorMinor)
+		param($versionMajorMinor, [switch]$dev)
 		$release = ((curl "https://sm.alliedmods.net/smdrop/$versionMajorMinor/").Links -match '-windows.zip').href | select -last 1
 		$version = [version]($release -split '-')[1]
 		$revision = ($release -split '-')[2] -replace '^git',''
@@ -21,17 +21,22 @@ function global:au_GetLatest {
 		if ($version.build) {
 			$build = $version.build
 		}
+		
+		$version = ($version.major, $version.minor -join '.'), ($build, $revision -join '') -join '.'
+		if ($dev) {$version = $version, 'dev' -join '-'}
 
 		@{
 			URL32   = "https://sm.alliedmods.net/smdrop/$versionMajorMinor/$release"
-			Version = ($version.major, $version.minor -join '.'), ($build, $revision -join '') -join '.'
+			Version = $version
 		}
 	}
 	
 	@{
 		Streams = [ordered] @{
+		    '1.10' = getSourcemodLatestRelease '1.10' -dev
 		    '1.9' = getSourcemodLatestRelease '1.9'
 		    '1.8' = getSourcemodLatestRelease '1.8'
+		    '1.7' = getSourcemodLatestRelease '1.7'
 		}
 	}
 }
