c069bb8edcc07375bb42fdb426d8c5854bdb76e9
Alexander Georgievskiy <galeksandrp@gmail.com>
2018-08-18 12:15:13 +0300

    [AU radmin-vpn] Migrate radmin-vpn to AU

---
+++ radmin-vpn/tools/chocolateyinstall.ps1
@@ -0,0 +1,9 @@
+﻿$packageArgs = @{
+  packageName            = "$env:chocolateyPackageName"
+  FileType               = 'exe'
+  SilentArgs             = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+  url                    = 'https://www.radmin-vpn.com/download/Radmin_VPN_1.0.3524.2.exe'
+  checksum               = 'd9da7751058c2ea776e7364a3a1269d129cdf8e9d1b9d39a5c9f372a150c6139'
+  checksumType           = 'sha256'
+}
+Install-ChocolateyPackage @packageArgs
---
+++ radmin-vpn/update.ps1
@@ -0,0 +1,24 @@
+import-module au
+import-module $env:ChocolateyInstall\helpers\chocolateyInstaller.psm1
+
+function global:au_SearchReplace {
+   @{
+        ".\tools\chocolateyInstall.ps1" = @{
+            "(?i)(^\s*url\s*=\s*)('.*')"        = "`$1'$($Latest.URL32)'"
+            "(?i)(^\s*checksum\s*=\s*)('.*')"   = "`$1'$($Latest.Checksum32)'"
+        }
+    }
+}
+
+function global:au_GetLatest {
+	$hostname = 'https://www.radmin-vpn.com'
+	$URL32 = $hostname + ((curl $hostname).Links.href -match '\.exe$' | Get-Unique)
+    Get-ChocolateyWebFile 'radmin-vpn' $URL32 -FileFullPath "$env:TMP\Radmin_VPN.exe"
+	
+	@{
+        URL32   = $URL32
+        Version = (Get-Item "$env:TMP\Radmin_VPN.exe").VersionInfo.ProductVersion.trim()
+    }
+}
+
+update
