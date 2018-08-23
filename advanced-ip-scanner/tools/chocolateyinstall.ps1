0860b120b3cc193a93576200592468fd4ae0b758
Chocolatey <chocolatey@realdimensions.net>
2018-08-23 13:47:26 -0700

    AU: 1 updated - advanced-ip-scanner [skip ci] https://gist.github.com/03903cd565061a825f153ce2f9d6227c/3ced32ed971757d97815515125aad365ef2a2302

--- advanced-ip-scanner/tools/chocolateyinstall.ps1
+++ advanced-ip-scanner/tools/chocolateyinstall.ps1
@@ -2,8 +2,8 @@
   packageName            = "$env:chocolateyPackageName"
   FileType               = 'exe'
   SilentArgs             = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
-  url                    = 'https://www.advanced-ip-scanner.com/download/Advanced_IP_Scanner_2.5.3581.exe'
-  checksum               = '49AFD1429544C5F722C188C299287014E4D24AD8C5F32EB0AA9D8B8C32FC03BE'
+  url                    = 'https://www.advanced-ip-scanner.com/download/Advanced_IP_Scanner_2.5.3646.exe'
+  checksum               = '02ec949206023f22fe1a5b67b3864d6a653cc4c5bfcb32241ecf802f213805e8'
   checksumType           = 'sha256'
 }
 Install-ChocolateyPackage @packageArgs
