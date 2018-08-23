a7edf5135a4bd8aa952c1a9e363d01ab8a926269
Alexander Georgievskiy <galeksandrp@gmail.com>
2018-08-23 23:44:11 +0300

    [AU advanced-ip-scanner] Migrate advanced-ip-scanner to AU

---
+++ advanced-ip-scanner/tools/chocolateyinstall.ps1
@@ -0,0 +1,9 @@
+﻿$packageArgs = @{
+  packageName            = "$env:chocolateyPackageName"
+  FileType               = 'exe'
+  SilentArgs             = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+  url                    = 'https://www.advanced-ip-scanner.com/download/Advanced_IP_Scanner_2.5.3581.exe'
+  checksum               = '49AFD1429544C5F722C188C299287014E4D24AD8C5F32EB0AA9D8B8C32FC03BE'
+  checksumType           = 'sha256'
+}
+Install-ChocolateyPackage @packageArgs
---
+++ advanced-ip-scanner/update.ps1
@@ -0,0 +1,24 @@
+import-module au
+import-module $env:ChocolateyInstall\helpers\chocolateyInstaller.psm1
+
+function global:au_SearchReplace {
+   @{
+        ".\tools\chocolateyInstall.ps1" = @{
+            "(?i)(^\s*url\s*=\s*)('.*')"        = "`$1'$($Latest.URL32)'"
+            "(?i)(^\s*checksum\s*=\s*)('.*')"   = "`$1'$($Latest.Checksum32)'"
+        }
+    }
+}
+
+function global:au_GetLatest {
+	$hostname = 'https://www.advanced-ip-scanner.com'
+	$URL32 = $hostname + ((curl $hostname).Links.href -match '\.exe$' | Get-Unique)
+    Get-ChocolateyWebFile 'advanced-ip-scanner' $URL32 -FileFullPath "$env:TMP\Advanced_IP_Scanner.exe"
+	
+	@{
+        URL32   = $URL32
+        Version = (Get-Item "$env:TMP\Advanced_IP_Scanner.exe").VersionInfo.ProductVersion.trim()
+    }
+}
+
+update
