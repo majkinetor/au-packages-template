04bc75f166eacfd76540f2b17a1a13571ca0b6e2
Chocolatey <chocolatey@realdimensions.net>
2018-08-24 05:31:09 -0700

    AU: 2 updated - cspclean sdrsharp [skip ci] https://gist.github.com/03903cd565061a825f153ce2f9d6227c/ccb7da2f62f14f0f724e5063e07e60d1507db2ed

--- cspclean/tools/chocolateyinstall.ps1
+++ cspclean/tools/chocolateyinstall.ps1
@@ -2,7 +2,7 @@
   packageName            = "$env:chocolateyPackageName"
   FileFullPath           = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)\cspclean.exe"
   url                    = 'https://www.cryptopro.ru/sites/default/files/public/cspclean.exe'
-  checksum               = '5185d970e2e4f4747ec691d7db35ef75683f4612f74cddc070afe4b84e9f372d'
+  checksum               = '2a9d80165e95ba52ad93c4baee282bcb2c6770419484927905c00b47d3a9e72b'
   checksumType           = 'sha256'
 }
 Get-ChocolateyWebFile @packageArgs
--- sdrsharp/tools/chocolateyinstall.ps1
+++ sdrsharp/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@
   url                    = 'https://airspy.com/downloads/sdrsharp-x86.zip'
   url64bit               = 'https://airspy.com/downloads/sdrsharp-x64.zip'
   UnzipLocation          = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
-  checksum               = '59648e1472c318170a08d5c6837ca1ed5787c7c05841593c1c9968bb2616f894'
-  checksum64             = 'cdac015aba8189a5dce2b6bb3215fdc191d45853f51955096976210c143df1e1'
+  checksum               = 'ea823fd45af3bb8c34923a7e6dbb31a7909c6ed5902f7f09d2e4a14a6e66757d'
+  checksum64             = '3ad7a06e48dd99dde5371774c8ac88a2eaa84ce17c2d7ee4b49ff7127b5dbbbd'
   checksumType           = 'sha256'
   checksumType64         = 'sha256'
 }
