07694cb7a232002c5b7d37c57f995499885943b7
Chocolatey <chocolatey@realdimensions.net>
2018-08-31 05:35:20 -0700

    AU: 1 updated - kingston-ssd-manager [skip ci] https://gist.github.com/03903cd565061a825f153ce2f9d6227c/246f4bd8e7b5bd161b62eac850508ae625fc1145

--- kingston-ssd-manager/tools/chocolateyinstall.ps1
+++ kingston-ssd-manager/tools/chocolateyinstall.ps1
@@ -3,7 +3,7 @@
   FileType               = 'exe'
   SilentArgs             = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
   url                    = 'https://media.kingston.com/support/downloads/KSM-setup.exe'
-  checksum               = '878477181f099210ea6cc315b0f977111c4ee36fab2315cd851d55d4f1bf0ed9'
+  checksum               = '3eb4ea076442cc9adec86fb05b2452291744a7ab48b1bfbcacfee1420e1d1c43'
   checksumType           = 'sha256'
 }
 Install-ChocolateyPackage @packageArgs
