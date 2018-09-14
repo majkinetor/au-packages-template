a63a1afe058e86a4fc1c42b9a4c8fb72c9985af1
Chocolatey <chocolatey@realdimensions.net>
2018-09-14 05:39:47 -0700

    AU: 1 updated - xca [skip ci] https://gist.github.com/03903cd565061a825f153ce2f9d6227c/86503ec482ca5e7dcd38a6f2fb1c2564e20a6721

--- xca/tools/chocolateyinstall.ps1
+++ xca/tools/chocolateyinstall.ps1
@@ -2,8 +2,8 @@
   packageName            = "$env:chocolateyPackageName"
   FileType               = 'exe'
   SilentArgs             = '/S'
-  url                    = 'https://github.com/chris2511/xca/releases/download/RELEASE.2.1.0/setup_xca-2.1.0.exe'
-  checksum               = 'b519aec836d9d4888a2606dc1f3b40077ed17f67caf73ce8b8b3b38f7ccb71db'
+  url                    = 'https://github.com/chris2511/xca/releases/download/RELEASE.2.1.1/setup_xca-2.1.1.exe'
+  checksum               = '42635b17c3a92b3aa4bc59307a80225cbfa7c553957c1dd59c1fd3aeee022df0'
   checksumType           = 'sha256'
 }
 Install-ChocolateyPackage @packageArgs
