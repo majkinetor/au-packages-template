a704e1b7c8bbd5179174ee5ba1563317c3e87668
Chocolatey <chocolatey@realdimensions.net>
2018-09-17 12:42:00 +0000

    AU: 1 updated - gogs [skip ci] https://gist.github.com/03903cd565061a825f153ce2f9d6227c/1a64a98763c05ac07074d46a61632bb40fd567db

--- gogs/tools/chocolateyinstall.ps1
+++ gogs/tools/chocolateyinstall.ps1
@@ -1,10 +1,10 @@
 ﻿$packageArgs = @{
   packageName            = "$env:chocolateyPackageName"
-  url                    = 'https://github.com/gogs/gogs/releases/download/v0.11.53/windows_386.zip'
-  url64bit               = 'https://github.com/gogs/gogs/releases/download/v0.11.53/windows_amd64.zip'
+  url                    = 'https://github.com/gogs/gogs/releases/download/v0.11.66/windows_386.zip'
+  url64bit               = 'https://github.com/gogs/gogs/releases/download/v0.11.66/windows_amd64.zip'
   UnzipLocation          = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
-  checksum               = 'c59fb7af4bf4ddb298c415ed999a337edd53bd2b115940cdb05dd1629434841b'
-  checksum64             = 'cc1e43ffa4a8fc37ec695adb2cccd75c84a3901088b9995f5516d06f5e7e811c'
+  checksum               = '0620f201b53f24ba8d2c29c54585609f73890f67e3fb843dc4fc708a14bf2592'
+  checksum64             = '2ff78e94f03e6cc02e819dd6e1a45bacd3a5975cf570b3b8dd73c9cc1d0215c1'
   checksumType           = 'sha256'
   checksumType64         = 'sha256'
 }
