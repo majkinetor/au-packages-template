4498a6eb518ef60fbc04c003650eb917d8c88ae0
Chocolatey <chocolatey@realdimensions.net>
2018-09-29 12:47:29 +0000

    AU: 1 updated - traefik [skip ci] https://gist.github.com/03903cd565061a825f153ce2f9d6227c/70447753855a59a6ae78a50e8d88c63e1392fe2b

--- traefik/tools/chocolateyinstall.ps1
+++ traefik/tools/chocolateyinstall.ps1
@@ -1,10 +1,10 @@
 ﻿$packageArgs = @{
   packageName            = "$env:chocolateyPackageName"
-  url                    = 'https://github.com/containous/traefik/releases/download/v1.7.0/traefik_windows-386.exe'
-  url64bit               = 'https://github.com/containous/traefik/releases/download/v1.7.0/traefik_windows-amd64.exe'
+  url                    = 'https://github.com/containous/traefik/releases/download/v1.7.1/traefik_windows-386.exe'
+  url64bit               = 'https://github.com/containous/traefik/releases/download/v1.7.1/traefik_windows-amd64.exe'
   FileFullPath           = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)\traefik.exe"
-  checksum               = 'dff7018aeb9b775619fdcc15e28318733defcdac24e34bb5e406790af8bd3650'
-  checksum64             = '710819197badf26c4b7def2f5e4ade3e90a4bc37dd7e5e947f4a212a3d65d199'
+  checksum               = '7e97054889c1b466ccd5a3d5cfbfdc5884dd1ebeebf1fb814e05878ac66c037c'
+  checksum64             = 'cc5cd2dab93db8c8deff9648ded4cd2409e97602f22122d252158d804550965f'
   checksumType           = 'sha256'
   checksumType64         = 'sha256'
 }
