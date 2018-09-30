758274af390614b4197aba0cbe1eea8603900cd8
David Gardiner <david@gardiner.net.au>
2018-09-30 18:13:48 +0930

    update for windows-admin-center

--- windows-admin-center/tools/chocolateyinstall.ps1
+++ windows-admin-center/tools/chocolateyinstall.ps1
@@ -1,5 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop';
 $toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url = 'https://download.microsoft.com/download/1/0/5/1059800B-F375-451C-B37E-758FFC7C8C8B/WindowsAdminCenter1804.25.msi'
+$checksum = '3F49AD90CA3BBCA3147B1DF4E5C55E1A8B44BBAEF8F6DFA05BA8CF1AE0D2089E'
 
 $pp = Get-PackageParameters
 
@@ -24,8 +26,8 @@ $packageArgs = @{
   fileType      = 'msi'
   silentArgs    = "/qn /norestart SME_PORT=$port SSL_CERTIFICATE_OPTION=$certificateOption $thumbprint /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`"" # ALLUSERS=1 DISABLEDESKTOPSHORTCUT=1 ADDDESKTOPICON=0 ADDSTARTMENU=0
   validExitCodes= @(0, 3010, 1641)
-  url           = "https://download.microsoft.com/download/1/0/5/1059800B-F375-451C-B37E-758FFC7C8C8B/WindowsAdminCenter1804.25.msi"
-  checksum      = '3F49AD90CA3BBCA3147B1DF4E5C55E1A8B44BBAEF8F6DFA05BA8CF1AE0D2089E'
+  url           = $url
+  checksum      = $checksum
   checksumType  = 'sha256'
   destination   = $toolsDir
 }
---
+++ windows-admin-center/update.ps1
@@ -0,0 +1,55 @@
+import-module au
+
+function global:au_SearchReplace {
+    @{
+        'tools\chocolateyInstall.ps1' = @{
+            "(^[$]url\s*=\s*)('.*')"     = "`$1'$($Latest.Url32)'"
+            "(^[$]checksum\s*=\s*)('.*')"    = "`$1'$($Latest.Checksum32)'"
+        }
+        'windows-admin-center.nuspec' = @{
+            "(\<title\>).*?(\</title\>)" = "`${1}Windows Admin Center ($($Latest.DisplayVersion))`$2"
+        }
+     }
+}
+
+function global:au_GetLatest {
+
+    $Latest = @{}
+
+    try {
+        # Get latest version from XML
+        $response = Invoke-WebRequest -Method Head -Uri "https://aka.ms/WACDownload" -MaximumRedirection 0 -ErrorAction Ignore
+
+        $url = $response.Headers.Location -replace "http:", "https:"
+
+        # https://download.microsoft.com/download/1/0/5/1059800B-F375-451C-B37E-758FFC7C8C8B/WindowsAdminCenter1809.msi
+        $url -match "WindowsAdminCenter(?<version>\d+(\.\d+)?)\.msi$"
+
+        Write-Verbose "Downloading $url"
+        $client = new-object System.Net.WebClient
+        $downloadedFile = [IO.Path]::GetTempFileName()
+
+        $client.DownloadFile($url, $downloadedFile)
+
+        $version = (.\Get-MSIInfo.ps1 -Path $downloadedFile -Property ProductVersion)[3]
+
+        Write-Verbose "$version"
+        $checksum = (Get-FileHash $downloadedFile -Algorithm SHA256).Hash
+        Write-Verbose "$checksum"
+
+        Remove-Item $downloadedFile -Force
+
+        $Latest = @{
+            Version = $version
+            DisplayVersion = $Matches.version
+            Url32 = $url
+            Checksum32 = $checksum
+        }
+    }
+    catch {
+        Write-Error $_
+    }
+    return $Latest
+}
+
+update -ChecksumFor none
