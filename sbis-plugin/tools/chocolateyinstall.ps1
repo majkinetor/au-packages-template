dafeef3589c1ea11570e59f2d76cea7adcc4fa38
Chocolatey <chocolatey@realdimensions.net>
2018-10-02 12:50:57 +0000

    AU: 1 updated - sbis-plugin [skip ci] https://gist.github.com/03903cd565061a825f153ce2f9d6227c/bdbf0e178b078835f37949da225826721f905480

--- sbis-plugin/tools/chocolateyinstall.ps1
+++ sbis-plugin/tools/chocolateyinstall.ps1
@@ -3,7 +3,7 @@
   FileType               = 'exe'
   SilentArgs             = '/verysilent /SUPPRESSMSGBOXES /NORESTART /notShowInstallInProcessMessage'
   url                    = 'https://update.sbis.ru/SbisPlugin/master/SbisPlugin.exe'
-  checksum               = '42ba6270f64c021e0c666bb333c359fda6fb5eb99257e4f1d0c6d40924ca28d3'
+  checksum               = 'ace56dc80739c164157839cc15e29e33e72df4e6617fb7905a53cdcef4363336'
   checksumType           = 'sha256'
   ValidExitCodes         = @(0,1)
 }
