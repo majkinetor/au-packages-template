6dd04b6b126238b4f6a1363d74506d0fb5b56679
Alexander Georgievskiy <galeksandrp@gmail.com>
2018-10-04 11:56:26 +0300

    [AU onescript\1.0:1.0.21] Init onescript

---
+++ onescript/tools/chocolateyinstall.ps1
@@ -0,0 +1,9 @@
+﻿$packageArgs = @{
+  packageName            = "$env:chocolateyPackageName"
+  FileType               = 'exe'
+  SilentArgs             = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+  url                    = 'https://github.com/EvilBeaver/OneScript/releases/download/v1.0.21/OneScript-1.0.21-setup.exe'
+  checksum               = 'a37161180f930d67692f6f4d42b04fe542ddefedb74853d7284af6da9493d957'
+  checksumType           = 'sha256'
+}
+Install-ChocolateyPackage @packageArgs
