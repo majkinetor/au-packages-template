74f8518ed586c663b69ee51afcf9449889f80779
CompactFactor <40072316+CompactFactor@users.noreply.github.com>
2018-10-05 21:00:38 -0300

    add clipboardhelpandspell

---
+++ clipboardhelpandspell/tools/chocolateyInstall.ps1
@@ -0,0 +1,18 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+  fileType      = 'exe'
+  url           = 'https://www.donationcoder.com/Software/Mouser/clipboardhelpandspell/downloads/ClipboardHelpAndSpellSetup.exe'
+
+  softwareName  = 'Clipboard Help+Spell*'
+
+  checksum      = '7e4ea4dbb31f05e1397672d0914b663a2d69ffe5d8e210a3f4128804baf0be69'
+  checksumType  = 'sha256'
+
+  silentArgs    = "/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-"
+  validExitCodes= @(0)
+}
+
+Install-ChocolateyPackage @packageArgs
