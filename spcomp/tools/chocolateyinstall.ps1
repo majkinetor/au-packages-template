10402e520757d57ecfce401b2a93bc1f44dba88e
Chocolatey <chocolatey@realdimensions.net>
2018-10-15 12:55:14 +0000

    AU: 2 updated - spcomp traefik [skip ci] https://gist.github.com/03903cd565061a825f153ce2f9d6227c/3eac6705aebc25b307dd1a29cce1d25aebd81537

--- spcomp/tools/chocolateyinstall.ps1
+++ spcomp/tools/chocolateyinstall.ps1
@@ -1,8 +1,8 @@
 ﻿$packageArgs = @{
   packageName            = "$env:chocolateyPackageName"
-  url                    = 'https://sm.alliedmods.net/smdrop/1.10/sourcemod-1.10.0-git6352-windows.zip'
+  url                    = 'https://sm.alliedmods.net/smdrop/1.10/sourcemod-1.10.0-git6353-windows.zip'
   UnzipLocation          = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
-  checksum               = '8d06a45b3d84f9c194c1b05a16d497333bdc4ac841ff99754eec4ad32a38d21c'
+  checksum               = '7c5a92a03e439cbd45ab26ec3be3ee121a1490c88ac97cc04080ea4a542160ed'
   checksumType           = 'sha256'
   SpecificFolder         = "addons\sourcemod\scripting"
 }
--- traefik/tools/chocolateyinstall.ps1
+++ traefik/tools/chocolateyinstall.ps1
@@ -1,10 +1,10 @@
 ﻿$packageArgs = @{
   packageName            = "$env:chocolateyPackageName"
-  url                    = 'https://github.com/containous/traefik/releases/download/v1.7.2/traefik_windows-386.exe'
-  url64bit               = 'https://github.com/containous/traefik/releases/download/v1.7.2/traefik_windows-amd64.exe'
+  url                    = 'https://github.com/containous/traefik/releases/download/v1.7.3/traefik_windows-386.exe'
+  url64bit               = 'https://github.com/containous/traefik/releases/download/v1.7.3/traefik_windows-amd64.exe'
   FileFullPath           = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)\traefik.exe"
-  checksum               = 'ffdf289aa6bdd2dfa1722a6f86d940b1eae83de4bc1d2e993b79768047e7a95a'
-  checksum64             = '16e1690d95f27797ee6fbe578ba7ba3f531e0df4f61b6c215a7f0a454cdde743'
+  checksum               = '86256f8111291d40622e7d0f22c9266e4952c835ea092ee02117f7bf9d54d2a9'
+  checksum64             = '80f06a922a4cc8649a97016c9f907148f0b1d79fa35efc06ef8b3da2dcb06138'
   checksumType           = 'sha256'
   checksumType64         = 'sha256'
 }
