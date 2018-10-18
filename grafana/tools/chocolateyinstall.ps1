974a56594e3e1c10c0a2587dff5abc3786de034c
Chocolatey <chocolatey@realdimensions.net>
2018-10-18 12:56:20 +0000

    AU: 2 updated - grafana kontur-certificates [skip ci] https://gist.github.com/03903cd565061a825f153ce2f9d6227c/53a4746cac249c1ccfb96440dc78db5c837fe20c

--- grafana/tools/chocolateyinstall.ps1
+++ grafana/tools/chocolateyinstall.ps1
@@ -1,8 +1,8 @@
 ﻿$packageArgs = @{
   packageName            = "$env:chocolateyPackageName"
-  url                    = 'https://s3-us-west-2.amazonaws.com/grafana-releases/release/grafana-5.3.0.windows-amd64.zip'
+  url                    = 'https://s3-us-west-2.amazonaws.com/grafana-releases/release/grafana-5.3.1.windows-amd64.zip'
   UnzipLocation          = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
-  checksum               = '2785f021c313d4e7d1de426e0eeb64d4db9ebe47f95fc4d32e4a330e7c86aeb9'
+  checksum               = 'cecc4aaf221c2504f8a735a6f87eb66b1a53e7d7f8c5dbae290150ec2e552c30'
   checksumType           = 'sha256'
 }
 Install-ChocolateyZipPackage @packageArgs
--- kontur-certificates/tools/chocolateyinstall.ps1
+++ kontur-certificates/tools/chocolateyinstall.ps1
@@ -3,7 +3,7 @@
   FileType               = 'exe'
   SilentArgs             = '/nowait'
   url                    = 'https://help.kontur.ru/content/components/Certificates_Kontur_Admin.exe'
-  checksum               = '7ad558b2afea91b67f9a3d5b757829d80e1ec3e4ed9781d3e646141160f8d878'
+  checksum               = '33d0c92c9095c7be95e94413caebeacf4f2867e170924aa3457bf06bc0572439'
   checksumType           = 'sha256'
 }
 Install-ChocolateyPackage @packageArgs
