3ff391f074d0ab8651d1644f77ebf7ee52a03749
Alexander Georgievskiy <galeksandrp@gmail.com>
2018-10-25 12:50:51 +0300

    [AU gps-sdr-sim] Init gps-sdr-sim

---
+++ gps-sdr-sim/tools/chocolateyinstall.ps1
@@ -0,0 +1,8 @@
+﻿$packageArgs = @{
+  packageName            = "$env:chocolateyPackageName"
+  url                    = 'https://github.com/osqzss/gps-sdr-sim/releases/download/v1.0/gps-sdr-sim.zip'
+  UnzipLocation          = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
+  checksum               = '4c60abcad9fb21a1400f4de2639a580007ed6a432eb517940cc151f58bc2ffee'
+  checksumType           = 'sha256'
+}
+Install-ChocolateyZipPackage @packageArgs
