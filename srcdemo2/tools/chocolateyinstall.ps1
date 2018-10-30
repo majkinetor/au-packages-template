5a1a0e8046f337d6caf54ec45a09caae03eff623
Chocolatey <chocolatey@realdimensions.net>
2018-10-30 15:06:05 +0000

    AU: 1 updated - srcdemo2 [skip ci] https://gist.github.com/03903cd565061a825f153ce2f9d6227c/645dca8e7705893b7e77948a70d8230b805560c2

--- srcdemo2/tools/chocolateyinstall.ps1
+++ srcdemo2/tools/chocolateyinstall.ps1
@@ -2,8 +2,8 @@
   packageName            = "$env:chocolateyPackageName"
   FileType               = 'exe'
   SilentArgs             = '/S'
-  url                    = 'https://storage.googleapis.com/google-code-archive-downloads/v2/code.google.com/srcdemo2/SrcDemo2-setup-2012-04-07.exe'
-  checksum               = 'd6a081046f3b2672f03ed4c94850179faff9f0dd725916ddc0c7746dadf4b339'
+  url                    = 'https://storage.googleapis.com/google-code-archive-downloads/v2/code.google.com/srcdemo2/SrcDemo2-setup-2012-03-31.exe'
+  checksum               = '241877096af2be271a70328dc58fa7aeb483c07d25c2b9fd446c155bbd47879f'
   checksumType           = 'sha256'
 }
 Install-ChocolateyPackage @packageArgs
