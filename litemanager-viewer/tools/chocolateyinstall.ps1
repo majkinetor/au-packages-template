6a2c4226aa33ab3ffaec78fd12dc6bad6c06603f
Alexander Georgievskiy <galeksandrp@gmail.com>
2018-10-30 23:30:55 +0300

    [AU litemanager-viewer] Migrate litemanager-viewer to AU

---
+++ litemanager-viewer/tools/chocolateyinstall.ps1
@@ -0,0 +1,16 @@
+﻿$packageArgs = @{
+  packageName            = "$env:chocolateyPackageName"
+  url                    = 'http://litemanager.com/soft/litemanager_4.8.zip'
+  checksum               = '5e27c47fac38ef31467776b3872a2033c599d3cbd91cfbd5109e84509f0e14b2'
+  checksumType           = 'sha256'
+  UnzipLocation          = "$env:TMP"
+}
+Install-ChocolateyZipPackage @packageArgs
+
+$packageArgs = @{
+  packageName            = "$env:chocolateyPackageName"
+  FileType               = 'msi'
+  SilentArgs             = '/qn /norestart'
+  File                   = "$env:TMP\LiteManager Pro - Viewer.msi"
+}
+Install-ChocolateyInstallPackage @packageArgs
