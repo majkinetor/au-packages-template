8489f380698ddfc63eaa95294a968c451f6e3afa
Chocolatey <chocolatey@realdimensions.net>
2018-10-30 15:47:42 +0000

    AU: 1 updated - ninefs [skip ci] https://gist.github.com/03903cd565061a825f153ce2f9d6227c/edaf062fbb1e1b12658e2b03d8b4f8d4cf94a42a

--- ninefs/tools/chocolateyinstall.ps1
+++ ninefs/tools/chocolateyinstall.ps1
@@ -1,8 +1,8 @@
 ﻿$packageArgs = @{
   packageName            = "$env:chocolateyPackageName"
   FileFullPath           = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)\ninefs.exe"
-  url                    = 'https://storage.googleapis.com/google-code-archive-downloads/v2/code.google.com/ninefs/ninefs-20100121.exe'
-  checksum               = '01676879c8f1e9bf2047ebf4c84e96bca7a5d88844d350f5f374dbc980f87979'
+  url                    = 'https://storage.googleapis.com/google-code-archive-downloads/v2/code.google.com/ninefs/ninefs.exe'
+  checksum               = 'bda2b14ae66e9aff363ed642193b1badd3f3ee3e67b02bea281067e7a2baae8c'
   checksumType           = 'sha256'
 }
 Get-ChocolateyWebFile @packageArgs
