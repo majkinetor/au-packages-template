07f033292a8f8dcfab20695936fe618078b183c4
Alexander Georgievskiy <galeksandrp@gmail.com>
2018-10-30 22:07:43 +0300

    [AU twitch-bandwidth-tester] Revert "[ci skiptest] Move twitch-bandwidth-tester to manual packages"
    This reverts commit 2a85d6b3e536e7a36116fd1411b50eb56e4dab4e.

---
+++ twitch-bandwidth-tester/update.ps1
@@ -0,0 +1,26 @@
+import-module au
+import-module $env:ChocolateyInstall\helpers\chocolateyInstaller.psm1
+
+function global:au_SearchReplace {
+   @{
+        ".\tools\chocolateyInstall.ps1" = @{
+            "(?i)(^\s*url\s*=\s*)('.*')"        = "`$1'$($Latest.URL32)'"
+            "(?i)(^\s*checksum\s*=\s*)('.*')"   = "`$1'$($Latest.Checksum32)'"
+        }
+    }
+}
+
+function global:au_GetLatest {
+	$baseURL = 'https://r1ch.net'
+	$filepath = (curl "$baseURL/projects/twitchtest").Links.href -match '\.zip'
+	"$filepath" -match '[0-9]+[0-9.]+[0-9]+' > $null
+	$version = $Matches[0] -split '\.'
+	$version[1] = [int]$version[1] * 10
+		
+	@{
+		URL32   = "$baseURL$filepath"
+		Version = $version -join '.'
+	}
+}
+
+update
