f1a569edb70e044836729afa74d427ccc5fe16e8
Alexander Georgievskiy <galeksandrp@gmail.com>
2018-10-31 08:59:01 +0300

    [AU cuemounter] Migrate cuemounter to AU Streams

---
+++ cuemounter/tools/chocolateyinstall.ps1
@@ -0,0 +1,16 @@
+﻿$packageArgs = @{
+  packageName            = "$env:chocolateyPackageName"
+  url                    = 'https://codeplexarchive.blob.core.windows.net/archive/projects/cuemounter/cuemounter.zip'
+  checksum               = '1e7565d59e38a14208ecb750ddd6fb4858651dacaef03e14c1c7dca85bebbc15'
+  checksumType           = 'sha256'
+  UnzipLocation          = "$env:TMP"
+}
+Install-ChocolateyZipPackage @packageArgs
+
+$packageArgs = @{
+  packageName            = "$env:chocolateyPackageName"
+  FileType               = 'msi'
+  SilentArgs             = '/qn /norestart'
+  File                   = "$env:TMP\releases\1\c2f47213-bbd4-4541-b5e7-a9eab25dfc61"
+}
+Install-ChocolateyInstallPackage @packageArgs
---
+++ cuemounter/update.ps1
@@ -0,0 +1,36 @@
+import-module au
+import-module $env:ChocolateyInstall\helpers\chocolateyInstaller.psm1
+
+function global:au_SearchReplace {
+   @{
+        ".\tools\chocolateyInstall.ps1" = @{
+            "(?i)(^\s*url\s*=\s*)('.*')"        = "`$1'$($Latest.URL32)'"
+            "(?i)(^\s*checksum\s*=\s*)('.*')"   = "`$1'$($Latest.Checksum32)'"
+			"(?i)(^\s*File\s*=\s*)(`".*`")"   = "`$1`"$($Latest.File)`""
+        }
+    }
+}
+
+function global:au_GetLatest {
+	$URL32 = 'https://codeplexarchive.blob.core.windows.net/archive/projects/cuemounter/cuemounter.zip'
+    Get-ChocolateyWebFile 'cuemounter' "$env:TMP\cuemounter.zip" $URL32
+    Get-ChocolateyUnzip "$env:TMP\cuemounter.zip" "$env:TMP" -SpecificFolder "releases\releaseList.json"
+
+	$versions = [ordered]@{}
+
+	(cat "$env:TMP\releases\releaseList.json" | ConvertFrom-Json) | ? {$_.Files | ? {$_.FileName -match '\.msi$'}} | % {
+		$_.Name -match '([0-9]+[0-9.]+[0-9]+)'
+		$version = $Matches[1]
+        $filepath = ($_.Files | ? {$_.FileName -match '\.msi$'}).Url
+
+		$versions[$version] = @{
+			URL32   = $URL32
+			Version = $version
+			File = "`$env:TMP\releases\$filepath"
+		}
+	}
+
+	@{'Streams' = $versions}
+}
+
+update
