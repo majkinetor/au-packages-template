876e1aee794cdf4c3fbdc1935c0bb972d308bca8
Alexander Georgievskiy <galeksandrp@gmail.com>
2018-11-04 10:13:40 +0300

    [AU pdf417\3.0.32:3.0.32] Migrate pdf417 to AU Streams

--- pdf417/update.ps1
+++ pdf417/update.ps1
@@ -1,24 +1,43 @@
 import-module au
+import-module $env:ChocolateyInstall\helpers\chocolateyInstaller.psm1
 
 function global:au_SearchReplace {
    @{
         ".\tools\chocolateyInstall.ps1" = @{
             "(?i)(^\s*url\s*=\s*)('.*')"        = "`$1'$($Latest.URL32)'"
             "(?i)(^\s*checksum\s*=\s*)('.*')"   = "`$1'$($Latest.Checksum32)'"
+			"(?i)(^\s*File\s*=\s*)(`".*`")"     = "`$1`"$($Latest.File)`""
         }
     }
 }
 
 function global:au_GetLatest {
-	$baseURL = 'https://www.gnivc.ru'
-	$filepath = (curl "$baseURL/software/fnspo/software_ul_fl/pdf417/").Links.href -match '\.rar$'
-	$url = "$baseURL$filepath"
-	([uri]$url).segments[-1] -match '_([0-9]+[0-9_]+[0-9]+)'
+	$versions = [ordered]@{}
 
-	@{
-		URL32   = $url
-		Version = $Matches[1] -replace '_','.'
+	$baseURL = 'https://www.gnivc.ru'
+
+	"/software/fnspo/software_ul_fl/pdf417/","/software/fnspo/software_ul_fl/pdf_417/158125/" | % {
+		(curl "$baseURL/$_").Links.href -match '\.(rar|zip)$'
+	} | Get-Unique | % {
+		$URL32 = "$baseURL$_"
+		$archivePath = "$env:TMP\$($([uri]$URL32).segments[-1])"
+
+		Get-ChocolateyWebFile 'pdf417' $archivePath $URL32
+
+		$versionFolderPath = "$env:TMP\$($(ls $archivePath).BaseName)"
+
+		Get-ChocolateyUnzip $archivePath $versionFolderPath
+
+		$(ls "$versionFolderPath\*.msi").BaseName -match '\(([0-9]+[0-9.]+[0-9]+)\)'
+
+		$versions[$Matches[1]] = @{
+			URL32   = $URL32
+			Version = $Matches[1]
+			File    = "`$env:TMP\$($(ls "$versionFolderPath\*.msi").Name)"
+		}
 	}
+
+	@{'Streams' = $versions}
 }
 
 update
