f51bcac1852e583db18f70aff13ae36be6488501
Chocolatey <chocolatey@realdimensions.net>
2018-12-01 13:19:04 +0000

    AU: 1 updated - letsencrypt-win-simple [skip ci] https://gist.github.com/03903cd565061a825f153ce2f9d6227c/f967c22df714de2d57bd9eb79e32e5ec6bf80d01

--- letsencrypt-win-simple/tools/chocolateyinstall.ps1
+++ letsencrypt-win-simple/tools/chocolateyinstall.ps1
@@ -1,8 +1,8 @@
 ﻿$packageArgs = @{
   packageName            = "$env:chocolateyPackageName"
-  url                    = 'https://github.com/PKISharp/win-acme/releases/download/v1.9.12.1/win-acme.v1.9.12.1.zip'
+  url                    = 'https://github.com/PKISharp/win-acme/releases/download/v1.9.12.2/win-acme.v1.9.12.2.zip'
   UnzipLocation          = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
-  checksum               = '9ffc0f92c9c3391aeda90bc686cb3762794021b37f146337b111fcdd27319d84'
+  checksum               = '0e44e325af91cfbc1c1dc9222f47a01aea55a95fc6214f4f82560b715d2c82b1'
   checksumType           = 'sha256'
 }
 Install-ChocolateyZipPackage @packageArgs
