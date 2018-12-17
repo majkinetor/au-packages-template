460ac51fd2515cece8f955538a7f1e13b7ad573a
Chocolatey <chocolatey@realdimensions.net>
2018-12-17 13:32:50 +0000

    AU: 2 updated - kontur-certificates spcomp [skip ci] https://gist.github.com/03903cd565061a825f153ce2f9d6227c/dc1faab019a014088a6aee47596f6fbc3a9fe19c

--- kontur-certificates/tools/chocolateyinstall.ps1
+++ kontur-certificates/tools/chocolateyinstall.ps1
@@ -3,7 +3,7 @@
   FileType               = 'exe'
   SilentArgs             = '/nowait'
   url                    = 'https://help.kontur.ru/content/components/Certificates_Kontur_Admin.exe'
-  checksum               = 'd05b7cc6f9ce0ec78ddb62b9f7a34a9e49e2ddd36b4e641c65deca2414f8856f'
+  checksum               = '89a1ea9ee9f2d6b349033cc674cc2b90c854c91b0ff08e042cc19c350799ef9c'
   checksumType           = 'sha256'
 }
 Install-ChocolateyPackage @packageArgs
--- spcomp/tools/chocolateyinstall.ps1
+++ spcomp/tools/chocolateyinstall.ps1
@@ -1,8 +1,8 @@
 ﻿$packageArgs = @{
   packageName            = "$env:chocolateyPackageName"
-  url                    = 'https://sm.alliedmods.net/smdrop/1.10/sourcemod-1.10.0-git6366-windows.zip'
+  url                    = 'https://sm.alliedmods.net/smdrop/1.10/sourcemod-1.10.0-git6371-windows.zip'
   UnzipLocation          = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
-  checksum               = 'cc83e4232a9611f37d67bf57304c521b86dedeecdc3f30067ec02fdda0a44b83'
+  checksum               = '8d19634030e7fa950df7184098ca796977c3d18bcece43faab1bc4b05bdf076d'
   checksumType           = 'sha256'
   SpecificFolder         = "addons\sourcemod\scripting"
 }
