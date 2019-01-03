f45351273c3c0eed99e2c5052691a0c004ce7f68
Chocolatey <chocolatey@realdimensions.net>
2019-01-03 13:36:40 +0000

    AU: 2 updated - goodbyedpi spcomp [skip ci] https://gist.github.com/03903cd565061a825f153ce2f9d6227c/69276dc966ff6403bff76ab7153fe9b0c2547bfb

--- goodbyedpi/tools/chocolateyinstall.ps1
+++ goodbyedpi/tools/chocolateyinstall.ps1
@@ -3,9 +3,9 @@ $subfolder = '_64'
 }
 $packageArgs = @{
   packageName            = "$env:chocolateyPackageName"
-  url                    = 'https://github.com/ValdikSS/GoodbyeDPI/releases/download/0.1.4/goodbyedpi-0.1.4.zip'
+  url                    = 'https://github.com/ValdikSS/GoodbyeDPI/releases/download/0.1.5/goodbyedpi-0.1.5.zip'
   UnzipLocation          = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
-  checksum               = '0c834614e884b5840f6d2c52ed2815ef9544cfe305e30da8c3f9c9fad7d59315'
+  checksum               = '1e225ea392aebc2815bb5bec9a552a20d2feef6e6647e60d3b779deabeb65d55'
   checksumType           = 'sha256'
   SpecificFolder         = "goodbyedpi-0.1.4/x86$subfolder"
 }
--- spcomp/tools/chocolateyinstall.ps1
+++ spcomp/tools/chocolateyinstall.ps1
@@ -1,8 +1,8 @@
 ﻿$packageArgs = @{
   packageName            = "$env:chocolateyPackageName"
-  url                    = 'https://sm.alliedmods.net/smdrop/1.9/sourcemod-1.9.0-git6271-windows.zip'
+  url                    = 'https://sm.alliedmods.net/smdrop/1.10/sourcemod-1.10.0-git6374-windows.zip'
   UnzipLocation          = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
-  checksum               = '3ce73d759ac43d4cc386a84af7faf014ce6269d0108cd8105cb5e86aac132c89'
+  checksum               = 'af2bd47fe002071106258a05150fdc1513efa3fd971aaee1a9a5b16dd0ccefbe'
   checksumType           = 'sha256'
   SpecificFolder         = "addons\sourcemod\scripting"
 }
