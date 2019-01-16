84ff96f2f295b60a54e5bbb134c5006e95721864
flcdrg <david@gardiner.net.au>
2019-01-16 00:10:33 +0000

    AU: 1 updated - beyondcompare [skip ci] https://gist.github.com/7d00c69c2cde8309a8594abaa897a9a6/a13f8bb0af1c1426584c3e411a992e12894192ae

--- beyondcompare/tools/chocolateyInstall.ps1
+++ beyondcompare/tools/chocolateyInstall.ps1
@@ -15,7 +15,7 @@ $german = @(3079,1031,5127,4103,2055)
 $french = @(2060,11276,3084,9228,12300,1036,5132,13324,6156,14348,10252,4108,7180)
 $japanese = @(17, 1041)
 
-$version = '4.2.8.23479'
+$version = '4.2.9.23626'
 
 $packageArgs = @{
   packageName   = 'beyondcompare'
@@ -27,10 +27,10 @@ $packageArgs = @{
   checksumType  = 'sha256'
 }
 
-$checksumde = 'e1f8f150d57022da2dbb4024279bd6b731e516b4f5c2b7de5ddfab86fffce85d'
-$checksumfr = '7b0eecffbf08dcb674027ad065000a142387881903339d2b40bb1b58b3eaf477'
-$checksumjp = '022e9fc7a1e13ea3ab36061c30a6f227298f9578d70bfded6c4d930facef74cf'
-$checksum = 'fad4d8b9d481d55ef3e71a58be7b2a40790565eebf732a089be7b3c786b0f6f5'
+$checksumde = 'bedf5f567dc52f338a95598fd41a5ef1de5130180b8ec936b27a788bedbec3df'
+$checksumfr = '42e26935e97b6cddc5e2ca4e44a645a480dee016a9a339f1e9711a9e3a2f9327'
+$checksumjp = '6ce1bba2e03edc0dcd7f91825fb3b8c361addbff4aacf43d31728a71182c0454'
+$checksum = '366e78cb7ffd536fbc4a42dbecb094a41a008f30439c95e760710f0ec7b1f300'
 
 if ($german -contains $LCID)
 {
