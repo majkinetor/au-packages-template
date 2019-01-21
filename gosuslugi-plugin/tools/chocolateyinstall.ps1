f9528c439896a78b5cbd7185f43dfe22b9ca3f56
Chocolatey <chocolatey@realdimensions.net>
2019-01-21 13:47:16 +0000

    AU: 2 updated - gosuslugi-plugin spcomp [skip ci] https://gist.github.com/03903cd565061a825f153ce2f9d6227c/db617f4b202873acf5a0a03a62016c9e61fdddf6

--- gosuslugi-plugin/tools/chocolateyinstall.ps1
+++ gosuslugi-plugin/tools/chocolateyinstall.ps1
@@ -4,8 +4,8 @@
   SilentArgs             = '/qn /norestart'
   url                    = 'https://ds-plugin.gosuslugi.ru/plugin/upload/assets/distrib/IFCPlugin.msi'
   url64bit               = 'https://ds-plugin.gosuslugi.ru/plugin/upload/assets/distrib/IFCPlugin-x64.msi'
-  checksum               = 'b5384e5ae84a88205a020a59b97c1e4a7920b5cbca3954b383117483274f547a'
-  checksum64             = '751ca0dbc6d41f38edb7e1d839cf4382d6d8478f610e5241ec79d51f35210a95'
+  checksum               = '7bf743f5a9b59af46cde0740e89b2968e243da79f2b87dc830d5a5161e70e5fd'
+  checksum64             = 'ad45043d041d5d2f4f5424f709dc40744cd9d8f1929386e57b8971406b360b72'
   checksumType           = 'sha256'
   checksumType64         = 'sha256'
 }
--- spcomp/tools/chocolateyinstall.ps1
+++ spcomp/tools/chocolateyinstall.ps1
@@ -1,8 +1,8 @@
 ﻿$packageArgs = @{
   packageName            = "$env:chocolateyPackageName"
-  url                    = 'https://sm.alliedmods.net/smdrop/1.10/sourcemod-1.10.0-git6378-windows.zip'
+  url                    = 'https://sm.alliedmods.net/smdrop/1.10/sourcemod-1.10.0-git6380-windows.zip'
   UnzipLocation          = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
-  checksum               = '25d98bc987e9438354ff4a2d4cf44a88c60641e2b68022ac52a371ca986c03ee'
+  checksum               = '54c162a61c62e7311b607dc0666477e9559d60b8039cd68c2c569b087fa6d464'
   checksumType           = 'sha256'
   SpecificFolder         = "addons\sourcemod\scripting"
 }
