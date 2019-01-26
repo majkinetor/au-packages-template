9770879d21da036fb72838e6e12d5a6533e6d543
Chocolatey <chocolatey@realdimensions.net>
2019-01-26 14:18:41 +0000

    AU: 1 updated - eventghost [skip ci] https://gist.github.com/eae7c31972662d6ea5f024c6c89a5e90/9f41320194a42aa0e54a56ef2e7b5384c55569fe

--- eventghost/tools/chocolateyInstall.ps1
+++ eventghost/tools/chocolateyInstall.ps1
@@ -5,11 +5,11 @@ $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $toolsDir
   fileType      = 'exe'
-  url           = 'https://github.com/EventGhost/EventGhost/releases/download/v0.5.0-rc4/EventGhost_0.5.0-rc4_Setup.exe'
+  url           = 'https://github.com/EventGhost/EventGhost/releases/download/v0.5.0-rc6/EventGhost_0.5.0-rc6_Setup.exe'
 
   softwareName  = 'eventghost*'
 
-  checksum      = '17d41633cb999cb163c83bce1f39ae27866116660dfd026351362c830a330872'
+  checksum      = '5f47157b1af5b6ffbb3927ec3c2aa06181d945d6be09b1641e4e85036d840868'
   checksumType  = 'sha256'
 
   silentArgs    = "/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-"
