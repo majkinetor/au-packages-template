1ca7674ebd5f61288b7beb17f11ef14ac6dfd167
Marcio Cruz <40072316+CompactFactor@users.noreply.github.com>
2019-01-28 14:29:18 -0200

    Updated files

--- videostream/tools/chocolateyInstall.ps1
+++ videostream/tools/chocolateyInstall.ps1
@@ -13,6 +13,7 @@ $packageArgs = @{
   checksum      = '147d091f2e62278ade8f0d10adc838ab1713f40241449972849f70259e64f912'
   checksum64    = '07317ab6b720f7341452e476dd7d8ff423b6b764d94c1a1cf2f2ac8e179d9440'
   checksumType  = 'sha256'
+  checksumType64= 'sha256'
 
   silentArgs    = ""
   validExitCodes= @(0)
