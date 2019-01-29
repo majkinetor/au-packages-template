ec428fa320d7f686e4dd33a9e3eb230f8b61f3d1
Marcio Cruz <40072316+CompactFactor@users.noreply.github.com>
2019-01-29 19:12:55 -0200

    Add farr

---
+++ find-and-run-robot/tools/chocolateyInstall.ps1
@@ -0,0 +1,18 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+  fileType      = 'exe'
+  url           = 'https://www.donationcoder.com/Software/Mouser/findrun/downloads/FindAndRunRobotSetup.exe'
+
+  softwareName  = 'Find+Run Robot*'
+
+  checksum      = '9381ffe35b4ca5fd007cd626c24d0a851c1133cfd97b537932f48c2ab0e00131'
+  checksumType  = 'sha256'
+
+  silentArgs    = "/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-"
+  validExitCodes= @(0)
+}
+
+Install-ChocolateyPackage @packageArgs
---
+++ find-and-run-robot/update.ps1
@@ -0,0 +1,25 @@
+import-module au
+
+$releases = 'https://www.donationcoder.com/software/mouser/popular-apps/farr'
+
+function global:au_SearchReplace {
+   @{
+        ".\tools\chocolateyInstall.ps1" = @{
+            "(?i)(^\s*url\s*=\s*)('.*')"        = "`$1'$($Latest.URL32)'"
+            "(?i)(^\s*checksum\s*=\s*)('.*')"   = "`$1'$($Latest.Checksum32)'"
+        }
+    }
+}
+
+function global:au_GetLatest {
+    $download_page = Invoke-WebRequest -Uri $releases
+
+    $ob = $download_page.links | Where-Object href -like '*download*.exe'
+	$version = $ob.innerText.Replace(' Download v','')
+    @{
+        URL32   = "https://www.donationcoder.com/Software/Mouser/findrun/downloads/FindAndRunRobotSetup.exe"
+        Version = $version
+    }
+}
+
+update
