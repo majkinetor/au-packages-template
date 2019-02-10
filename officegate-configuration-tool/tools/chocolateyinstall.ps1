7a1cda64adcaa79b42a3d68ccf3c6af7d813466b
Chocolatey <chocolatey@realdimensions.net>
2019-02-10 15:46:50 +0000

    AU: 1 updated - officegate-configuration-tool [skip ci] https://gist.github.com/03903cd565061a825f153ce2f9d6227c/81f354085a75b97535a4d855a02691c878610f14

--- officegate-configuration-tool/tools/chocolateyinstall.ps1
+++ officegate-configuration-tool/tools/chocolateyinstall.ps1
@@ -2,7 +2,7 @@
   packageName            = "$env:chocolateyPackageName"
   url                    = 'https://teleofis.ru/php/download.php?f=1038'
   UnzipLocation          = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
-  checksum               = '7d9c4fef20941d38113171c00b1ee98eb8a383c20a5aeede77f6578200fc1aaf'
+  checksum               = '0cc5aa2c6c7d7b06f3674cbc55453f3fcb4449817311218c25c707e2beb5849e'
   checksumType           = 'sha256'
 }
 Install-ChocolateyZipPackage @packageArgs
