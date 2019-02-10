afe2334fb2199275f66c0b2439006d5306c2087d
Alexander Georgievskiy <galeksandrp@gmail.com>
2019-02-10 18:09:41 +0300

    [AU officegate-configuration-tool] Accept all cookies in IE to avoid parsing hangup
    IE is used internally by powershell curl for parsing. Website from which
    software updates gathered sets cookie, but IE does not accept some
    cookies by default. Change default behaviour in registry before calling AU.

--- officegate-configuration-tool/tools/chocolateyinstall.ps1
+++ officegate-configuration-tool/tools/chocolateyinstall.ps1
@@ -1,1 +1,8 @@
-﻿Install-ChocolateyZipPackage "$env:chocolateyPackageName" -Url 'https://teleofis.ru/php/download.php?f=1037' -UnzipLocation "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)" -Url64bit 'https://teleofis.ru/php/download.php?f=1038' -Checksum 'ebf15577b5e4268d1c0448092482730783601731c510289481c2f1dc295992ef' -ChecksumType 'sha256' -Checksum64 '7d9c4fef20941d38113171c00b1ee98eb8a383c20a5aeede77f6578200fc1aaf' -ChecksumType64 'sha256'
+﻿$packageArgs = @{
+  packageName            = "$env:chocolateyPackageName"
+  url                    = 'https://teleofis.ru/php/download.php?f=1038'
+  UnzipLocation          = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
+  checksum               = '7d9c4fef20941d38113171c00b1ee98eb8a383c20a5aeede77f6578200fc1aaf'
+  checksumType           = 'sha256'
+}
+Install-ChocolateyZipPackage @packageArgs
