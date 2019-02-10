effd38e1d766cd0476e53e5d5631ad83e6d94f53
Chocolatey <chocolatey@realdimensions.net>
2019-02-10 17:18:57 +0000

    AU: 1 updated - lesegais [skip ci] https://gist.github.com/03903cd565061a825f153ce2f9d6227c/93462ff53cc86644580c909c34b19d2d95cd8696

--- lesegais/tools/chocolateyinstall.ps1
+++ lesegais/tools/chocolateyinstall.ps1
@@ -2,7 +2,7 @@
   packageName            = "$env:chocolateyPackageName"
   FileType               = 'exe'
   SilentArgs             = '/S'
-  url                    = 'http://www.lesegais.ru/upload/EGAISCryptoSetup.exe'
+  url                    = 'https://lesegais.ru/upload/EGAISCryptoSetup.exe'
   checksum               = '96b3843571b8bf669852734656f262bc346c1b9c85e02735dbaa244e03d5306b'
   checksumType           = 'sha256'
 }
