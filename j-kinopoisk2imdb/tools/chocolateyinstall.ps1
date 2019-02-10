9b8aee77fe243d666e82e9a05ee090e71e784962
Alexander Georgievskiy <galeksandrp@gmail.com>
2019-02-10 14:27:54 +0300

    [AU j-kinopoisk2imdb] Init j-kinopoisk2imdb

---
+++ j-kinopoisk2imdb/tools/chocolateyinstall.ps1
@@ -0,0 +1,8 @@
+﻿$packageArgs = @{
+  packageName            = "$env:chocolateyPackageName"
+  url                    = 'https://github.com/REDNBLACK/J-Kinopoisk2IMDB/files/1990807/Kinopoisk2IMDB_1.2.exe.zip'
+  UnzipLocation          = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
+  checksum               = '2959ab5868333d1aaffb7413e205aeabed6cff7715e62e252447d6add1f12491'
+  checksumType           = 'sha256'
+}
+Install-ChocolateyZipPackage @packageArgs
