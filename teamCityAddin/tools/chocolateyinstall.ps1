a9017dd1f1939893bb5b3f959848e18aa803fe0e
flcdrg <david@gardiner.net.au>
2019-02-12 00:09:52 +0000

    AU: 7 updated - dotCover dotMemory dotPeek resharper-clt ReSharperCpp resharper-ultimate-all teamCityAddin [skip ci] https://gist.github.com/7d00c69c2cde8309a8594abaa897a9a6/008b8df8fa4ff90ca1066809da97d08efac29906

--- teamCityAddin/tools/chocolateyinstall.ps1
+++ teamCityAddin/tools/chocolateyinstall.ps1
@@ -2,7 +2,7 @@
 $packageName = 'teamCityAddin'
 $scriptPath = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
 $commonPath = $(Split-Path -parent $(Split-Path -parent $scriptPath))
-$filename = 'JetBrains.ReSharperUltimate.2018.3.2.exe'
+$filename = 'JetBrains.ReSharperUltimate.2018.3.3.exe'
 
 $installPath = Join-Path  (Join-Path $commonPath $platformPackageName) $filename
 $packageArgs = @{
