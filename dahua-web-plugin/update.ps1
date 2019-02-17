cc03a239c94a5ac41559f063164eda8e31e1d2e7
Alexander Georgievskiy <galeksandrp@gmail.com>
2019-02-17 11:19:21 +0300

    [AU 1c-barcode-printing litemanager-viewer litemanager-server dahua-web-plugin cuemounter.portable] Migrate dahua-web-plugin to AU Streams

--- dahua-web-plugin/update.ps1
+++ dahua-web-plugin/update.ps1
@@ -11,13 +11,21 @@ function global:au_SearchReplace {
 }
 
 function global:au_GetLatest {
-	$URL32 = 'http://4c11bfaa17c7.dahuaddns.com:7000/webplugin.exe'
-    Get-ChocolateyWebFile 'dahua-web-plugin' $URL32 -FileFullPath "$env:TMP\webplugin.exe"
-	
-	@{
-        URL32   = $URL32
-        Version = (Get-Item "$env:TMP\webplugin.exe").VersionInfo.ProductVersion.trim()
+    $versions = [ordered]@{}
+
+    (curl https://dahuawiki.com/Live_Demo).Links.href -match '\.dahuaddns\.com' | % {
+        $URL32 = "$_/webplugin.exe"
+        Get-ChocolateyWebFile 'dahua-web-plugin' $URL32 -FileFullPath "$env:TMP\webplugin.exe"
+
+        $version = (Get-Item "$env:TMP\webplugin.exe").VersionInfo.ProductVersion.trim()
+
+        $versions[$version] = @{
+            URL32   = $URL32
+            Version = $version
+        } 
     }
+
+    @{Streams = $versions}
 }
 
 update
