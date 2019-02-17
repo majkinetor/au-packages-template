b8729bcd2a0c067182dd6b7a37e3cf676845978a
Marcio Cruz <40072316+CruzMarcio@users.noreply.github.com>
2019-02-16 23:04:57 -0200

    [PUSH ksp-ckan]

--- ksp-ckan/tools/chocolateyInstall.ps1
+++ ksp-ckan/tools/chocolateyInstall.ps1
@@ -1,19 +1,21 @@
 ﻿$ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$exePath = Join-Path $toolsDir "ckan.exe"
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
-  unzipLocation = $toolsDir
-  fileType      = 'exe'
+  fileFullPath  = $exePath
   url           = 'https://github.com/KSP-CKAN/CKAN/releases/download/v1.25.4/ckan.exe'
-
-  softwareName  = 'eventghost*'
 
   checksum      = 'e992982615c6afcfe5f52d9872df22f4a2fadb3bf97c37931393ea767e7c3ffc'
   checksumType  = 'sha256'
-
-  silentArgs    = "/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-"
-  validExitCodes= @(0)
 }
 
-Install-ChocolateyPackage @packageArgs
+
+Get-ChocolateyWebFile @packageArgs
+
+$desktop = $([System.Environment]::GetFolderPath([System.Environment+SpecialFolder]::DesktopDirectory))
+$link = Join-Path $desktop "CKAN.lnk"
+if (!(Test-Path $link)) {
+    Install-ChocolateyShortcut -ShortcutFilePath "$link" -TargetPath $exePath -WorkingDirectory "$toolsDir"
+}
---
+++ ksp-ckan/tools/chocolateyUninstall.ps1
@@ -0,0 +1,9 @@
+﻿$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$exePath = Join-Path $toolsDir "ckan.exe"
+Get-Process ckan -ErrorAction SilentlyContinue | Where-Object {$_.Path -eq $exePath }  | Stop-Process -PassThru
+
+$desktop = $([System.Environment]::GetFolderPath([System.Environment+SpecialFolder]::DesktopDirectory))
+$link = Join-Path $desktop "CKAN.lnk"
+If (Test-Path $link) {
+    Remove-Item "$link"
+}
