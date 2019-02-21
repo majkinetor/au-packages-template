795210dec1f087f778b9082415e82cc3a5d24029
flcdrg <david@gardiner.net.au>
2019-02-21 00:09:21 +0000

    AU: 1 updated - x-lite [skip ci] https://gist.github.com/7d00c69c2cde8309a8594abaa897a9a6/59a0302bfd84e0f0e4816a20dfb8eb285dcccb64

--- x-lite/tools/chocolateyInstall.ps1
+++ x-lite/tools/chocolateyInstall.ps1
@@ -5,10 +5,10 @@ $packageName = 'x-lite'
 $installerType = 'exe'
 
 $silentArgs = '-s'
-$url = 'https://counterpath.s3.amazonaws.com/downloads/X-Lite_5.4.0_94388.exe'
+$url = 'https://counterpath.s3.amazonaws.com/downloads/X-Lite_5.5.0_97566.exe'
 $validExitCodes = @(0)
 
-$checksum = '4e69c2b09187f1f41d8f265e44d42a74ea3ab38584ad023af0b90b3848128a14'
+$checksum = '26a265eef9ea8d56a097b30f812762cd76d60d5c81f450dfcbe7fee1fea73f3f'
 $checksumType = 'sha256'
 
 $ahkScript = "$toolsDir\install.ahk"
