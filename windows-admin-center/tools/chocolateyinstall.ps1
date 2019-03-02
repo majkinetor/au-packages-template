ed960025be79874d46884982775750140c0d9d65
David Gardiner <david@gardiner.net.au>
2019-03-03 08:48:42 +1030

    Latest version of windows-admin-center.nuspec

--- windows-admin-center/tools/chocolateyinstall.ps1
+++ windows-admin-center/tools/chocolateyinstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop';
 $toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url = 'https://download.microsoft.com/download/1/0/5/1059800B-F375-451C-B37E-758FFC7C8C8B/WindowsAdminCenter1809.5.msi'
-$checksum = 'F37D6123170F2BD78EF8B0FC5458000EA4E513CBD52208E5D66F517E00321DFA'
+$url = 'https://download.microsoft.com/download/1/0/5/1059800B-F375-451C-B37E-758FFC7C8C8B/WindowsAdminCenter1809.51.msi'
+$checksum = 'F549D413F7E8102B01FB4B0C9E2658B86316152C9710473BBB951D3D84F05A17'
 
 $pp = Get-PackageParameters
 
