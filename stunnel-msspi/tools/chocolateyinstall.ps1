6f0efb4fecb38eae7dcf3c641e5a0b0a8a1f45c4
Chocolatey <chocolatey@realdimensions.net>
2019-03-08 14:08:02 +0000

    AU: 1 updated - stunnel-msspi [skip ci] https://gist.github.com/03903cd565061a825f153ce2f9d6227c/f7d81af2441ee032c375ce7d9d5a51003a45b8a6

--- stunnel-msspi/tools/chocolateyinstall.ps1
+++ stunnel-msspi/tools/chocolateyinstall.ps1
@@ -1,10 +1,10 @@
 ﻿$packageArgs = @{
   packageName            = "$env:chocolateyPackageName"
-  url                    = 'https://github.com/deemru/stunnel/releases/download/stunnel-5.50-msspi-0.135/stunnel-5.50-msspi-0.135_windows-386.zip'
-  url64bit               = 'https://github.com/deemru/stunnel/releases/download/stunnel-5.50-msspi-0.135/stunnel-5.50-msspi-0.135_windows-amd64.zip'
+  url                    = 'https://github.com/deemru/stunnel/releases/download/stunnel-5.50-msspi-0.136/stunnel-5.50-msspi-0.136_windows-386.zip'
+  url64bit               = 'https://github.com/deemru/stunnel/releases/download/stunnel-5.50-msspi-0.136/stunnel-5.50-msspi-0.136_windows-amd64.zip'
   UnzipLocation          = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
-  checksum               = '11176616f0f3239b0d556e503784734216201c217edb2e3cbc502ae515bdc406'
-  checksum64             = '7285b5a3af6eeeefca41beed4219569f9086be7b4c98aaef3823481eac4850ce'
+  checksum               = 'd09eef51443d563bb2657b12697481fd215b92c375f5a75cf45d7ff278974720'
+  checksum64             = '69c453e2845dabb801f63780a0592f80f3a7621d46fc879b168e80fe88cc3357'
   checksumType           = 'sha256'
   checksumType64         = 'sha256'
 }
