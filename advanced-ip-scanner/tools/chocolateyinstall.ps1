fe1522417a10bfaf7e494e07f9b3102df525466c
Chocolatey <chocolatey@realdimensions.net>
2019-03-29 14:20:37 +0000

    AU: 2 updated - advanced-ip-scanner traefik [skip ci] https://gist.github.com/03903cd565061a825f153ce2f9d6227c/ae1f57f870b961910e2edf03fc8ee8c1072c1a33

--- advanced-ip-scanner/tools/chocolateyinstall.ps1
+++ advanced-ip-scanner/tools/chocolateyinstall.ps1
@@ -2,8 +2,8 @@
   packageName            = "$env:chocolateyPackageName"
   FileType               = 'exe'
   SilentArgs             = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
-  url                    = 'https://www.advanced-ip-scanner.com/download/Advanced_IP_Scanner_2.5.3784.exe'
-  checksum               = '1976e556909dd8d8c3b901965333b171ad5986593c83ff4b061814126de49a82'
+  url                    = 'https://www.advanced-ip-scanner.com/download/Advanced_IP_Scanner_2.5.3850.exe'
+  checksum               = '87bfb05057f215659cc801750118900145f8a22fa93ac4c6e1bfd81aa98b0a55'
   checksumType           = 'sha256'
 }
 Install-ChocolateyPackage @packageArgs
--- traefik/tools/chocolateyinstall.ps1
+++ traefik/tools/chocolateyinstall.ps1
@@ -1,10 +1,10 @@
 ﻿$packageArgs = @{
   packageName            = "$env:chocolateyPackageName"
-  url                    = 'https://github.com/containous/traefik/releases/download/v1.7.9/traefik_windows-386.exe'
-  url64bit               = 'https://github.com/containous/traefik/releases/download/v1.7.9/traefik_windows-amd64.exe'
+  url                    = 'https://github.com/containous/traefik/releases/download/v1.7.10/traefik_windows-386.exe'
+  url64bit               = 'https://github.com/containous/traefik/releases/download/v1.7.10/traefik_windows-amd64.exe'
   FileFullPath           = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)\traefik.exe"
-  checksum               = '12d2e0288f3c29d5c6d42e6ac541421e7cd09a9fe4c61a3866b5a876f9ac0371'
-  checksum64             = '88a8f2c7b0032a2edbf896153f9bbae4929872abb7fb51b9ffd0b136ce38a102'
+  checksum               = '1b767b1766c1e84a4adf426a739bd9c529defc531023f96d98b34185af8a54c5'
+  checksum64             = '3918aad1f59d7ff84e12dc5d5db57314fa5569cc5261ad36aba0a91e6ff42d85'
   checksumType           = 'sha256'
   checksumType64         = 'sha256'
 }
