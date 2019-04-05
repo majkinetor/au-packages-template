fd8b4bb95148c39e419e6301c269a3927ed41aaf
Chocolatey <chocolatey@realdimensions.net>
2019-04-05 14:24:06 +0000

    AU: 2 updated - cspclean grafana [skip ci] https://gist.github.com/03903cd565061a825f153ce2f9d6227c/353b2f9173dbab4ed7cae02aa4048dd0092f64b9

--- cspclean/tools/chocolateyinstall.ps1
+++ cspclean/tools/chocolateyinstall.ps1
@@ -2,7 +2,7 @@
   packageName            = "$env:chocolateyPackageName"
   FileFullPath           = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)\cspclean.exe"
   url                    = 'https://www.cryptopro.ru/sites/default/files/public/cspclean.exe'
-  checksum               = '2a9d80165e95ba52ad93c4baee282bcb2c6770419484927905c00b47d3a9e72b'
+  checksum               = '9671ef97597e1f6cd68126a046c7a56d6b1d81042f2a779a44a4a01c73c5b9e8'
   checksumType           = 'sha256'
 }
 Get-ChocolateyWebFile @packageArgs
--- grafana/tools/chocolateyinstall.ps1
+++ grafana/tools/chocolateyinstall.ps1
@@ -1,8 +1,8 @@
 ﻿$packageArgs = @{
   packageName            = "$env:chocolateyPackageName"
-  url                    = 'https://s3-us-west-2.amazonaws.com/grafana-releases/release/grafana-6.1.0.windows-amd64.zip'
+  url                    = 'https://s3-us-west-2.amazonaws.com/grafana-releases/release/grafana-6.1.1.windows-amd64.zip'
   UnzipLocation          = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
-  checksum               = '66e844d874e2ef76d46e3f5c778f7e905d83720b2ad80596a801ccb85c6162d8'
+  checksum               = 'b39845d807f238c5480aaa6e9b751b879c86ba6f0076940fd29e6c2ee166d4ba'
   checksumType           = 'sha256'
 }
 Install-ChocolateyZipPackage @packageArgs
