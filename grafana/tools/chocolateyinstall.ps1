7a28bc617a3f9f97a92c10132fca9eef6a02ed89
Chocolatey <chocolatey@realdimensions.net>
2019-04-16 14:31:45 +0000

    AU: 2 updated - grafana teamviewer-qs [skip ci] https://gist.github.com/03903cd565061a825f153ce2f9d6227c/4cbd8cf1ed2d1277577e6c208d217ac3041fd477

--- grafana/tools/chocolateyinstall.ps1
+++ grafana/tools/chocolateyinstall.ps1
@@ -1,8 +1,8 @@
 ﻿$packageArgs = @{
   packageName            = "$env:chocolateyPackageName"
-  url                    = 'https://s3-us-west-2.amazonaws.com/grafana-releases/release/grafana-6.1.3.windows-amd64.zip'
+  url                    = 'https://s3-us-west-2.amazonaws.com/grafana-releases/release/grafana-6.1.4.windows-amd64.zip'
   UnzipLocation          = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
-  checksum               = 'c87f645204e14365771f8450de2583a71dbf13a056e0d32afbf2f12624ef49bb'
+  checksum               = 'e886227edbab9e7a7299857d1760c8e45998a2775bc220b5dc4435e72ae53832'
   checksumType           = 'sha256'
 }
 Install-ChocolateyZipPackage @packageArgs
--- teamviewer-qs/tools/chocolateyinstall.ps1
+++ teamviewer-qs/tools/chocolateyinstall.ps1
@@ -2,7 +2,7 @@
   packageName            = "$env:chocolateyPackageName"
   FileFullPath           = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)\TeamViewerQS.exe"
   url                    = 'https://download.teamviewer.com/download/TeamViewerQS.exe'
-  checksum               = 'e54ae8ac2174cb33fd5457027961bde16951d8ad48db48090e75bdf709759d33'
+  checksum               = 'dbac2232d8f2be82962c17e11a36b1e30b4e459235dbd0a43353c8b47981a365'
   checksumType           = 'sha256'
 }
 Get-ChocolateyWebFile @packageArgs
