b4a819cfce3a1ae81d2966663e782a3a07c2aa32
David Gardiner <david@gardiner.net.au>
2019-04-29 22:41:51 +0930

    Automate keepass-rpc

--- keepass-rpc/tools/chocolateyinstall.ps1
+++ keepass-rpc/tools/chocolateyinstall.ps1
@@ -1,5 +1,8 @@
 ﻿$ErrorActionPreference = 'Stop';
 
+$url           = 'https://github.com/kee-org/keepassrpc/releases/download/v1.9.0/KeePassRPC.plgx'
+$checksum      = '4b24fc8e47ca70989a7d948397a4707913484027635ad5f8be7f96794cfe0309'
+
 if (Test-Path env:"ProgramFiles(x86)")
 {
   $fileFullPath = ${env:ProgramFiles(x86)}
@@ -11,8 +14,8 @@ $fileFullPath = [IO.Path]::Combine($fileFullPath, "KeePass Password Safe 2\Plugi
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
-  url           = "https://github.com/kee-org/keepassrpc/releases/download/v1.8.0/KeePassRPC.plgx"
-  checksum      = '8D9D5E390FC4A3B8D6D8F24DD26A712DC032C4FF49708C8EC32C95A2E27594B5'
+  url           = $url
+  checksum      = $checksum
   checksumType  = 'sha256'
   FileFullPath   = $fileFullPath
 }
---
+++ keepass-rpc/update.ps1
@@ -0,0 +1,50 @@
+import-module au
+
+function global:au_SearchReplace {
+    @{
+        'tools\chocolateyInstall.ps1' = @{
+            "(^\`$url\s*=\s*)('.*')"    = "`$1'$($Latest.Url32)'"
+            "(^\`$checksum\s*=\s*)('.*')"   = "`$1'$($Latest.Checksum32)'"
+        }
+    }
+}
+
+function global:au_GetLatest {
+
+    $token = $env:github_api_key
+    $headers = @{
+        'User-Agent' = 'flcdrg'
+    }
+    if ($token) {
+        $headers['Authorization'] = ("token {0}" -f $token)
+    }
+    $response = Invoke-RestMethod -Method Get -Uri "https://api.github.com/repos/kee-org/keepassrpc/releases/latest" -Headers $headers
+    
+    $version = $response.name
+
+    $assets = $response.assets
+
+    $Latest = @{
+        Url32 = $assets[0].browser_download_url
+        Version = $version
+        ReleaseNotes = $response.body
+    }
+    return $Latest
+}
+
+function global:au_BeforeUpdate() {
+    $Latest.Checksum32 = Get-RemoteChecksum $Latest.Url32
+ }
+
+function global:au_AfterUpdate
+{ 
+    $nuspecFileName = $Latest.PackageName + ".nuspec"
+    $nu = Get-Content $nuspecFileName -Raw -Encoding UTF8
+    $nu = $nu -replace "(?smi)(\<releaseNotes\>).*?(\</releaseNotes\>)", "`${1}$($Latest.ReleaseNotes)`$2"
+    
+    $Utf8NoBomEncoding = New-Object System.Text.UTF8Encoding($False)
+    $NuPath = (Resolve-Path $NuspecFileName)
+    [System.IO.File]::WriteAllText($NuPath, $nu, $Utf8NoBomEncoding)
+}
+
+update -ChecksumFor none
