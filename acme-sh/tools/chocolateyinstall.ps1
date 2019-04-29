8d032208cea904267ee655e9f5455279542edb95
Chocolatey <chocolatey@realdimensions.net>
2019-04-29 14:38:13 +0000

    AU: 2 updated - acme-sh grafana [skip ci] https://gist.github.com/03903cd565061a825f153ce2f9d6227c/e87b1131e90ea699d4f00ccd9e6b9a1a3333e6aa

--- acme-sh/tools/chocolateyinstall.ps1
+++ acme-sh/tools/chocolateyinstall.ps1
@@ -1,8 +1,8 @@
 ﻿$packageArgs = @{
   packageName            = "$env:chocolateyPackageName"
-  url                    = 'https://github.com/Neilpang/acme.sh/archive/2.8.0.zip'
+  url                    = 'https://github.com/Neilpang/acme.sh/archive/2.8.1.zip'
   UnzipLocation          = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
-  checksum               = 'ec4c49b5ad164a36477bbb040716ff645870b840d219f32766f5b38dc12af360'
+  checksum               = '1a70fe16edc6c51c0316c29c97c20efbdc218c77ec5acb70f2fc05f87ef2d431'
   checksumType           = 'sha256'
 }
 Install-ChocolateyZipPackage @packageArgs
--- grafana/tools/chocolateyinstall.ps1
+++ grafana/tools/chocolateyinstall.ps1
@@ -1,8 +1,8 @@
 ﻿$packageArgs = @{
   packageName            = "$env:chocolateyPackageName"
-  url                    = 'https://s3-us-west-2.amazonaws.com/grafana-releases/release/grafana-6.1.4.windows-amd64.zip'
+  url                    = 'https://s3-us-west-2.amazonaws.com/grafana-releases/release/grafana-6.1.6.windows-amd64.zip'
   UnzipLocation          = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
-  checksum               = 'e886227edbab9e7a7299857d1760c8e45998a2775bc220b5dc4435e72ae53832'
+  checksum               = '178e6fb92dfb560fa4f2871bccb93a37283a35cc8ce40a3b6962ccb92e645f33'
   checksumType           = 'sha256'
 }
 Install-ChocolateyZipPackage @packageArgs
