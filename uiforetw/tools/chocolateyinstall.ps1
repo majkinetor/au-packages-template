7082aba7a570894b8cc923ccc7a02ea44653925d
Chocolatey <chocolatey@realdimensions.net>
2019-05-18 14:49:02 +0000

    AU: 1 updated - uiforetw [skip ci] https://gist.github.com/03903cd565061a825f153ce2f9d6227c/bfce7e3c4e67d44070a4a2ebf9670acc537a6b51

--- uiforetw/tools/chocolateyinstall.ps1
+++ uiforetw/tools/chocolateyinstall.ps1
@@ -1,8 +1,8 @@
 ﻿$packageArgs = @{
   packageName            = "$env:chocolateyPackageName"
-  url                    = 'https://github.com/google/UIforETW/releases/download/v1.50/etwpackage1.50.zip'
+  url                    = 'https://github.com/google/UIforETW/releases/download/v1.51/etwpackage1.51.zip'
   UnzipLocation          = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
-  checksum               = '6b15249203d94bc8c30f18e8dcdc521358f4f0a886bab040172027a332097e6e'
+  checksum               = '2cf7b3d008daafa1191db4b9bb49e6ce8b1c7a968badd1d8e9a64349d9b0b8fc'
   checksumType           = 'sha256'
 }
 Install-ChocolateyZipPackage @packageArgs
