6c4a9034a821f5cc378bd6723ed6948c72fdd774
David Gardiner <david@gardiner.net.au>
2019-05-26 08:08:28 +0930

    Migration TeamCity from https://github.com/skyguy94/chocolatey-packages

---
+++ teamcity/tools/chocolateyUninstall.ps1
@@ -0,0 +1,22 @@
+﻿$optionsFile = (Join-Path $PSScriptRoot 'options.xml')
+
+if (!(Test-Path $optionsFile)) {
+  throw 'Install options file missing. Could not uninstall.'
+}
+
+$options = Import-CliXml -Path $optionsFile
+
+$service = Get-Service | Where-Object Name -eq $options['serviceName']
+if ($service -ne $null) {
+  Stop-Service $service
+}
+
+$binPath = Join-Path $options['unzipLocation'] 'TeamCity\bin'
+if ((Test-Path $binPath) -and ($service -ne $null)) {
+  Push-Location $binPath
+  Start-ChocolateyProcessAsAdmin '.\teamcity-server.bat service delete'
+  Pop-Location
+}
+
+
+Remove-Item (Join-Path $options['unzipLocation'] 'TeamCity') -Recurse -Force
---
+++ teamcity/update.ps1
@@ -0,0 +1,46 @@
+import-module au
+
+function global:au_SearchReplace {
+    @{
+        'tools\chocolateyInstall.ps1' = @{
+            "(^[$]checksum\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
+        }
+        "$($Latest.PackageName).nuspec" = @{
+            "(\<releaseNotes\>).*?(\</releaseNotes\>)" = "`${1}$($Latest.ReleaseNotes)`$2"
+        }
+     }
+}
+
+function global:au_GetLatest {
+    $response = Invoke-RestMethod "https://www.jetbrains.com/teamcity/update.xml"
+
+    $latest = @{
+        Streams = [ordered] @{
+        }
+    }
+
+    $response.products.product | Where-Object { $_.code -eq "TC" } | Select-Object -ExpandProperty channel | Where-Object { $_.status -eq "release" } | ForEach-Object {
+
+        $version = $_.build.version
+        $buildNumber = $_.build.number
+        $releaseNotes = $_.build.link | Where-Object { $_.rel -eq "releaseNotes" } | Select-Object -ExpandProperty href
+
+        if (-not ($releaseNotes)) {
+            # If there wasn't a link in the payload (which appears to be the case for older releases), then make one up following this convention:
+            # https://confluence.jetbrains.com/display/TW/TeamCity+2018.2.1+(build+61078)+Release+Notes
+            $releaseNotes = "https://confluence.jetbrains.com/display/TW/TeamCity+$($version)+(build+$($buildNumber))+Release+Notes"
+        }
+
+        $latest.Streams.Add($_.id,
+            @{
+                Version = $version
+                Checksum32 = $_.build.distribution.sha256
+                ReleaseNotes = $releaseNotes
+            }
+        )
+    }
+    
+    $latest
+}
+
+update -ChecksumFor none
