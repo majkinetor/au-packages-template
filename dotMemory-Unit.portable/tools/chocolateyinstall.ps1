4e269e55ea55b8e64988d1cd89bda2c89215bee7
David Gardiner <david@gardiner.net.au>
2019-06-02 11:00:03 +0930

    Added dotmemory-unit
    Fixes #61

---
+++ dotMemory-Unit.portable/tools/chocolateyinstall.ps1
@@ -0,0 +1,23 @@
+﻿$ErrorActionPreference = 'Stop'; # stop on all errors
+
+$url = 'https://download.jetbrains.com/resharper/JetBrains.dotMemoryUnit.3.0.20171219.105559.zip'
+$checksum = '02b3463a99d922a431f5ecb3167d2e3473e73b086aed4cb7fb6a811ce5f50ace'
+
+$installPath  = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  url           = $url
+  UnzipLocation = $installPath
+  checksum      = $checksum
+  ChecksumType  = 'sha256'
+}
+
+Install-ChocolateyZipPackage @packageArgs
+
+# Exclude ancillory executables from shimming
+$files = Get-ChildItem $installPath -Recurse -Include *.exe -Exclude dotMemoryUnit.exe
+
+foreach ($file in $files) {
+  New-Item "$file.ignore" -type file -force | Out-Null
+}
