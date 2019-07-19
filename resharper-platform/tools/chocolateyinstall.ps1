23c23398c69ff41a6ce0f9b3a9943d692aab3d96
flcdrg <david@gardiner.net.au>
2019-07-19 00:08:07 +0000

    AU: 7 updated - dotCover dotMemory dotPeek ReSharper ReSharperCpp resharper-platform resharper-ultimate-all [skip ci] https://gist.github.com/7d00c69c2cde8309a8594abaa897a9a6/3ca8777cd2ff734d6aa5d26f94125596b0705a30

--- resharper-platform/tools/chocolateyinstall.ps1
+++ resharper-platform/tools/chocolateyinstall.ps1
@@ -1,10 +1,10 @@
 ﻿$ErrorActionPreference = 'Stop'; # stop on all errors
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-$filename = 'JetBrains.ReSharperUltimate.2019.1.3.exe'
-$checksum = '1ce8b815d12b6bdc4b9fcb648b97c0c6aea4733b3626fbea2a7b747986c7cb6e'
+$filename = 'JetBrains.ReSharperUltimate.2019.2.EAP4.Checked.exe'
+$checksum = '7601c9f0548bf4835faacc06e4296303e1d99adaebb0ae6615ef9dfbf9c2eab2'
 
-$url = 'https://download.jetbrains.com/resharper/ReSharperUltimate.2019.1.3/JetBrains.ReSharperUltimate.2019.1.3.exe'
+$url = 'https://download.jetbrains.com/resharper/ReSharperUltimate.2019.2.EAP4/JetBrains.ReSharperUltimate.2019.2.EAP4.Checked.exe'
 $packagePath = $(Split-Path -parent $toolsDir)
 $installPath = Join-Path $packagePath $filename
 
