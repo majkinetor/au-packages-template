09016f17b6cd5f05248745127860c36e630c2e03
Chocolatey <chocolatey@realdimensions.net>
2019-08-14 16:07:25 +0000

    AU: 1 updated - ksp-ckan [skip ci] https://gist.github.com/eae7c31972662d6ea5f024c6c89a5e90/0ed09171b451b652c0789188e2b997b65860c8c7

--- ksp-ckan/tools/chocolateyInstall.ps1
+++ ksp-ckan/tools/chocolateyInstall.ps1
@@ -5,9 +5,9 @@ $exePath = Join-Path $toolsDir "ckan.exe"
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   fileFullPath  = $exePath
-  url           = 'https://github.com/KSP-CKAN/CKAN/releases/download/v1.26.2/ckan.exe'
+  url           = 'https://github.com/KSP-CKAN/CKAN/releases/download/v1.26.4/ckan.exe'
 
-  checksum      = 'a0b4c43640d91ef5a0c3171906152636de75bf8497a1dc85d116a97c2c416185'
+  checksum      = '18e294ff3f0301a95d467d7ee62349c2a8c5fc38d8fa6a9fa3d37e8cc6c8bfd2'
   checksumType  = 'sha256'
 }
 
