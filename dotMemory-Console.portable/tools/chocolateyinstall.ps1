847d6c15d3a6f3b076af04d56bc7cb4096dcf192
David Gardiner <david@gardiner.net.au>
2019-08-22 14:09:58 +0930

    Add dotMemory Console

---
+++ dotMemory-Console.portable/tools/chocolateyinstall.ps1
@@ -0,0 +1,16 @@
+﻿$ErrorActionPreference = 'Stop'; # stop on all errors
+
+$url = 'https://download-cf.jetbrains.com/resharper/ReSharperUltimate.2019.2.1/JetBrains.dotMemoryConsole.2019.2.1.zip'
+$checksum = '8f5368cda7d48ef2b097a601622f76e9e3c9355f056299813f5cc2122d9f46f1'
+
+$installPath  = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  url           = $url
+  UnzipLocation = $installPath
+  checksum      = $checksum
+  ChecksumType  = 'sha256'
+}
+
+Install-ChocolateyZipPackage @packageArgs
