d1e7be91fbcff86f09a9ef7572aa94782214f035
Chocolatey <chocolatey@realdimensions.net>
2019-08-22 15:23:43 +0000

    AU: 1 updated - foldersizes [skip ci] https://gist.github.com/03903cd565061a825f153ce2f9d6227c/5d244c31b01ecc03b09e03baa6e658abdf92399a

--- foldersizes/tools/chocolateyinstall.ps1
+++ foldersizes/tools/chocolateyinstall.ps1
@@ -3,7 +3,7 @@
   FileType               = 'exe'
   SilentArgs             = '/qn'
   url                    = 'https://s3.amazonaws.com/keymetricsoft/FolderSizes/fs9-setup.exe'
-  checksum               = '1e44a07495043f45b8c3270317e00719ff0361a4bcb3f95fbba43df9a407c1b8'
+  checksum               = '15c1e219929c1ff6af165287e1384d4a881191b86bd0c847a6558706da68e5c1'
   checksumType           = 'sha256'
 }
 Install-ChocolateyPackage @packageArgs
