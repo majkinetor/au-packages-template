fc48f2cd6ac093e8a77d30a92621a50320717cd8
Chocolatey <chocolatey@realdimensions.net>
2019-08-29 15:28:50 +0000

    AU: 1 updated - nsq [skip ci] https://gist.github.com/03903cd565061a825f153ce2f9d6227c/8b771618e80b0e26d93771724a6e87923bbae630

--- nsq/tools/chocolateyinstall.ps1
+++ nsq/tools/chocolateyinstall.ps1
@@ -1,14 +1,14 @@
 ﻿$packageArgs = @{
   packageName            = "$env:chocolateyPackageName"
-  url                    = 'https://github.com/nsqio/nsq/releases/download/v1.1.0/nsq-1.1.0.windows-amd64.go1.10.3.tar.gz'
+  url                    = 'https://github.com/nsqio/nsq/releases/download/v1.2.0/nsq-1.2.0.windows-amd64.go1.12.9.tar.gz'
   UnzipLocation          = "$env:TMP"
-  checksum               = 'b309014ec223c13377b99e8f803c8e16b4311c872276f7ad1999a0769be3c89d'
+  checksum               = 'e7515ecadd742cfe45713f564dd21ee3b8f5aa8e17ae34f977ad0ecae81ad960'
   checksumType           = 'sha256'
 }
 Install-ChocolateyZipPackage @packageArgs
 
 $packageArgs = @{
-  FileFullPath           = "$env:TMP\nsq-1.1.0.windows-amd64.go1.10.3.tar"
+  FileFullPath           = "$env:TMP\nsq-1.2.0.windows-amd64.go1.12.9.tar"
   Destination            = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
 }
 Get-ChocolateyUnzip @packageArgs
