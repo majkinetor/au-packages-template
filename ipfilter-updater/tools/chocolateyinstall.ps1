70db3b96cdf9b934852e28710f803369cf23d284
Chocolatey <chocolatey@realdimensions.net>
2019-08-30 15:27:56 +0000

    AU: 2 updated - ipfilter-updater teamviewer-qs [skip ci] https://gist.github.com/03903cd565061a825f153ce2f9d6227c/43c1ef4f7222b4c1d68b955a061ba6c2722114f3

--- ipfilter-updater/tools/chocolateyinstall.ps1
+++ ipfilter-updater/tools/chocolateyinstall.ps1
@@ -2,8 +2,8 @@
   packageName            = "$env:chocolateyPackageName"
   FileType               = 'msi'
   SilentArgs             = "MSIFASTINSTALL=1 DISABLEROLLBACK=1 /qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
-  url                    = 'https://github.com/DavidMoore/ipfilter/releases/download/3.0.1.4-beta/IPFilter.msi'
-  checksum               = '918654d8f42e7168efe68cbae0e08f495eaab5b0298d31e77d4685797cd60d6e'
+  url                    = 'https://github.com/DavidMoore/ipfilter/releases/download/3.0.2.3-beta/IPFilter.msi'
+  checksum               = '279e8a5cd2512ba71d56b649fd2c834e0a73a0e218152bed378b80ac31932fbc'
   checksumType           = 'sha256'
 }
 Install-ChocolateyPackage @packageArgs
--- teamviewer-qs/tools/chocolateyinstall.ps1
+++ teamviewer-qs/tools/chocolateyinstall.ps1
@@ -2,7 +2,7 @@
   packageName            = "$env:chocolateyPackageName"
   FileFullPath           = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)\TeamViewerQS.exe"
   url                    = 'https://download.teamviewer.com/download/TeamViewerQS.exe'
-  checksum               = '03fb3ac4ea944e49791bb912bc5e7f736205220d270e975168c5dae534ef1f1c'
+  checksum               = '38d6a30b501371cc1dd755850e4489b163ea48e4d07c5d8248386b34f0e39d78'
   checksumType           = 'sha256'
 }
 Get-ChocolateyWebFile @packageArgs
