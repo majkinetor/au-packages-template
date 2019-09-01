5e7e491bc14d9c4c2f4137222186940393498555
Chocolatey <chocolatey@realdimensions.net>
2019-09-01 15:28:10 +0000

    AU: 1 updated - ipfilter-updater [skip ci] https://gist.github.com/03903cd565061a825f153ce2f9d6227c/91ef99b080c0b97547e155cc5c9df1329994a371

--- ipfilter-updater/tools/chocolateyinstall.ps1
+++ ipfilter-updater/tools/chocolateyinstall.ps1
@@ -2,8 +2,8 @@
   packageName            = "$env:chocolateyPackageName"
   FileType               = 'msi'
   SilentArgs             = "MSIFASTINSTALL=1 DISABLEROLLBACK=1 /qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
-  url                    = 'https://github.com/DavidMoore/ipfilter/releases/download/3.0.2.3-beta/IPFilter.msi'
-  checksum               = '279e8a5cd2512ba71d56b649fd2c834e0a73a0e218152bed378b80ac31932fbc'
+  url                    = 'https://github.com/DavidMoore/ipfilter/releases/download/3.0.2.4-beta/IPFilter.msi'
+  checksum               = '04912bbd35d80fc3bfec34ed03350f13ac163940ee121dca377b6ea1f856d62c'
   checksumType           = 'sha256'
 }
 Install-ChocolateyPackage @packageArgs
