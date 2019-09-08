e7d2022a7b77678fb2840e5ce70402d879c95865
Chocolatey <chocolatey@realdimensions.net>
2019-09-08 15:33:51 +0000

    AU: 1 updated - blockcheck [skip ci] https://gist.github.com/03903cd565061a825f153ce2f9d6227c/106816e560a87456998cbf401d836d0900c39ea9

--- blockcheck/tools/chocolateyinstall.ps1
+++ blockcheck/tools/chocolateyinstall.ps1
@@ -1,8 +1,8 @@
 ﻿$packageArgs = @{
   packageName            = "$env:chocolateyPackageName"
   FileFullPath                = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)\blockcheck.exe"
-  url                    = 'https://github.com/ValdikSS/blockcheck/releases/download/0.0.9.6/blockcheck-0.0.9.6-windows.exe'
-  checksum               = 'b5aa82ea803ccf7315ca53659b214aa9ed43aaf9a7d903bbcb77ac82811d0429'
+  url                    = 'https://github.com/ValdikSS/blockcheck/releases/download/0.0.9.7/blockcheck-0.0.9.7-windows.exe'
+  checksum               = '4292dcc59328cf70e16493b4e959e35d2cf9e732ac7922b6d031ff8aa53920a0'
   checksumType           = 'sha256'
 }
 Get-ChocolateyWebFile @packageArgs
