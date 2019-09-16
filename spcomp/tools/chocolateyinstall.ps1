91f382ab8e6bb955a33d32d8c914d543810ca37d
Chocolatey <chocolatey@realdimensions.net>
2019-09-16 15:38:49 +0000

    AU: 1 updated - spcomp [skip ci] https://gist.github.com/03903cd565061a825f153ce2f9d6227c/723f9acfb202eb2b92f018d42cf4a058adef5454

--- spcomp/tools/chocolateyinstall.ps1
+++ spcomp/tools/chocolateyinstall.ps1
@@ -1,8 +1,8 @@
 ﻿$packageArgs = @{
   packageName            = "$env:chocolateyPackageName"
-  url                    = 'https://sm.alliedmods.net/smdrop/1.10/sourcemod-1.10.0-git6439-windows.zip'
+  url                    = 'https://sm.alliedmods.net/smdrop/1.10/sourcemod-1.10.0-git6442-windows.zip'
   UnzipLocation          = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
-  checksum               = '1829c7e2472d4bd9469516719d1e2e50ea7b34a61aba6c4ffa9516e0ec9981b6'
+  checksum               = 'da3757ac74f3a9762023b9551f521773fb4c389da973d83b6843e08999aa60a1'
   checksumType           = 'sha256'
   SpecificFolder         = "addons\sourcemod\scripting"
 }
