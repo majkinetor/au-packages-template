302d6da53e6ae12673bb87c8b32f6468b276fee1
flcdrg <david@gardiner.net.au>
2019-09-24 00:06:39 +0000

    AU: 1 updated - pingplotter [skip ci] https://gist.github.com/7d00c69c2cde8309a8594abaa897a9a6/42f94366c71178349b1e0717cbe2578ac2b74a02

--- pingplotter/tools/chocolateyinstall.ps1
+++ pingplotter/tools/chocolateyinstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop';
 $toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-$checksum     = '751ac771db956c594abcc8bcad683b74a98a6cac11cfc2be2d6555f4556ac5d1'
+$checksum     = '60667ca0ae1295fc9932699cea0e121ff8d0286ffe3201ff36b6dc1b49d5ac76'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
