fd30eea4cfe86f1e02a25176c0d1e9f02d581f46
Chocolatey <chocolatey@realdimensions.net>
2019-09-29 15:47:39 +0000

    AU: 1 updated - acme-sh [skip ci] https://gist.github.com/03903cd565061a825f153ce2f9d6227c/3e48aa42472ad28a0a557297bdc6b8ad031d8acb

--- acme-sh/tools/chocolateyinstall.ps1
+++ acme-sh/tools/chocolateyinstall.ps1
@@ -1,8 +1,8 @@
 ﻿$packageArgs = @{
   packageName            = "$env:chocolateyPackageName"
-  url                    = 'https://github.com/Neilpang/acme.sh/archive/2.8.2.zip'
+  url                    = 'https://github.com/Neilpang/acme.sh/archive/2.8.3.zip'
   UnzipLocation          = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
-  checksum               = 'ce7d96975edd0f6d0a8e1cf0afbfdc5141c21cfddf7fd7f7a18dd40570adca7e'
+  checksum               = '471bd2e1d02fc968d8f5deb6f9c9704a5b3325247f0ae8f6933cf90a339e7825'
   checksumType           = 'sha256'
 }
 Install-ChocolateyZipPackage @packageArgs
