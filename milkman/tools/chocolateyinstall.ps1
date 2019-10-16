8a1a49c75303fbb07f8ddd8243275385e8afabec
Chocolatey <chocolatey@realdimensions.net>
2019-10-16 14:38:01 +0000

    Initial Milkman AU Package
    Added milkman
    AU: 1 updated - milkman
    [skip ci] https://gist.github.com/f445eca3c7e0a1daf1af7ca6bb0b3082/f003e13ae25ce8d5deb364fa6157c09513436b12

---
+++ milkman/tools/chocolateyinstall.ps1
@@ -0,0 +1,24 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$url64 = 'https://github.com/warmuuh/milkman/releases/download/3.6.0/milkman-dist-win64-bin.zip'
+$checksum64 = '258bf82c535b344f4fd8999ae80e8393c5eb764b185314581e28336288a080f6bc9d498c42085c7afeca48495e3d3977cf578bb80dfcf55f433ed69f3ad5546c'
+$checksumType64 = 'sha512'
+$UnzipLocation = Join-Path $env:ChocolateyInstall (Join-Path 'lib' $env:ChocolateyPackageName)
+Write-Output $UnzipLocation
+Install-ChocolateyZipPackage -PackageName $env:ChocolateyPackageName -Url $url64 -UnzipLocation $UnzipLocation -CheckSum $checksum64 -CheckSumType $checksumType64
+$ExecutableFiles = get-childitem $UnzipLocation -include *.exe -recurse
+
+foreach ($file in $ExecutableFiles) {
+  if($file -match '.*\.exe\.ignore$'){
+  #generate an ignore file
+  New-Item "$file.ignore" -type file -force | Out-Null}
+}
+
+$ExecutableFolder = Join-Path $UnzipLocation ("$($env:ChocolateyPackageName)-" + $env:ChocolateyPackageVersion)
+Rename-Item -Path $ExecutableFolder -NewName $env:ChocolateyPackageName
+$ExecutableFolder = Join-Path $UnzipLocation $env:ChocolateyPackageName
+
+$InstallLocation = Join-Path (Get-ToolsLocation) $env:ChocolateyPackageName
+Copy-Item -Path $ExecutableFolder -Destination $InstallLocation -Recurse
+Remove-Item -Path $ExecutableFolder -Recurse -Force
+Write-Host "Installed $($envChocolateyPackageName) to $($InstallLocation)"
---
+++ milkman/update.ps1
@@ -0,0 +1,24 @@
+﻿import-module au
+
+$releases = "https://github.com/warmuuh/milkman/releases"
+
+function global:au_SearchReplace {
+  @{
+    ".\tools\chocolateyInstall.ps1" = @{
+      "(^[$]url64\s*=\s*)('.*')"      = "`$1'$($Latest.URL)'"
+      "(^[$]checksum64\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
+      "(^[$]checksumType64\s*=\s*)('.*')" = "`$1'$($Latest.ChecksumType32)'"
+    }
+  }
+}
+
+function global:au_GetLatest {
+  $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
+  $regex   = '\/warmuuh\/milkman\/releases\/tag\/[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}$'
+  $url     = $download_page.links | Where-Object href -match $regex | Select-Object -First 1 -expand href
+  $version = $url -split '\/' | Select-Object -Last 1
+  $url = 'https://github.com/warmuuh/milkman/releases/download/' + $version + '/milkman-dist-win64-bin.zip'
+  return @{ Version = $version; URL = $url; ChecksumType32 = 'sha512';}
+}
+
+Update-Package -ChecksumFor 32
