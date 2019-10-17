dab1bc4bb995c618ae7d09d029f93d56f37ef698
Curtis Carter <lokiphasma@gmail.com>
2019-10-16 22:31:02 -0500

    Added Milkman-Sync-Git AU Package
    Added milkman-sync-git

---
+++ milkman-sync-git/tools/chocolateyinstall.ps1
@@ -0,0 +1,10 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$checksum64 = ''
+$checksumType64 = ''
+$JarFile = Get-ChildItem -Path (Join-Path $env:ChocolateyInstall (Join-Path 'lib' $env:ChocolateyPackageName)) -Filter "$env:ChocolateyPackageName.jar"
+Get-ChecksumValid -File $JarFile -CheckSum $checksum64 -CheckSumType $checksumType64
+$InstallLocation = (Join-Path (Join-Path (Join-Path (Get-ToolsLocation) "milkman") "plugins") "$env:ChocolateyPackageName.jar")
+Copy-Item -Path $JarFile -Destination $InstallLocation -Recurse
+Remove-Item -Path $JarFile -Force
+Write-Host "Installed $($envChocolateyPackageName) to $($InstallLocation)"
---
+++ milkman-sync-git/tools/chocolateyuninstall.ps1
@@ -0,0 +1,3 @@
+﻿$ErrorActionPreference = 'Stop';
+
+Remove-Item -Path (Join-Path (Join-Path (Join-Path (Get-ToolsLocation) "milkman") "plugins") "$env:ChocolateyPackageName.jar") -Force
