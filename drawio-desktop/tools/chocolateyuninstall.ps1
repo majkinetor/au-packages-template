3a61cb7f86c55a7c43d5d126f3cd67fed0e10a1e
Curtis Carter <lokiphasma@gmail.com>
2019-10-17 07:31:45 -0500

    Added draw.io Desktop AU Package
    Added drawio-desktop

---
+++ drawio-desktop/tools/chocolateyuninstall.ps1
@@ -0,0 +1,8 @@
+﻿# Should not be necessary unless using an old version of Chocolatey
+# Uninstall-ChocolateyPackage `
+#   -PackageName $env:ChocolateyPackageName `
+#   -FileType 'exe' `
+#   -SilentArgs '/S' `
+#   [-File <String>] `
+#   [-ValidExitCodes <Object>] `
+#   [-IgnoredArguments <Object[]>] [<CommonParameters>]
