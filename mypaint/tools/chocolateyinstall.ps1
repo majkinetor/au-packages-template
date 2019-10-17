00588790a406654a8eca91afa00310744a8eb982
Chocolatey <chocolatey@realdimensions.net>
2019-10-17 19:28:52 +0000

    AU: 1 updated - mypaint [skip ci] https://gist.github.com/e9202f68aebb2dd879dc46b458908ff3/e723c63d8d71b2b346775244a09b8df9c346423b

--- mypaint/tools/chocolateyinstall.ps1
+++ mypaint/tools/chocolateyinstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop';
 
-$url32 = ''
-$checksum32 = ''
+$url32 = 'https://github.com/mypaint/mypaint/releases/download/v2.0.0-alpha.12/mypaint-git-w64-2.0.0-alpha-master.502-installer.exe'
+$checksum32 = 'beaeff34990681d69091933734d813464daafe7a632aed5a86f002b491753841f9819f2f15d2abe3bc8bff4cd8d7890f85cb788ba2e4351a5c2366364613ff06'
 $checksumType32 = 'sha512'
 Install-ChocolateyPackage -PackageName $env:ChocolateyPackageName -FileType 'exe' -SilentArgs '/S' -Url $url32 -Checksum $checksum32 -ChecksumType $checksumType32
   # [-Url64bit <String>] `
