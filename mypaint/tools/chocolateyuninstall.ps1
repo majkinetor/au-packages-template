f5af277710667ad3fdddaa59c95d8dcd3c00092f
Curtis Carter <lokiphasma@gmail.com>
2019-10-17 12:45:14 -0500

    Added MyPaint AU Package
    Added mypaint

---
+++ mypaint/tools/chocolateyuninstall.ps1
@@ -0,0 +1,5 @@
+﻿# Should not be necessary unless using an old version of Chocolatey
+$file = "$Env:Programfiles\mypaint-git-w64\uninstall.exe"
+Uninstall-ChocolateyPackage -PackageName $env:ChocolateyPackageName -File $file -FileType 'exe' -SilentArgs '/S'
+#   [-ValidExitCodes <Object>] `
+#   [-IgnoredArguments <Object[]>] [<CommonParameters>]
---
+++ mypaint/update.ps1
@@ -0,0 +1,27 @@
+﻿import-module au
+
+$releases = "https://github.com/mypaint/mypaint/releases"
+
+function global:au_SearchReplace {
+  @{
+    ".\tools\chocolateyInstall.ps1" = @{
+      "(^[$]url32\s*=\s*)('.*')"      = "`$1'$($Latest.URL)'"
+      "(^[$]checksum32\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
+      "(^[$]checksumType32\s*=\s*)('.*')" = "`$1'$($Latest.ChecksumType32)'"
+    }
+  }
+}
+
+function global:au_GetLatest {
+  $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
+  $regex   = '\/mypaint\/mypaint\/releases\/download\/v\d{1,3}\.\d{1,3}\.\d{1,3}.*\/mypaint-git-w64-\d{1,3}\.\d{1,3}\.\d{1,3}.*-installer\.exe'
+  $url     = $download_page.links | Where-Object href -match $regex | Select-Object -First 1 -expand href
+  $version = $url -split '\/|v' | Select-Object -Last 1 -Skip 1
+  $url = "https://github.com$url"
+  while($version.LastIndexOf(".") -gt $version.LastIndexOf("-") -and $version.LastIndexOf("-") -ne '-1'){
+    $version = $version.remove($version.LastIndexOf("."),1).insert($version.LastIndexOf("."),"-") 
+  }
+  return @{ Version = $version; URL = $url; ChecksumType32 = 'sha512';}
+}
+
+Update-Package -ChecksumFor 32
