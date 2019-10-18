4a019087e3829301c8906aad28e2f07e0e80fa6f
Chocolatey <chocolatey@realdimensions.net>
2019-10-18 15:30:25 +0000

    AU: 1 updated - deno [skip ci] https://gist.github.com/b600269006e9056c8ca2f77dd8fa6000/4852217fdc9c561d3463f6d492ddb84100a24ec9

--- deno/tools/chocolateyinstall.ps1
+++ deno/tools/chocolateyinstall.ps1
@@ -1,8 +1,8 @@
 ﻿$ErrorActionPreference = 'Stop';
 
-$url64 = ''
-$checksum64 = ''
-$checksumType64 = ''
+$url64 = 'https://github.com/denoland/deno/releases/download/v0.20.0/deno_win_x64.zip'
+$checksum64 = '6a3c4c49c846633692e42306eb3b6e859cc726162247ed11043369a45e3d1451d3619eb4c8f1ea23453468d65e27298d09a02a135db1a1f7835d957da1e8e007'
+$checksumType64 = 'sha512'
 $UnzipLocation = Join-Path $env:ChocolateyInstall (Join-Path 'lib' $env:ChocolateyPackageName)
 Install-ChocolateyZipPackage -PackageName $env:ChocolateyPackageName -Url $url64 -UnzipLocation $UnzipLocation -CheckSum $checksum64 -CheckSumType $checksumType64
 Write-Output "Run 'deno --help' to get started"
