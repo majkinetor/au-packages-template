b76c40dd25278b6dce5a99c9cc5db003d6323ffa
Curtis Carter <lokiphasma@gmail.com>
2019-10-18 16:49:56 -0500

    Added Kind AU Package
    Added kind

---
+++ kind/tools/chocolateyinstall.ps1
@@ -0,0 +1,11 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$url64 = ''
+$checksum64 = ''
+$checksumType64 = 'sha512'
+$File = Join-Path (Join-Path $env:ChocolateyInstall (Join-Path 'lib' $env:ChocolateyPackageName)) 'kind.exe'
+$file = Get-ChocolateyWebFile -PackageName $env:ChocolateyPackageName `
+ -FileFullPath $File `
+ -Url64 $url64  `
+ -CheckSum64 $checksum64 `
+ -CheckSumType64 $checksumType64
---
+++ kind/update.ps1
@@ -0,0 +1,24 @@
+﻿import-module au
+
+$releases = "https://github.com/kubernetes-sigs/kind/releases"
+
+function global:au_SearchReplace {
+  @{
+    ".\tools\chocolateyInstall.ps1" = @{
+      "(^[$]url64\s*=\s*)('.*')"      = "`$1'$($Latest.URL64)'"
+      "(^[$]checksum64\s*=\s*)('.*')" = "`$1'$($Latest.Checksum64)'"
+      "(^[$]checksumType64\s*=\s*)('.*')" = "`$1'$($Latest.ChecksumType64)'"
+    }
+  }
+}
+
+function global:au_GetLatest {
+  $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
+  $regex   = '\/kubernetes-sigs\/kind\/releases\/tag\/v\d{1,3}\.\d{1,3}\.\d{1,3}$'
+  $url     = $download_page.links | Where-Object href -match $regex | Select-Object -First 1 -expand href
+  $version = $url -split '\/|v' | Select-Object -Last 1
+  $url = "https://github.com/kubernetes-sigs/kind/releases/download/v$version/kind-windows-amd64"
+  return @{ Version = $version; URL64 = $url; ChecksumType64 = 'sha512';}
+}
+
+Update-Package -ChecksumFor 64
