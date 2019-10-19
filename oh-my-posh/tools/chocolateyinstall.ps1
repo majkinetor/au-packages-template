cf2d50becf1a0f44d5f7ba3274075e7a114a957e
Curtis Carter <lokiphasma@gmail.com>
2019-10-18 22:04:30 -0500

    Added Oh-My-Posh AU Package
    Added oh-my-posh

---
+++ oh-my-posh/tools/chocolateyinstall.ps1
@@ -0,0 +1,14 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$version = ''
+Install-Script -RequiredVersion $version -Name 'oh-my-posh' -Scope 'AllUsers' -AcceptLicense -AllowPrerelease -Force
+if($PSVersionTable.PSVersion.Major -gt 5){
+    Install-Module -Name PSReadLine -AllowPrerelease -Scope CurrentUser -Force -SkipPublisherCheck
+}
+Set-Prompt
+
+if (!(Test-Path -Path $PROFILE )) { New-Item -Type File -Path $PROFILE -Force }
+Add-Content $PROFILE `
+"Import-Module posh-git
+Import-Module oh-my-posh
+Set-Theme Paradox"
---
+++ oh-my-posh/update.ps1
@@ -0,0 +1,21 @@
+﻿import-module au
+
+$releases = "https://github.com/kubernetes-sigs/kind/releases"
+
+function global:au_SearchReplace {
+  @{
+    ".\tools\chocolateyInstall.ps1" = @{
+      "(^[$]version\s*=\s*)('.*')"  = "`$1'$($Latest.Version)'"
+    }
+  }
+}
+
+function global:au_GetLatest {
+  $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
+  $regex   = '\/kubernetes-sigs\/kind\/releases\/tag\/\d{1,3}\.\d{1,3}\.\d{1,3}$'
+  $url     = $download_page.links | Where-Object href -match $regex | Select-Object -First 1 -expand href
+  $version = $url -split '\/' | Select-Object -Last 1
+  return @{ Version = $version; }
+}
+
+Update-Package -ChecksumFor none
