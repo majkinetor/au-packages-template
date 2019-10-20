4e6a868ba01a42745d86357bbad783b0b9f3d33c
Chocolatey <chocolatey@realdimensions.net>
2019-10-20 15:55:25 +0000

    AU: 2 updated - sdrsharp spcomp [skip ci] https://gist.github.com/03903cd565061a825f153ce2f9d6227c/d6e9474ca130b50a4f9dc7f4ae9c1a73a3158427

--- sdrsharp/tools/chocolateyinstall.ps1
+++ sdrsharp/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@
   url                    = 'https://airspy.com/downloads/sdrsharp-x86.zip'
   url64bit               = 'https://airspy.com/downloads/sdrsharp-x64.zip'
   UnzipLocation          = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
-  checksum               = '54334ed87e6ae15f7783df7dc4288af95192ca9fc4337a097963b002429b9a87'
-  checksum64             = '8a1a8b2da6881068d0e4f252853d4259788263099d06315234694868518ae52a'
+  checksum               = '57cf6a87b75b481f4a4c52a8cf105df72915676d94ace519029f850716e9d4cb'
+  checksum64             = 'ab492ba79df6ceb5d4543b4f4bee2823050298d104b6ca09e2b61f47fdb4ca47'
   checksumType           = 'sha256'
   checksumType64         = 'sha256'
 }
--- spcomp/tools/chocolateyinstall.ps1
+++ spcomp/tools/chocolateyinstall.ps1
@@ -1,8 +1,8 @@
 ﻿$packageArgs = @{
   packageName            = "$env:chocolateyPackageName"
-  url                    = 'https://sm.alliedmods.net/smdrop/1.10/sourcemod-1.10.0-git6452-windows.zip'
+  url                    = 'https://sm.alliedmods.net/smdrop/1.10/sourcemod-1.10.0-git6453-windows.zip'
   UnzipLocation          = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
-  checksum               = '147d0b276d728381578f67bdc92b4e825a349eec7484959ad7977d312fa4e97d'
+  checksum               = 'b3a817a61865a31763ff38f0c7e790b4d67578df10ce906a462d12165d257b8b'
   checksumType           = 'sha256'
   SpecificFolder         = "addons\sourcemod\scripting"
 }
