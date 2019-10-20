7eb44d48b52a2d5d23805c8b711b7b613913aa7a
Curtis Carter <lokiphasma@gmail.com>
2019-10-20 16:14:12 -0500

    Added Android Log Viewer AU Package
    Added android-log-viewer

---
+++ android-log-viewer/tools/chocolateyinstall.ps1
@@ -0,0 +1,14 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$url32 = ''
+$checksum32 = ''
+$checksumType32 = 'sha512'
+Install-ChocolateyPackage -PackageName $env:ChocolateyPackageName -FileType 'msi' -SilentArgs '/q' -Url $url32 -Checksum $checksum32 -ChecksumType $checksumType32
+  # [-Url64bit <String>] `
+  # [-ValidExitCodes <Object>] `
+  # [-Options <Hashtable>] `
+  # [-File <String>] `
+  # [-File64 <String>] `
+  # [-UseOnlyPackageSilentArguments] `
+  # [-UseOriginalLocation] `
+  # [-IgnoredArguments <Object[]>] [<CommonParameters>]<#  #>
---
+++ android-log-viewer/update.ps1
@@ -0,0 +1,25 @@
+﻿import-module au
+
+$releases = "https://github.com/MarkusPalcer/AndroidLogViewer/releases"
+
+function global:au_SearchReplace {
+  @{
+    ".\tools\chocolateyInstall.ps1" = @{
+      "(^[$]url32\s*=\s*)('.*')"      = "`$1'$($Latest.URL)'"
+      "(^[$]checksum32\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
+      "(^[$]checksumType32\s*=\s*)('.*')" = "`$1'$($Latest.ChecksumType32)'"
+    }
+  }
+}
+
+function global:au_GetLatest {
+  $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
+  $regex   = "\/MarkusPalcer\/AndroidLogViewer\/releases\/download\/v\d{1,3}\.\d{1,3}\.\d{1,3}\/Android.Log.Viewer.msi$"
+  $url     = $download_page.links | Where-Object href -match $regex | Select-Object -First 1 -expand href
+  $version = $url -split '\/' | Select-Object -Last 1 -Skip 1
+  $version = $version -split 'v' | Select-Object -Last 1
+  $url = "https://github.com/MarkusPalcer/AndroidLogViewer/releases/download/v$version/Android.Log.Viewer.msi"
+  return @{ Version = $version; URL = $url; ChecksumType32 = 'sha512';}
+}
+
+Update-Package -ChecksumFor 32
