70daba6a111bbbfa67231cd4cc96fac9866948ca
Curtis Carter <lokiphasma@gmail.com>
2019-10-20 16:02:57 -0500

    Added EM-n-EN AU Package
    Added em-n-en

---
+++ em-n-en/tools/chocolateyinstall.ps1
@@ -0,0 +1,15 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$url = 'https://github.com/CemraJC/em-n-en/releases/download/v1.0.0/Em-n-en.exe'
+$checksum = 'be07a49c497d712a912b67b1d41afb1ffe872718b42da83528d091eceee978f1'
+$checksumType = 'sha512'
+$File = Join-Path (Join-Path $env:ChocolateyInstall (Join-Path 'lib' $env:ChocolateyPackageName)) 'Em-n-en.exe'
+$file = Get-ChocolateyWebFile -PackageName $env:ChocolateyPackageName `
+ -FileFullPath $File `
+ -Url $url  `
+ -CheckSum $checksum `
+ -CheckSumType $checksumType
+Write-Host "1. Run $File
+2. Choose your options – there is one recommended by default
+3. Tell it to `"Start on Windows Startup`"
+4. Click `"Ok`""
---
+++ em-n-en/update.ps1
@@ -0,0 +1,24 @@
+﻿import-module au
+
+$releases = "https://github.com/CemraJC/em-n-en/releases/"
+
+function global:au_SearchReplace {
+  @{
+    ".\tools\chocolateyInstall.ps1" = @{
+      "(^[$]url\s*=\s*)('.*')"      = "`$1'$($Latest.URL)'"
+      "(^[$]checksum\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
+      "(^[$]checksumType\s*=\s*)('.*')" = "`$1'$($Latest.ChecksumType)'"
+    }
+  }
+}
+
+function global:au_GetLatest {
+  $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
+  $regex   = '\/CemraJC\/em-n-en\/releases\/download\/v\d{1,3}\.\d{1,3}\.\d{1,3}\/Em-n-en.exe$'
+  $url     = $download_page.links | Where-Object href -match $regex | Select-Object -First 1 -expand href
+  $version = $url -split '\/|v' | Select-Object -Last 1 -Skip 1
+  $url = "https://github.com/CemraJC/em-n-en/releases/download/v$version/Em-n-en.exe"
+  return @{ Version = $version; URL = $url; ChecksumType = 'sha512';}
+}
+
+Update-Package -ChecksumFor 32
