4ab1855a33ffa95203b170727dc4ed2b56267cb2
Chocolatey <chocolatey@realdimensions.net>
2019-10-20 21:16:49 +0000

    AU: 1 updated - android-log-viewer [skip ci] https://gist.github.com/162c382382f9a6c662cdb3ec28291665/940e27b41205d51ca4edbbdac2b19b5444ec16ec

--- android-log-viewer/tools/chocolateyinstall.ps1
+++ android-log-viewer/tools/chocolateyinstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop';
 
-$url32 = ''
-$checksum32 = ''
+$url32 = 'https://github.com/MarkusPalcer/AndroidLogViewer/releases/download/v1.2.2/Android.Log.Viewer.msi'
+$checksum32 = '784feaeeb418b06ed28bc5ee24daf220afb1c99830011ba70a28d435a33a5f8bb8fedcec46c3862d9a6eb9365c8b933ef4f885f3a6ebba8540a7349d4bea3e42'
 $checksumType32 = 'sha512'
 Install-ChocolateyPackage -PackageName $env:ChocolateyPackageName -FileType 'msi' -SilentArgs '/q' -Url $url32 -Checksum $checksum32 -ChecksumType $checksumType32
   # [-Url64bit <String>] `
