5276417aad33cfe33331a1a70e78240e5d6f6329
Curtis Carter <lokiphasma@gmail.com>
2019-10-21 07:38:05 -0500

    Correction to Kind 64-bit URL
    It looks like Kind was incorrectly committed with url64 instead of url64bit.

--- kind/tools/chocolateyinstall.ps1
+++ kind/tools/chocolateyinstall.ps1
@@ -6,6 +6,6 @@ $checksumType64 = 'sha512'
 $File = Join-Path (Join-Path $env:ChocolateyInstall (Join-Path 'lib' $env:ChocolateyPackageName)) 'kind.exe'
 $file = Get-ChocolateyWebFile -PackageName $env:ChocolateyPackageName `
  -FileFullPath $File `
- -Url64 $url64  `
+ -Url64bit $url64  `
  -CheckSum64 $checksum64 `
  -CheckSumType64 $checksumType64
