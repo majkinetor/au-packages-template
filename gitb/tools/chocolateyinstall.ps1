695ba426aefdec8338f3f536754e19f9a4ec0b1c
Curtis Carter <lokiphasma@gmail.com>
2019-10-24 14:15:57 -0500

    Added gitb AU Package
    Added gitb

---
+++ gitb/tools/chocolateyinstall.ps1
@@ -0,0 +1,23 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$url64 = ''
+$url32 = ''
+$checksum64 = ''
+$checksum32 = ''
+$checksumType64 = 'sha512'
+$checksumType32 = 'sha512'
+$UnzipLocation = Join-Path $env:ChocolateyInstall (Join-Path 'lib' $env:ChocolateyPackageName)
+Install-ChocolateyZipPackage -PackageName $env:ChocolateyPackageName `
+-Url $url32 `
+-Url64 $url64 `
+-UnzipLocation $UnzipLocation `
+-CheckSum $checksum32 `
+-CheckSum64 $checksum64 `
+-CheckSumType $checksumType32 `
+-CheckSumType64 $checksumType64 
+
+$tempUnzipfolder = Get-ChildItem -Path $UnzipLocation -Directory -Filter 'gitb*'
+$tempUnzipfolder = Join-Path $UnzipLocation $tempUnzipfolder
+Copy-Item -Path "$tempUnzipfolder\*" -Destination $UnzipLocation -Force
+Remove-Item -Path $tempUnzipfolder -Force -Recurse
+Rename-Item "$(Join-Path $UnzipLocation 'gitb')" -NewName 'gitb.exe'
---
+++ gitb/update.ps1
@@ -0,0 +1,33 @@
+﻿import-module au
+
+$releases = "https://github.com/vvatanabe/gitb/releases"
+
+function global:au_SearchReplace {
+  @{
+    ".\tools\chocolateyInstall.ps1" = @{
+      "(^[$]url32\s*=\s*)('.*')"      = "`$1'$($Latest.URL32)'"
+      "(^[$]url64\s*=\s*)('.*')"      = "`$1'$($Latest.URL64)'"
+      "(^[$]checksum32\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
+      "(^[$]checksum64\s*=\s*)('.*')" = "`$1'$($Latest.Checksum64)'"
+      "(^[$]checksumType32\s*=\s*)('.*')" = "`$1'$($Latest.ChecksumType32)'"
+      "(^[$]checksumType64\s*=\s*)('.*')" = "`$1'$($Latest.ChecksumType64)'"
+    }
+    "$($Latest.PackageName).nuspec" = @{
+      "(\<releaseNotes\>\[Release Notes\]\().*?(\)\</releaseNotes\>)" = "`${1}$($Latest.ReleaseNotes)`$2"
+  }
+  }
+}
+
+function global:au_GetLatest {
+  $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
+  $regex   = '\/vvatanabe\/gitb\/releases\/tag\/\d{1,3}\.\d{1,3}\.\d{1,3}$'
+  $url     = $download_page.links | Where-Object href -match $regex | Select-Object -First 1 -expand href
+  Write-Host $url
+  $version = $url -split '\/' | Select-Object -Last 1
+  $url = "https://github.com/vvatanabe/gitb/releases/download/$version/gitb_$($version)_windows_386.zip"
+  $url64 = "https://github.com/vvatanabe/gitb/releases/download/$version/gitb_$($version)_windows_amd64.zip"
+  $releaseNotes = "https://github.com/vvatanabe/gitb/releases/tag/$version"
+  return @{ Version = $version; URL32 = $url; ChecksumType32 = 'sha512'; URL64 = $url64; ChecksumType64 = 'sha512'; ReleaseNotes = $releaseNotes;}
+}
+
+Update-Package -ChecksumFor all
