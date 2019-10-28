14a5c0b89b25558fa5183dfeb595a9f9c5383db5
Curtis Carter <lokiphasma@gmail.com>
2019-10-28 11:43:48 -0500

    Added Features Documentation to Bonbo Git Server
    Reformatted Nuspec Description
    Added Information regarding Features Installed and removed.
    Added Uninstall messages to the unisntall script if AppPool/Site is not removed

--- bonobo-git-server/tools/chocolateyuninstall.ps1
+++ bonobo-git-server/tools/chocolateyuninstall.ps1
@@ -4,19 +4,24 @@ Import-Module WebAdministration
 
 if(Test-Path IIS:\AppPools\BonoboGitServer)
 {
-        #May not want to do this on every upgrade
-        Write-Output "Removing Existing AppPool"
-        Remove-WebAppPool BonoboGitServer
+    Write-Output "Removing AppPool: Bonobo.Git.Server"
+    Remove-WebAppPool BonoboGitServer
     Get-ChildItem IIS:\AppPools
+}
+else {
+    Write-Output "Default AppPool (Bonobo.Git.Server) was not found. You may need to manually remove the AppPool if it is no longer in use."
 }
 
 if(Test-Path IIS:\Sites\BonoboGitServer)
 {
-    #May not want to do this on every upgrade
-    Write-Output "Removing Existing Website"
-    Remove-WebSite BonoboGitServer
+    Write-Output "Removing Website: Bonobo.Git.Server"
+    Remove-WebSite Bonobo.Git.Server
     Get-ChildItem IIS:\Sites
+}
+else {
+    Write-Output "Default Website (Bonobo.Git.Server) was not found. You may need to manually remove the Website."
 }
 
 Uninstall-ChocolateyZipPackage -PackageName $env:ChocolateyPackageName
 Write-Output "Files and repositories have not been deleted and may need to be cleaned up manually."
+Write-Output "Optional Windows Features required for Bonobo have not been uninstalled (Use caution when removing them manually as they may be in use by other applications)."
