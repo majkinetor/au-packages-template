cae42242f789e653f316578a72996278d4c7f8ad
flcdrg <david@gardiner.net.au>
2019-10-29 09:09:34 +0000

    AU: 1 updated - SqlSearch [skip ci] https://gist.github.com/7d00c69c2cde8309a8594abaa897a9a6/aa7866a38465dc13d04f850e47c394f33cda3606

--- SqlSearch/tools/chocolateyInstall.ps1
+++ SqlSearch/tools/chocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop';
 $toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $checksum = 'AD91C5A582861006FA5AAE5B378E328C4A5A9125CB9B24C2AA4DA88A7ADC34CC'
-$url = 'ftp://support.red-gate.com/patches/SQLSearch/03Sep2019/SQLSearch.exe'
+$url = 'https://download.red-gate.com/installers/SQL_Search/2019-09-27/SQL_Search.exe'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
