3ad37a6376776fbe63cba3c87098bd2b94acfb1a
Curtis Carter <lokiphasma@gmail.com>
2019-11-01 07:33:58 -0500

    Added AU Script for Kubeval
    Altered ChocolateInstall.ps1 slightly to fit Update Script

--- kubeval/tools/chocolateyinstall.ps1
+++ kubeval/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@ $ErrorActionPreference = 'Stop'
 
 $packageName= $env:ChocolateyPackageName
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = "https://github.com/garethr/kubeval/releases/download/$($env:ChocolateyPackageVersion)/kubeval-windows-386.zip"
-$url64      = "https://github.com/garethr/kubeval/releases/download/$($env:ChocolateyPackageVersion)/kubeval-windows-amd64.zip"
+$url        = ''
+$url64      = ''
 
 $packageArgs = @{
   packageName   = $packageName
@@ -12,10 +12,10 @@ $packageArgs = @{
   url           = $url
   url64bit      = $url64
 
-  checksum      = '1EB9F826E6E607B07E38C79AFFF425392016953368158D6B33817311D44F14EF'
+  checksum      = ''
   checksumType  = 'sha256'
-  checksum64    = '4C7085BA0366F961FC2664DED0F09AE61FB54F393717A73AAC8601987D7BDA31' 
-  checksumType64= 'sha256'
+  checksum64    = '' 
+  checksumType64= 'sha512'
 }
 
 Install-ChocolateyZipPackage @packageArgs
---
+++ kubeval/update.ps1
@@ -0,0 +1,28 @@
+﻿import-module au
+
+$releases = "https://github.com/instrumenta/kubeval/releases/"
+
+function global:au_SearchReplace {
+  @{
+    ".\tools\chocolateyInstall.ps1" = @{
+      "(^[$]url\s*=\s*)('.*')"      = "`$1'$($Latest.URL32)'"
+      "(^[$]url64\s*=\s*)('.*')"      = "`$1'$($Latest.URL64)'"
+      "(checksum\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
+      "(checksum64\s*=\s*)('.*')" = "`$1'$($Latest.Checksum64)'"
+      "(checksumType\s*=\s*)('.*')" = "`$1'$($Latest.ChecksumType32)'"
+      "(checksumType64\s*=\s*)('.*')" = "`$1'$($Latest.ChecksumType64)'"
+    }
+  }
+}
+
+function global:au_GetLatest {
+  $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
+  $regex   = '\/instrumenta\/kubeval\/releases\/tag\/\d{1,3}\.\d{1,3}\.\d{1,3}$'
+  $url     = $download_page.links | Where-Object href -match $regex | Select-Object -First 1 -expand href
+  $version = $url -split '\/' | Select-Object -Last 1
+  $url = "https://github.com/instrumenta/kubeval/releases/download/$version/kubeval-windows-386.zip"
+  $url64      = "https://github.com/instrumenta/kubeval/releases/download/$version/kubeval-windows-amd64.zip"
+  return @{ Version = $version; URL32 = $url; URL64 = $url64; ChecksumType64 = 'sha512'; ChecksumType = 'sha512';}
+}
+
+Update-Package -ChecksumFor all
