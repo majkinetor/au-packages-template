c4ca91c347f489f9c2236ed5469a1b698806dba4
Curtis Carter <lokiphasma@gmail.com>
2019-11-16 08:45:48 -0600

    Splatted Andorid-Log-Viewer AU Package

--- android-log-viewer/tools/chocolateyinstall.ps1
+++ android-log-viewer/tools/chocolateyinstall.ps1
@@ -1,14 +1,12 @@
 ﻿$ErrorActionPreference = 'Stop';
 
-$url32 = 'https://github.com/MarkusPalcer/AndroidLogViewer/releases/download/v1.2.2/Android.Log.Viewer.msi'
-$checksum32 = '784feaeeb418b06ed28bc5ee24daf220afb1c99830011ba70a28d435a33a5f8bb8fedcec46c3862d9a6eb9365c8b933ef4f885f3a6ebba8540a7349d4bea3e42'
-$checksumType32 = 'sha512'
-Install-ChocolateyPackage -PackageName $env:ChocolateyPackageName -FileType 'msi' -SilentArgs '/q' -Url $url32 -Checksum $checksum32 -ChecksumType $checksumType32
-  # [-Url64bit <String>] `
-  # [-ValidExitCodes <Object>] `
-  # [-Options <Hashtable>] `
-  # [-File <String>] `
-  # [-File64 <String>] `
-  # [-UseOnlyPackageSilentArguments] `
-  # [-UseOriginalLocation] `
-  # [-IgnoredArguments <Object[]>] [<CommonParameters>]<#  #>
+$InstallArgs = @{
+  PackageName = $env:ChocolateyPackageName
+  FileType = 'msi'
+  SilentArgs = '/q'
+  Url = 'https://github.com/MarkusPalcer/AndroidLogViewer/releases/download/v1.2.2/Android.Log.Viewer.msi'
+  Checksum = '784feaeeb418b06ed28bc5ee24daf220afb1c99830011ba70a28d435a33a5f8bb8fedcec46c3862d9a6eb9365c8b933ef4f885f3a6ebba8540a7349d4bea3e42'
+  ChecksumType = 'sha512'
+}
+
+Install-ChocolateyPackage @InstallArgs
--- android-log-viewer/update.ps1
+++ android-log-viewer/update.ps1
@@ -5,9 +5,9 @@ $releases = "https://github.com/MarkusPalcer/AndroidLogViewer/releases"
 function global:au_SearchReplace {
   @{
     ".\tools\chocolateyInstall.ps1" = @{
-      "(^[$]url32\s*=\s*)('.*')"      = "`$1'$($Latest.URL)'"
-      "(^[$]checksum32\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
-      "(^[$]checksumType32\s*=\s*)('.*')" = "`$1'$($Latest.ChecksumType32)'"
+      "(Url\s*=\s*)('.*')"      = "`$1'$($Latest.URL)'"
+      "(Checksum\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
+      "(ChecksumType\s*=\s*)('.*')" = "`$1'$($Latest.ChecksumType32)'"
     }
   }
 }
