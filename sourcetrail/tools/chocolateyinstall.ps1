c0ab1d2bf216faf212c36cd4cdd1eb1a1f4b36c1
Curtis Carter <lokiphasma@gmail.com>
2019-11-29 17:25:36 -0600

    Added SourceTrail AU Package
    Added sourcetrail
    Removed deno.png

---
+++ sourcetrail/tools/chocolateyinstall.ps1
@@ -0,0 +1,24 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$ZipFile = Join-Path (Join-Path $env:ChocolateyInstall (Join-Path 'lib' $env:ChocolateyPackageName)) 'Sourcetrail'
+$InstallChocolateyZipPackageArgs = @{
+    PackageName = $env:ChocolateyPackageName
+    UnzipLocation = $ZipFile
+    Url = 'https://github.com/CoatiSoftware/Sourcetrail/releases/download/2019.4.61/Sourcetrail_2019_4_61_Windows_32bit_Installer.zip'
+    Url64Bit = 'https://github.com/CoatiSoftware/Sourcetrail/releases/download/2019.4.61/Sourcetrail_2019_4_61_Windows_64bit_Installer.zip'
+    CheckSum = ''
+    Checksum64 = ''
+    CheckSumType = 'sha512'
+    CheckSumType64 = 'sha512'
+}
+
+$file = Install-ChocolateyZipPackage @InstallChocolateyZipPackageArgs
+$InstallFileArgs = @{
+    PackageName = $env:ChocolateyPackageName
+    FileType = 'msi'
+    SilentArgs = '/qn'
+    File = (Get-ChildItem -Path $file -Filter 'sourcetrail.msi' -Recurse).FullName
+    ExitCodes = 0
+    validExitCodes= @(0)
+}
+Install-ChocolateyInstallPackage @InstallFileArgs
---
+++ sourcetrail/update.ps1
@@ -0,0 +1,29 @@
+﻿import-module au
+
+$releases = "https://github.com/CoatiSoftware/Sourcetrail/releases"
+
+function global:au_SearchReplace {
+  @{
+    ".\tools\chocolateyInstall.ps1" = @{
+      "(Url64Bit\s*=\s*)('.*')"      = "`$1'$($Latest.URL64)'"
+      "(Url\s*=\s*)('.*')"      = "`$1'$($Latest.URL)'"
+      "(Checksum64\s*=\s*)('.*')" = "`$1'$($Latest.Checksum64)'"
+      "(CheckSum\s*=\s*)('.*')" = "`$1'$($Latest.Checksum)'"
+      "(CheckSumType64\s*=\s*)('.*')" = "`$1'$($Latest.ChecksumType64)'"
+      "(CheckSumType\s*=\s*)('.*')" = "`$1'$($Latest.ChecksumType)'"
+    }
+  }
+}
+
+function global:au_GetLatest {
+  $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
+  $regex   = '\/CoatiSoftware\/Sourcetrail\/releases\/tag\/\d{1,4}\.\d{1,3}\.\d{1,3}$'
+  $url     = $download_page.links | Where-Object href -match $regex | Select-Object -First 1 -expand href
+  $version = $url -split '\/' | Select-Object -Last 1
+  $installversion = $version -replace '\.', '_'
+  $url32 = "https://github.com/CoatiSoftware/Sourcetrail/releases/download/$version/Sourcetrail_$($installversion)_Windows_32bit_Installer.zip"
+  $url64 = "https://github.com/CoatiSoftware/Sourcetrail/releases/download/$version/Sourcetrail_$($installversion)_Windows_64bit_Installer.zip"
+  return @{ Version = $version; URL = $url32; URL64 = $url64; ChecksumType64 = 'sha512'; ChecksumType = 'sha512';}
+}
+
+Update-Package -ChecksumFor all
