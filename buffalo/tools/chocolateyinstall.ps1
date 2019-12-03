8444a9cccd72e4d42fa94329ca4cf2dbcc5805d9
Chocolatey <chocolatey@realdimensions.net>
2019-12-03 12:51:14 +0000

    AU: 1 updated - buffalo [skip ci] https://gist.github.com/0bd20f9c86b34b17fca08f908d9db4c6/b68f871c4af5c925343a05c8c46f4866e0d8a94a

--- buffalo/tools/chocolateyinstall.ps1
+++ buffalo/tools/chocolateyinstall.ps1
@@ -1,9 +1,9 @@
 ﻿$ErrorActionPreference = 'Stop';
 
-$url32 = 'https://github.com/gobuffalo/buffalo/releases/download/v0.15.1/buffalo_0.15.1_windows_386.tar.gz'
-$url64 = 'https://github.com/gobuffalo/buffalo/releases/download/v0.15.1/buffalo_0.15.1_windows_amd64.tar.gz'
-$checksum32 = '9cd6f1a5d62299ba46b5e6a30cadf67334235c46f5e3beca346c425c60b85cb027211586915182d2c555e5854d20a3f35e04f5070c4868010e8811853ad970d2'
-$checksum64 = '14474423add4e4b597c5dbd0a0636b27c3df41850c201fa7b08e593f1a8c07047530c86adbbbc80f6451f801adbf99faaa50c811012c243837ff35940839f173'
+$url32 = 'https://github.com/gobuffalo/buffalo/releases/download/v0.15.3/buffalo_0.15.3_windows_i386.tar.gz'
+$url64 = 'https://github.com/gobuffalo/buffalo/releases/download/v0.15.3/buffalo_0.15.3_windows_x86_64.tar.gz'
+$checksum32 = '363f634e49eafe4d8fbd47ad19e19ae4c5f70ad13602d136b6e53b4992b9e3097034c69eef1651b21cce989c4325d885c717363c8f9f273462c1a84482aac1a8'
+$checksum64 = 'c5b1c9358cb8e3be031e4991288b4f577862ad0a117a5e7f209e0b12b97b985436ba323a15404789f94faa289c8b4acfe514792418b28c48fa85a882285c79c6'
 $checksumType32 = 'sha512'
 $checksumType64 = 'sha512'
 $UnzipLocation = Join-Path $env:ChocolateyInstall (Join-Path 'lib' $env:ChocolateyPackageName)
