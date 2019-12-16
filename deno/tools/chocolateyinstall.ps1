521dd8211ed57ae82120de139d2ece5063ab9a9d
Chocolatey <chocolatey@realdimensions.net>
2019-12-16 08:09:28 +0000

    AU: 2 updated - deno kind [skip ci] https://gist.github.com/c92dc2b71ad3ec6feb68e4df6bbc93e3/d84305133a69849ca30fb4367866bac0257b6141

--- deno/tools/chocolateyinstall.ps1
+++ deno/tools/chocolateyinstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop';
 
-$url64 = 'https://github.com/denoland/deno/releases/download/v0.25.0/deno_win_x64.zip'
-$checksum64 = 'b20b8d4f897ac498ed576afa647c80cedc65796bd47dbb7cb5753fbe252a1ac91d0446066cea223f009d5bcac83802206baa7af4ee3adb58a99e5e0c46c09136'
+$url64 = 'https://github.com/denoland/deno/releases/download/v0.26.0/deno_win_x64.zip'
+$checksum64 = '4a4577bedc92ecce6aca70a20afd7449ed1d0b9f1fbfe50575469d2007dc32c4c1d182f7ce8164ba68a97694220564ff3f41df10720744a7c55fc3a18e443ccc'
 $checksumType64 = 'sha512'
 $UnzipLocation = Join-Path $env:ChocolateyInstall (Join-Path 'lib' $env:ChocolateyPackageName)
 Install-ChocolateyZipPackage -PackageName $env:ChocolateyPackageName -Url64 $url64 -UnzipLocation $UnzipLocation -CheckSum64 $checksum64 -CheckSumType64 $checksumType64
--- kind/tools/chocolateyinstall.ps1
+++ kind/tools/chocolateyinstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop';
 
-$url64 = 'https://github.com/kubernetes-sigs/kind/releases/download/v0.6.0/kind-windows-amd64'
-$checksum64 = 'fc350da228d1237fdb8c0b370bd46f4d4d81cdeae25b7804c435e2643e12643926df8d95a444b430e8683a0fa7da4df60cad2450563d6c3d0fa7769f7a5822a8'
+$url64 = 'https://github.com/kubernetes-sigs/kind/releases/download/v0.6.1/kind-windows-amd64'
+$checksum64 = '3151ac2e3cfbb122a7e7c1d5f81b443ae27d6e1a6e686dea18d861674fd76cf2381e97a7f5a60468c638d12f1fca310461ea232d996b6d74d53e09a48820458a'
 $checksumType64 = 'sha512'
 $File = Join-Path (Join-Path $env:ChocolateyInstall (Join-Path 'lib' $env:ChocolateyPackageName)) 'kind.exe'
 $file = Get-ChocolateyWebFile -PackageName $env:ChocolateyPackageName `
