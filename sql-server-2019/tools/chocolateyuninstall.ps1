5c9c145db9f2400b155073d18a2f034a6dc2a99a
David Gardiner <david@gardiner.net.au>
2019-12-17 21:16:37 +1030

    PackageFix sql-server-2019 to fix uninstall

--- sql-server-2019/tools/chocolateyuninstall.ps1
+++ sql-server-2019/tools/chocolateyuninstall.ps1
@@ -1,6 +1,6 @@
 ﻿$ErrorActionPreference = 'Stop';
 
-$setupARPPath = ((Get-ItemProperty 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\Microsoft SQL Server SQL2017').UninstallString).Replace("`"", "")
+$setupARPPath = ((Get-ItemProperty 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\Microsoft SQL Server SQL2019').UninstallString).Replace("`"", "")
 
 $setupPath = [IO.Path]::Combine([IO.Path]::GetDirectoryName([IO.Path]::GetDirectoryName($setupARPPath)), "setup.exe")
 
