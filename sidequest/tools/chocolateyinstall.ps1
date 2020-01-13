a236ccfb323d584ef46f89b96758a6f45b6b797b
Curtis Carter <lokiphasma@gmail.com>
2020-01-13 10:10:09 -0600

    Added SideQuest AU Package
    Added sidequest

---
+++ sidequest/tools/chocolateyinstall.ps1
@@ -0,0 +1,12 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$InstallArgs = @{
+    PackageName = $env:ChocolateyPackageName
+    FileType = 'exe'
+    SilentArgs = '/S'
+    URL64 = 'https://github.com/the-expanse/SideQuest/releases/download/v0.8.5/SideQuest-Setup-0.8.5-x64-win.exe'
+    Checksum64 = 'f93a7055a28bbb9312d01e29f1b98698ecefcbbf2ab3d25a40ae409dfd803ebf833fb7a1ee119dc530ee3271015b0ed9ec8c28a182451dd5fc13d7b7171b14d1'
+    ChecksumType64 = 'sha512'
+}
+
+Install-ChocolateyPackage @InstallArgs
---
+++ sidequest/update.ps1
@@ -0,0 +1,24 @@
+﻿import-module au
+
+$releases = "https://github.com/the-expanse/SideQuest/releases"
+
+function global:au_SearchReplace {
+  @{
+    ".\tools\chocolateyInstall.ps1" = @{
+      "(URL64\s*=\s*)('.*')" = "`$1'$($Latest.URL64)'"
+      "(Checksum64\s*=\s*)('.*')" = "`$1'$($Latest.Checksum64)'"
+      "(ChecksumType64\s*=\s*)('.*')" = "`$1'$($Latest.ChecksumType64)'"
+    }
+  }
+}
+
+function global:au_GetLatest {
+  $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
+  $regex   = '/the-expanse/SideQuest/releases/download/v[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\/SideQuest-Setup-[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}-x64-win\.exe$'
+  $url     = $download_page.links | Where-Object href -match $regex | Select-Object -First 1 -expand href
+  $version = $url -split '\/|-' | Select-Object -Last 1 -Skip 2
+  $url = "https://github.com/the-expanse/SideQuest/releases/download/v0.8.5/SideQuest-Setup-$version-x64-win.exe"
+  return @{ Version = $version; URL64 = $url; ChecksumType64 = 'sha512';}
+}
+
+Update-Package -ChecksumFor 64
