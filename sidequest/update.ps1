54974e53362834f247b3954a9172ddef6f023f47
Curtis Carter <lokiphasma@gmail.com>
2020-01-15 07:52:41 -0600

    Corrected SideQuest Update Script
    update.ps1 had a hard-coded version in the URL

--- sidequest/update.ps1
+++ sidequest/update.ps1
@@ -17,7 +17,7 @@ function global:au_GetLatest {
   $regex   = '/the-expanse/SideQuest/releases/download/v[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\/SideQuest-Setup-[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}-x64-win\.exe$'
   $url     = $download_page.links | Where-Object href -match $regex | Select-Object -First 1 -expand href
   $version = $url -split '\/|-' | Select-Object -Last 1 -Skip 2
-  $url = "https://github.com/the-expanse/SideQuest/releases/download/v0.8.5/SideQuest-Setup-$version-x64-win.exe"
+  $url = "https://github.com/the-expanse/SideQuest/releases/download/v$version/SideQuest-Setup-$version-x64-win.exe"
   return @{ Version = $version; URL64 = $url; ChecksumType64 = 'sha512';}
 }
 
