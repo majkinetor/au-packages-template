49ec94c1ebc9ef1a347775a10d399e8eb22e063d
Curtis Carter <lokiphasma@gmail.com>
2020-01-21 07:24:39 -0600

    Added Jenkins CLI AU Package
    Added jcli

---
+++ jcli/tools/chocolateyinstall.ps1
@@ -0,0 +1,17 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$UnzipLocation = Join-Path $env:ChocolateyInstall (Join-Path 'lib' $env:ChocolateyPackageName)
+
+$ChocoWebFileArgs = @{
+    PackageName = $env:ChocolateyPackageName
+    FileFullPath = (Join-Path $UnzipLocation 'jcli.tar.gz')
+    Url = ''
+    CheckSum = ''
+    CheckSumType = ''
+}
+$file = Get-ChocolateyWebFile @ChocoWebFileArgs
+
+  & 7z x $file -aoa -o"jcli.tar"
+  $tarball = Get-ChildItem -Path 'jcli.tar'
+  & 7z x "jcli.tar\$tarball" -aoa -ttar -o"jcli"
+  Copy-Item -Path 'jcli\jcli.exe' -Destination "$UnzipLocation\jcli.exe"
