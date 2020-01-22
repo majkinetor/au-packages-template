0cbfc792c4dea561546b6fc6e833635f1362c915
Chocolatey <chocolatey@realdimensions.net>
2020-01-22 14:04:15 +0000

    AU: 1 updated - biome [skip ci] https://gist.github.com/9caff29bb1e0380ccfa5ed977fbafd7f/b8796b28447b0659fdc2e098a587680434b337b5

--- biome/tools/chocolateyinstall.ps1
+++ biome/tools/chocolateyinstall.ps1
@@ -3,9 +3,9 @@
 $ZipInstallArgs = @{
     PackageName = $env:ChocolateyPackageName 
     UnzipLocation = Join-Path $env:ChocolateyInstall (Join-Path 'lib' $env:ChocolateyPackageName)
-    Url64 = ''
-    CheckSum64 = ''
-    CheckSumType64 = ''
+    Url64 = 'https://github.com/biome-sh/biome/releases/download/bio-0.90.26/bio-0.90.26-x86_64-windows.zip'
+    CheckSum64 = '46457597d2beaad35ffc5aeb98cf0394f53ef91d3db826e83a823678171693a0c8869240bca9cc1ec34d2eea790a515bd1968b9618b602b28a95021e9fbe578b'
+    CheckSumType64 = 'sha512'
 }
 
 Install-ChocolateyZipPackage @ZipInstallArgs
