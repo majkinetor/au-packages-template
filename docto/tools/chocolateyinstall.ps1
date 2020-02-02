9b5e8c733d47afaa3c800be2ada74c1f6f05810f
Chocolatey <chocolatey@realdimensions.net>
2020-02-02 22:37:54 +0000

    AU: 1 updated - docto [skip ci] https://gist.github.com/ad5494a1ed58655518420756100ba5d5/c3ca769dba71a82d04a9b089410df2ce96c5e456

--- docto/tools/chocolateyinstall.ps1
+++ docto/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@
 $InstallArgs = @{
     PackageName = $env:ChocolateyPackageName
     FileFullPath = Join-Path (Join-Path $env:ChocolateyInstall (Join-Path 'lib' $env:ChocolateyPackageName)) 'docto.exe'
-    URL = 'https://github.com/tobya/DocTo/releases/download/v1.1.24/docto.exe'
-    Checksum = '806204789ed9faf07971dd6915e0a9c7d57034376a427c56c4587b54502893753cb73600ecdb9e9af3c58fea71c435a864d47ed1f70306f32069e3363b389015'
+    URL = 'https://github.com/tobya/DocTo/releases/download/V1.2/docto.exe'
+    Checksum = 'dc72bb998986400ce37a5f28f8545056179d665052d34ecea90673be64afb1c9b283966f2de87c54adf81819c272a21e0981f784b280e3ecbfe53994ab543981'
     ChecksumType = 'sha512'
 }
 
