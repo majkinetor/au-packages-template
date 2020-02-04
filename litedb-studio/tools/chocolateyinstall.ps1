7ede267a28919bcbd9cc19641cb6ab0092b8f8ab
Chocolatey <chocolatey@realdimensions.net>
2020-02-04 00:08:15 +0000

    AU: 1 updated - litedb-studio [skip ci] https://gist.github.com/61c46322bd75b1392a1a086674013eef/3e0a8945ed927863f3822ca897806a6ab20c0ace

--- litedb-studio/tools/chocolateyinstall.ps1
+++ litedb-studio/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@
 $InstallArgs = @{
     PackageName = $env:ChocolateyPackageName
     FileFullPath = Join-Path (Join-Path (Get-ToolsLocation) $env:ChocolateyPackageName) 'LiteDB.Studio.exe'
-    URL = 'https://github.com/mbdavid/LiteDB.Studio/releases/download/v1.0.0/LiteDB.Studio.exe'
-    Checksum = '24f46dcd2c1f39892d902dce3d2f427be96d09f51126e63eca47069f2b4bfaccb9a05cd850920ebc63cebbdf6fe92f26d2c4650e97fea93a45e25bc36b9daf35'
+    URL = 'https://github.com/mbdavid/LiteDB.Studio/releases/download/v1.0.1/LiteDB.Studio.exe'
+    Checksum = '29460002837dde57eda6e6f995ca9c9722e729149d3e0c615c8d4297b51af4cc501e15e2d6a21e22dd105dabfde8273a9f892ed6ad0bbfdcd1952b9067b90a21'
     ChecksumType = 'sha512'
 }
 
