7b044c99b04795100ae1e98f4de9b1091e85b78e
David Gardiner <david@gardiner.net.au>
2020-02-08 20:26:25 +1030

    Manually update SQL Server 2017 CU19

--- sql-server-2017-cumulative-update/tools/chocolateyinstall.ps1
+++ sql-server-2017-cumulative-update/tools/chocolateyinstall.ps1
@@ -1,9 +1,9 @@
 ﻿$ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-$url        = 'https://download.microsoft.com/download/C/4/F/C4F908C9-98ED-4E5F-88D5-7D6A5004AEBD/SQLServer2017-KB4527377-x64.exe'
-$checksum   = 'f43c2fd9ba972e7d0fe72accf916c1f8bc82a2fb47a0c8d9e91f3b0b61748312'
-$softwareName = 'Hotfix 3257 for SQL Server 2017*(KB4527377)*'
+$url        = 'https://download.microsoft.com/download/C/4/F/C4F908C9-98ED-4E5F-88D5-7D6A5004AEBD/SQLServer2017-KB4535007-x64.exe'
+$checksum   = 'e0cb8c5420e9e7cfd4b3b4e37126ff54ecf1a1bbc3c1b4b75362315c965ab368'
+$softwareName = 'Hotfix 3281 for SQL Server 2017*(KB4535007)*'
 
 $filename = [IO.Path]::GetFileName($url)
 
