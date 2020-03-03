ec8fa01d15761de7518f18565b57240d13ca80be
Chocolatey <chocolatey@realdimensions.net>
2020-03-03 00:06:39 +0000

    AU: 1 updated - sourcetrail [skip ci] https://gist.github.com/5080697aba9a6fd66b7fea4f43702657/37930099f7fbeb4d50e94ebb9edb32d4e1d62881

--- sourcetrail/tools/chocolateyinstall.ps1
+++ sourcetrail/tools/chocolateyinstall.ps1
@@ -4,8 +4,8 @@ $ZipFile = Join-Path (Join-Path $env:ChocolateyInstall (Join-Path 'lib' $env:Cho
 $InstallChocolateyZipPackageArgs = @{
     PackageName = $env:ChocolateyPackageName
     UnzipLocation = $ZipFile
-    Url64Bit = 'https://github.com/CoatiSoftware/Sourcetrail/releases/download/2019.4.102/Sourcetrail_2019_4_102_Windows_64bit_Installer.zip'
-    Checksum64 = '83da05aca615e71afbbe34a53f378ae79e8db3a13ec9f53059bcf7bcb6177fba2a52048a76bb26b9a75916ac75a11441828183a5fce33c7bb701d237df16995a'
+    Url64Bit = 'https://github.com/CoatiSoftware/Sourcetrail/releases/download/2020.1.106/Sourcetrail_2020_1_106_Windows_64bit_Installer.zip'
+    Checksum64 = '5678582c865a123ed31b0087d0a695bbe011c262ed924378be0ecbb0752c684a0db0ff0648e9489213e606465cf2f4457da87d39e42ba06469ecc0f2cf61d074'
     CheckSumType64 = 'sha512'
 }
 
