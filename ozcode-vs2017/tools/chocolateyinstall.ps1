3fc61d310b53a1f8820f275acc60f35d900d7b36
David Gardiner <david@gardiner.net.au>
2020-04-07 10:28:54 +0930

    Switch to using chocolatey-visualstudio.extension

--- ozcode-vs2017/tools/chocolateyinstall.ps1
+++ ozcode-vs2017/tools/chocolateyinstall.ps1
@@ -1,32 +1,14 @@
 ﻿$ErrorActionPreference = 'Stop';
 
 $packageName  = 'ozcode-vs2017'
-$toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $fileName     = 'OzCode_4.0.0.2540.vsix'
 $checksum     = '1b5b72bc379c398267693c3fc711dc4c4c5efb9c23c3f24275b133c58064b031'
-$fileFullPath = "$toolsDir\$fileName"
-
-# Flag whether we're being invoked by AU module
-[bool] $runningAU = (Test-Path Function:\au_GetLatest)
-
-if (-not $runningAU) {
-. $toolsDir\common.ps1
-} else {
-    # We don't want this file being put in the tools directory when running in AU
-    $fileFullPath = "$ENV:Temp\$filename"
-}
-
 
 $packageArgs = @{
   packageName   = $packageName
-  fileFullPath  = $fileFullPath
-  url           = "https://downloads.oz-code.com/files/$fileName"  #download URL, HTTPS preferrred
+  url           = "https://downloads.oz-code.com/files/$fileName"
   checksum      = $checksum
   checksumType  = 'sha256'
 }
 
-Get-ChocolateyWebFile @packageArgs
-
-if (-not $runningAU) {
-    & $vsixInstaller $fileFullPath /instanceIds:$vsInstances /logfile:`"$env:TEMP\$($packageName)\$($packageName).VsixInstall.log`" /quiet
-}
+Install-VisualStudioVsixExtension @packageArgs
--- ozcode-vs2017/tools/chocolateyuninstall.ps1
+++ ozcode-vs2017/tools/chocolateyuninstall.ps1
@@ -2,9 +2,9 @@
 
 $extensionId = "6c84f75d-38c3-48a2-9e54-d362238686b5"
 
-$toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$packageArgs = @{
+    packageName   = $packageName
+    VsixId        = $extensionId
+  }
 
-. $toolsDir\common.ps1
-
-# Be explicit about VS instances, otherwise uninstalling can affect older versions besides 2017
-& $vsixInstaller /uninstall:$extensionId /instanceIds:$vsInstances /logfile:`"$env:TEMP\$($packageName)\$($packageName).VsixUninstall.log`" /quiet
+Uninstall-VisualStudioVsixExtension @packageArgs
