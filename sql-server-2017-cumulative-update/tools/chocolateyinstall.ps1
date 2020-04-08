ed08d013f16ce0e8219648361004fc031f0722f0
flcdrg <david@gardiner.net.au>
2020-04-08 00:07:23 +0000

    AU: 1 updated - sql-server-2017-cumulative-update [skip ci] https://gist.github.com/7d00c69c2cde8309a8594abaa897a9a6/80f502e86efebba7842f4ed74c23c20630e2acc8

--- sql-server-2017-cumulative-update/tools/chocolateyinstall.ps1
+++ sql-server-2017-cumulative-update/tools/chocolateyinstall.ps1
@@ -1,9 +1,9 @@
 ﻿$ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-$url        = 'https://download.microsoft.com/download/C/4/F/C4F908C9-98ED-4E5F-88D5-7D6A5004AEBD/SQLServer2017-KB4535007-x64.exe'
-$checksum   = 'e0cb8c5420e9e7cfd4b3b4e37126ff54ecf1a1bbc3c1b4b75362315c965ab368'
-$softwareName = 'Hotfix 3281 for SQL Server 2017*(KB4535007)*'
+$url        = 'https://download.microsoft.com/download/C/4/F/C4F908C9-98ED-4E5F-88D5-7D6A5004AEBD/SQLServer2017-KB4541283-x64.exe'
+$checksum   = '90677f7d8ff04691bd5bd7653b9e2498eeccac1ad331b1dd12249190b4f442fc'
+$softwareName = 'Hotfix 3294 for SQL Server 2017*(KB4541283)*'
 
 $filename = [IO.Path]::GetFileName($url)
 
