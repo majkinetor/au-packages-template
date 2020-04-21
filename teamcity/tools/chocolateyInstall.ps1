0aee62575b2568c60bf2947a015df23e208df596
flcdrg <david@gardiner.net.au>
2020-04-21 00:08:38 +0000

    AU: 2 updated - teamcity TeamCity-PreinstalledJRE [skip ci] https://gist.github.com/7d00c69c2cde8309a8594abaa897a9a6/fba1373efa91af15278a8cd6f929be0c6c31e171

--- teamcity/tools/chocolateyInstall.ps1
+++ teamcity/tools/chocolateyInstall.ps1
@@ -2,7 +2,7 @@
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 $filename = "TeamCity-$($env:ChocolateyPackageVersion).tar.gz"
-$checksum = '108e08b1f9f59d533bb1f7e3a498900bb746bb31051c968ba425887b4c5503e9'
+$checksum = 'd3f93b239ca22b6e0c62f6ca9ff7fda0f8b111c684205969233c07772ddbc188'
 
 $url = "https://download.jetbrains.com/teamcity/TeamCity-$($env:ChocolateyPackageVersion).tar.gz"
 $packagePath = $(Split-Path -parent $toolsDir)
