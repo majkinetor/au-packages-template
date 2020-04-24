48ef0b0cce99e757f6b62fa4adf3473f205bd296
flcdrg <david@gardiner.net.au>
2020-04-24 00:06:58 +0000

    AU: 1 updated - ozcode-vs2017 [skip ci] https://gist.github.com/7d00c69c2cde8309a8594abaa897a9a6/963d81e52a09f9a2023a88dac73abb620dffba54

--- ozcode-vs2017/tools/chocolateyinstall.ps1
+++ ozcode-vs2017/tools/chocolateyinstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop';
 
-$fileName     = 'OzCode_4.0.0.2613.vsix'
-$checksum     = ''
+$fileName     = 'OzCode_4.0.0.2637.vsix'
+$checksum     = '490719260ff56b08d6262bda0794ec3737edf2146660baf08751063525844002'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
