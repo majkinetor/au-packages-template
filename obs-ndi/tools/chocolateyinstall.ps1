f6446c01ed4800d6ce9ae83f8966131e2e7610b6
David Gardiner <david@gardiner.net.au>
2020-05-02 16:22:44 +0930

    Initial obs-ndi

---
+++ obs-ndi/tools/chocolateyinstall.ps1
@@ -0,0 +1,41 @@
+﻿$ErrorActionPreference = 'Stop';
+$toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+# First, install NDI Runtime
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  softwareName  = 'NDI 4 Runtime'
+  fileType      = 'exe'
+  silentArgs    = "/VERYSILENT /LOG /NORESTART /SUPPRESSMSGBOXES"
+
+  validExitCodes= @(0)
+  url           = "https://ndi.palakis.fr/runtime/ndi-runtime-4.5.1-Windows.exe"
+  checksum      = 'AB9E6FE27ABD44874DA02E4E4A335883A2812A05F5AF63065E5DD53CAF7E4F64'
+  checksumType  = 'sha256'
+  destination   = $toolsDir
+}
+
+Install-ChocolateyPackage @packageArgs
+
+# Now install obs-ndi
+
+# Find install location from registry, but fall back to ProgramFiles 
+$installPath = "$ENV:ProgramFiles\obs-studio"
+$key = (Get-ItemProperty -Path 'HKLM:\SOFTWARE\WOW6432Node\OBS Studio' -ErrorAction SilentlyContinue)
+if ($key) {
+  $installPath = $key.'(default)'
+}
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+
+  url           = "https://github.com/Palakis/obs-ndi/releases/download/4.9.0/obs-ndi-4.9.0-Windows.zip"
+  checksum      = '531642B5A21A9774AC818C0913E9A8740CDA3993E25EB459DD3A3B5A81177A77'
+  checksumType  = 'sha256'
+  UnzipLocation = $installPath
+  file          = "$toolsDir\obs-ndi.zip"
+}
+
+Install-ChocolateyZipPackage @packageArgs
+
+Write-Warning "You must reboot your computer to make a new or updated NDI Runtime installation effective"
---
+++ obs-ndi/tools/chocolateyuninstall.ps1
@@ -0,0 +1,34 @@
+﻿$ErrorActionPreference = 'Stop';
+
+# Would use Uninstall-ChocolateyZipPackage @packageArgs, but for https://github.com/chocolatey/choco/issues/2042
+
+$ZipFileName  = "obs-ndi-4.9.0-Windows.zip"
+
+$zipContentFile=(join-path $env:chocolateyPackageFolder $zipFileName) + "Install.txt"
+
+if(-Not (Test-Path -Path $zipContentFile)) {
+  $zipContentFile=(Join-Path $env:chocolateyPackageFolder -ChildPath $zipFileName) + ".txt"
+}
+
+if ((Test-Path -path $zipContentFile)) {
+  $zipContentFile
+
+  # Sorted so files are done before the directories they belong to
+  $zipContents=get-content $zipContentFile | Sort-Object -Descending
+  foreach ($fileInZip in $zipContents) {
+    if ($fileInZip -ne $null -and $fileInZip.Trim() -ne '') {
+      if ((Get-Item $fileInZip).PSIsContainer) {
+        # Only delete if directory is empty
+        if ((Get-Item $fileInZip).GetFileSystemInfos().Count -eq 0) {
+          Remove-Item -Path "$fileInZip" -ErrorAction SilentlyContinue -Force
+        } else {
+          Write-Verbose "Skipping $fileInZip as it is not empty"
+        }
+      } else {
+        Remove-Item -Path "$fileInZip" -ErrorAction SilentlyContinue -Force
+      }
+    }
+  }
+} else {
+  Write-Verbose "Couldn't find $zipContentFile"
+}
