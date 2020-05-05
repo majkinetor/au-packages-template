4f804238678709baa6b3c57ed48627bc802a5760
David Gardiner <david@gardiner.net.au>
2020-05-06 07:13:02 +0930

    sql server 2019 cumulative update
    - 15.0.4033.1

---
+++ sql-server-2019-cumulative-update/tools/chocolateyinstall.ps1
@@ -0,0 +1,65 @@
+﻿$ErrorActionPreference = 'Stop';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+$url        = 'https://download.microsoft.com/download/6/e/7/6e72dddf-dfa4-4889-bc3d-e5d3a0fd11ce/SQLServer2019-KB4548597-x64.exe'
+$checksum   = '58d78ac13dd8bba0b5e17aafaa8fe38a9d9bbcd72dad480b66c914e6962db888'
+$softwareName = 'Hotfix 4033 for SQL Server 2019*(KB4548597)*'
+
+. $toolsDir\Get-PendingReboot.ps1
+
+if ([Version] (Get-CimInstance Win32_OperatingSystem).Version -lt [version] "10.0.0.0") {
+  Write-Error "SQL Server 2019 requires a minimum of Windows 10 or Windows Server 2016"
+}
+
+$pp = Get-PackageParameters
+
+if ( (!$pp['IGNOREPENDINGREBOOT']) -and (Get-PendingReboot).RebootPending) {
+  Write-Error "A system reboot is pending. You must restart Windows first before installing SQL Server updates"
+}
+
+$filename = [IO.Path]::GetFileName($url)
+
+# Download like Install-ChocolateyPackage (so we can restart from cached download)
+$chocTempDir = $env:TEMP
+
+$tempDir = Join-Path $chocTempDir "$($env:chocolateyPackageName)"
+if ($env:chocolateyPackageVersion -ne $null) { $tempDir = Join-Path $tempDir "$($env:chocolateyPackageVersion)"; }
+$tempDir = $tempDir -replace '\\chocolatey\\chocolatey\\', '\chocolatey\'
+if (![System.IO.Directory]::Exists($tempDir)) { [System.IO.Directory]::CreateDirectory($tempDir) | Out-Null }
+$downloadFilePath = Join-Path $tempDir $filename
+
+$fullFilePath = Join-Path $toolsDir $filename
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  url           = $url
+  FileFullPath  = $downloadFilePath
+  checksum      = $checksum
+  checksumType  = 'sha256'
+}
+
+$filePath = Get-ChocolateyWebFile @packageArgs
+
+if (Test-Path Function:\au_GetLatest) {
+  exit
+}
+
+# Copy into tools to keep for uninstall
+Copy-Item $filePath -Destination $fullFilePath
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  file  = $fullFilePath
+  softwareName  = $softwareName
+  silentArgs    = "/q /IAcceptSQLServerLicenseTerms /Action=Patch /AllInstances"
+  validExitCodes= @(0, 3010, 1641)
+}
+
+Install-ChocolateyInstallPackage @packageArgs
+
+$files = get-childitem $toolsDir -include *.exe -recurse
+
+foreach ($file in $files) {
+  # generate an ignore file
+  New-Item "$file.ignore" -type file -force | Out-Null
+}
---
+++ sql-server-2019-cumulative-update/update.ps1
@@ -0,0 +1,65 @@
+import-module au
+
+function global:au_SearchReplace {
+    @{
+        #   softwareName  = 'Hotfix 3026 for Microsoft SQL Server*(KB4229789)*'
+        'tools\chocolateyInstall.ps1' = @{
+            "(^[$]url\s*=\s*)('.*')"      = "`$1'$($Latest.URL64)'"
+            "(^[$]checksum\s*=\s*)('.*')" = "`$1'$($Latest.Checksum64)'"
+            "(^[$]softwareName\s*=\s*)('.*')" = "`$1'Hotfix $($Latest.Build) for SQL Server 2019*(KB$($Latest.KB))*'"
+        }
+     }
+}
+
+function global:au_GetLatest {
+    $downloadId = "100809"
+    $response = Invoke-WebRequest -Uri "https://www.microsoft.com/en-us/download/confirmation.aspx?id=$downloadId" -ErrorAction Ignore
+
+    $url = $response.Content | Select-String -AllMatches -Pattern "(http|ftp|https)://([\w_-]+(?:(?:\.[\w_-]+)+))([\w.,@?^=%&:/~+#-]*[\w@?^=%&/~+#-])?" | % { $_.Matches.Value } | Select-string "\.exe$" | Select-Object -First 1
+    
+    # <meta name="Description" content="Cumulative Update Package 7 for SQL Server 2017 - KB4229789" />
+    if ($response.Content -match "<meta name=`"Description`" content=`"Cumulative Update Package (\d+) for SQL Server 2019 - KB(\d+)`" \/\>") {
+        $cu = $Matches[1]
+        $kb = $Matches[2]
+    } else {
+        return @{}
+    }
+
+    $response = Invoke-WebRequest -Uri "https://www.microsoft.com/en-us/download/details.aspx?id=$downloadId"
+
+    if ($response.Content -match "\d+\.\d+\.\d+\.\d+") {
+        $version = $Matches[0]
+    } else {
+        return @{}
+    }
+    
+    $v = [Version] $version
+    $Latest = @{ 
+        URL64 = $url
+        Version = $version
+        KB = $kb
+        CU = $cu
+        Build = $v.Build
+    }
+    return $Latest
+}
+
+function global:au_AfterUpdate
+{ 
+    $nuspecFileName = $Latest.PackageName + ".nuspec"
+    $nu = Get-Content $nuspecFileName -Raw -Encoding UTF8
+    $nu = $nu -replace "(?smi)(\<releaseNotes\>).*?(\</releaseNotes\>)", "`${1}http://support.microsoft.com/help/$($Latest.KB)`$2"
+
+    $nu = $nu -replace "(?smi)(\<title\>).*?(\</title\>)", "`${1}Microsoft SQL Server 2019 Cumulative Update $($Latest.CU)`$2"
+    
+    $Utf8NoBomEncoding = New-Object System.Text.UTF8Encoding($False)
+    $NuPath = (Resolve-Path $NuspecFileName)
+    [System.IO.File]::WriteAllText($NuPath, $nu, $Utf8NoBomEncoding)
+}
+
+function global:au_BeforeUpdate() {
+    # Not sure why, but au isn't getting checksum from chocolateyInstall
+    $Latest.Checksum64 = Get-RemoteChecksum $Latest.Url64
+ }
+
+update
