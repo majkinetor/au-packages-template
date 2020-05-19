4f35881b460f63ac7614105c222c2f2d0f79e19e
David Gardiner <david@gardiner.net.au>
2020-05-19 18:37:32 +0930

    obs-virtualcam 2.0.5
    - Improve registration

--- obs-virtualcam/tools/chocolateyinstall.ps1
+++ obs-virtualcam/tools/chocolateyinstall.ps1
@@ -1,8 +1,8 @@
 ﻿$ErrorActionPreference = 'Stop';
 $toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-$url = "https://github.com/CatxFish/obs-virtual-cam/releases/download/2.0.4/OBS-VirtualCam2.0.4.zip"
-$checksum = '6A248C73499E911AE1DA0224FF2D21A07E2E1EC4416987C2D5F34718D9B3D730'
+$url = "https://github.com/Fenrirthviti/obs-virtual-cam/releases/download/2.0.5/OBS-Virtualcam-2.0.5-Windows.zip"
+$checksum = 'C33F1C884C33BEA15E9254A4CAB5F1B66403E2D2F2767562B8726E58B05D38FC'
 
 $zipFile = [System.IO.Path]::Combine($toolsDir, [System.IO.Path]::GetFileName($url))
 
@@ -17,24 +17,31 @@ $packageArgs = @{
 }
 
 Get-ChocolateyWebFile @packageArgs
-Get-ChocolateyUnzip -FileFullPath $zipFile -Destination $toolsDir
+Get-ChocolateyUnzip -FileFullPath $zipFile -Destination $toolsDir\OBS-VirtualCam
 
 $obsInstall = [System.IO.Path]::Combine($env:ProgramFiles, "obs-studio")
 
 # copy files and record destination file locations for uninstall
 # avutil-56.dll and swscale-5.dll are already part of OBS Studio, so we don't replace those
-Copy-Item -Recurse $toolsDir\OBS-VirtualCam\* -Exclude "avutil-56.dll", "swscale-5.dll" -Destination $obsInstall -Force -PassThru | Where-Object { -not $_.PSIsContainer } | Set-Content -path $toolsDir\files.txt
+Copy-Item -Recurse $toolsDir\OBS-VirtualCam\* -Exclude "avutil-56.dll", "swscale-5.dll" -Destination $obsInstall -Force -PassThru | 
+  Where-Object { -not $_.PSIsContainer } | 
+  Select-Object -ExpandProperty Fullname |
+  Tee-Object -FilePath $toolsDir\files.txt |
+  Write-Verbose
 
+# Only copy these files if they don't exist at the destination
+Get-ChildItem $toolsDir\OBS-VirtualCam\bin\32bit\ -Exclude obs* | 
+  Where-Object { -not ( Test-Path ("$obsInstall\bin\32bit\$($_.Name)") ) } | 
+  Copy-Item -Destination "$obsInstall\bin\32bit\" -PassThru | 
+  Select-Object -ExpandProperty Fullname |
+  Tee-Object -FilePath $toolsDir\files.txt -Append |
+  Write-Verbose
+
+Write-Verbose "Registering 32bit dll"
+Start-ChocolateyProcessAsAdmin -Statements "/s `"$obsInstall\bin\32bit\obs-virtualsource.dll`"" -ExeToRun "c:\windows\system32\regsvr32.exe"
 
 if (Get-OSArchitectureWidth -eq 64) {
-  Write-Verbose "Registering 32bit dll"
-  Start-ChocolateyProcessAsAdmin -Statements "c:\windows\SysWoW64\regsvr32.exe /s `"$obsInstall\bin\32bit\obs-virtualsource.dll`""
   
   Write-Verbose "Registering 64bit dll"
-  Start-ChocolateyProcessAsAdmin -Statements "/s `"$obsInstall\bin\64bit\obs-virtualsource.dll`"" -ExeToRun "C:\Windows\System32\regsvr32.exe"
-} else {
-  Write-Verbose "Registering 32bit dll"
-  Start-ChocolateyProcessAsAdmin -Statements "c:\windows\System32\regsvr32.exe /s `"$obsInstall\bin\32bit\obs-virtualsource.dll`""
+  Start-ChocolateyProcessAsAdmin -Statements "/s `"$obsInstall\bin\64bit\obs-virtualsource.dll`"" -ExeToRun "c:\windows\system32\regsvr32.exe"
 }
-
-Write-Warning "The previous calls to Start-ChocolateyProcessAsAdmin may generate red text, but it isn't an error"
--- obs-virtualcam/tools/chocolateyuninstall.ps1
+++ obs-virtualcam/tools/chocolateyuninstall.ps1
@@ -4,20 +4,17 @@ $toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 # unregister
 $obsInstall = [System.IO.Path]::Combine($env:ProgramFiles, "obs-studio")
 
+Write-Verbose "Unregistering 32bit dll"
+Start-ChocolateyProcessAsAdmin -Statements "/s /u `"$obsInstall\bin\32bit\obs-virtualsource.dll`"" -ExeToRun "c:\windows\system32\regsvr32.exe"
+
 if (Get-OSArchitectureWidth -eq 64) {
-    Write-Verbose "Unregistering 32bit dll"
-    Start-ChocolateyProcessAsAdmin -Statements "c:\windows\SysWoW64\regsvr32.exe /s /u `"$obsInstall\bin\32bit\obs-virtualsource.dll`""
 
     Write-Verbose "Unregistering 64bit dll"
-    Start-ChocolateyProcessAsAdmin -Statements "/s `"$obsInstall\bin\64bit\obs-virtualsource.dll`"" -ExeToRun "C:\Windows\System32\regsvr32.exe"
-} else {
-    Write-Verbose "Unregistering 32bit dll"
-    Start-ChocolateyProcessAsAdmin -Statements "c:\windows\System32\regsvr32.exe /s /u `"$obsInstall\bin\32bit\obs-virtualsource.dll`""
+    Start-ChocolateyProcessAsAdmin -Statements "/s /u `"$obsInstall\bin\64bit\obs-virtualsource.dll`"" -ExeToRun "c:\windows\system32\regsvr32.exe"
 }
 
-Write-Warning "The previous calls to Start-ChocolateyProcessAsAdmin may generate red text, but it isn't an error"
 # remove files
-
-(Get-Content $toolsDir\files.txt) | Remove-Item
+Write-Warning "If any running application has been using the virtual camera, these files may be locked for deletion"
+(Get-Content $toolsDir\files.txt) | Where-Object { Test-Path $_ } | Remove-Item -Verbose -Force
 
 Remove-Item "$obsInstall\data\obs-plugins\obs-virtualoutput" -Recurse -Force
