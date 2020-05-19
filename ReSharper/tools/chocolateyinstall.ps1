0f6d012dff66164fb34e30e5933a8365a970f9bf
flcdrg <david@gardiner.net.au>
2020-05-19 10:37:35 +0000

    AU: 9 updated - dotCover dotMemory dotPeek dotTrace ReSharper resharper-clt ReSharperCpp resharper-ultimate-all teamCityAddin [skip ci] https://gist.github.com/7d00c69c2cde8309a8594abaa897a9a6/bc695a0e79586d09e9711ea83a85c9dd7164ce92

--- ReSharper/tools/chocolateyinstall.ps1
+++ ReSharper/tools/chocolateyinstall.ps1
@@ -1,6 +1,6 @@
 ﻿$ErrorActionPreference = 'Stop'; # stop on all errors
 
-$filename = 'JetBrains.ReSharperUltimate.2020.1.2.exe'
+$filename = 'JetBrains.ReSharperUltimate.2020.1.3.exe'
 
 $platformPackageName = 'resharper-platform'
 
