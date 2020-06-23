9b9514e0f4e9cc6dc7bdb477e6d8279264962fee
Chocolatey <chocolatey@realdimensions.net>
2020-06-23 18:19:58 +0000

    AU: 1 updated - teamviewer-qs [skip ci] https://gist.github.com/03903cd565061a825f153ce2f9d6227c/515a8fed4ca52f0c6d6f8a89d1dfd188225e581e

--- teamviewer-qs/tools/chocolateyinstall.ps1
+++ teamviewer-qs/tools/chocolateyinstall.ps1
@@ -2,7 +2,7 @@
   packageName            = "$env:chocolateyPackageName"
   FileFullPath           = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)\TeamViewerQS.exe"
   url                    = 'https://download.teamviewer.com/download/TeamViewerQS.exe'
-  checksum               = '67816c035f4f3d79ec04b5d25e2cc7af78fd99935f6f206c65e8519e88d276d9'
+  checksum               = 'bfbd20b7de4c831af337ddd47008742facf0244e08d6fb3716573492d3d19eb1'
   checksumType           = 'sha256'
 }
 Get-ChocolateyWebFile @packageArgs
