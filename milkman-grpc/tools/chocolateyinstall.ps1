7b3db2a02625afba9b4e479ee80e205339880a45
Curtis Carter <lokiphasma@gmail.com>
2020-07-06 06:35:39 -0500

    Added milkman Cassandra and grpc plugins

---
+++ milkman-grpc/tools/chocolateyinstall.ps1
@@ -0,0 +1,10 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$checksum64 = '60DDBF55CDCB2C183889BDCB41F5F59BDFD4B437CEE3F5BEAED121E57CCB1291C5A20075D617A1E60018122A8ADD70787F91EF0CBEB2BCEC924CBBCC6BDFBFC4'
+$checksumType64 = 'sha512'
+$JarFile = Join-Path (Join-Path $env:ChocolateyInstall (Join-Path (Join-Path 'lib' $env:ChocolateyPackageName) 'tools')) "$env:ChocolateyPackageName.jar"
+Get-ChecksumValid -File $JarFile -CheckSum $checksum64 -CheckSumType $checksumType64
+$InstallLocation = (Join-Path (Join-Path (Join-Path (Get-ToolsLocation) "milkman") "plugins") "$env:ChocolateyPackageName.jar")
+Copy-Item -Path $JarFile -Destination $InstallLocation -Recurse
+Remove-Item -Path $JarFile -Force
+Write-Host "Installed $($env:ChocolateyPackageName) to $($InstallLocation)"
---
+++ milkman-grpc/update.ps1
@@ -0,0 +1,39 @@
+﻿import-module au
+
+$releases = "https://github.com/warmuuh/milkman/releases"
+$PackageName = "milkman-grpc"
+function global:au_SearchReplace {
+  @{
+    ".\tools\chocolateyInstall.ps1" = @{
+      "(^[$]checksum64\s*=\s*)('.*')" = "`$1'$($Latest.Checksum64)'"
+      "(^[$]checksumType64\s*=\s*)('.*')" = "`$1'$($Latest.ChecksumType64)'"
+    }
+    "$PackageName.nuspec" = @{
+      "(\<dependency id=""milkman"" version=""\[).*(\]""\s*/>)" = "`${1}$($Latest.Version)`$2"
+    }
+  }
+}
+
+function global:au_BeforeUpdate() {
+  $Latest.Checksum64 = (Get-FileHash -Path "tools\$PackageName.jar" -Algorithm SHA512).Hash
+  $Latest.ChecksumType64 = 'sha512'
+}
+
+function global:au_GetLatest {
+  $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
+  $regex   = '\/warmuuh\/milkman\/releases\/tag\/[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}$'
+  $url     = $download_page.links | Where-Object href -match $regex | Select-Object -First 1 -expand href
+  $version = $url -split '\/' | Select-Object -Last 1
+  $url = 'https://github.com/warmuuh/milkman/releases/download/' + $version + '/milkman-dist-plugins-bin.zip'
+  $DownloadedFile = "$PSScriptRoot\milkman-dist-plugins-bin.zip"
+  (New-Object System.Net.WebClient).DownloadFile($url, $DownloadedFile)
+  Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/warmuuh/milkman/master/LICENSE' -OutFile 'tools\License.txt'
+  $UnzipDirectory = "$PSScriptRoot\milkman-dist-plugins-bin\"
+  Expand-Archive -Path $DownloadedFile -DestinationPath $UnzipDirectory -Force
+  Get-ChildItem -Path $UnzipDirectory  -Filter "$PackageName.jar" -Recurse | Select-Object -first 1 | Copy-Item -Destination "tools\$PackageName.jar"
+  Remove-Item -Path "milkman-dist-plugins-bin.zip"
+  Remove-Item -Path "milkman-dist-plugins-bin" -Recurse
+  return @{ Version = $version;}
+}
+
+Update-Package -ChecksumFor none
