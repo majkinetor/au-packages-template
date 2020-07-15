8c0ff7b265aecadd4c642046e263b67422d355ff
flcdrg <david@gardiner.net.au>
2020-07-15 00:06:32 +0000

    AU: 2 updated - beyondcompare dotnetdeveloperbundle [skip ci] https://gist.github.com/7d00c69c2cde8309a8594abaa897a9a6/803aff5570156ea4921aad3b14571cb918c7857f

--- beyondcompare/tools/chocolateyInstall.ps1
+++ beyondcompare/tools/chocolateyInstall.ps1
@@ -14,7 +14,7 @@ $german = @(3079,1031,5127,4103,2055)
 $french = @(2060,11276,3084,9228,12300,1036,5132,13324,6156,14348,10252,4108,7180)
 $japanese = @(17, 1041)
 $chinese = @(2052)
-$version = '4.3.4.24657'
+$version = '4.3.5.24893'
 
 $packageArgs = @{
   packageName   = 'beyondcompare'
@@ -26,11 +26,11 @@ $packageArgs = @{
   checksumType  = 'sha256'
 }
 
-$checksumde = '51a3fea02c849340462d7588fe8e3df24f5f5204ce1f4da1685752b12a29864d'
-$checksumfr = '1d6ac8155530cd25971edb16427cf94125b3ada92d2b480ed17a079db94dc9aa'
-$checksumjp = 'd6e783c7d57d972c7db88f4b091fa368302ffb3db8e78ab8e2d23439313aad24'
-$checksumzh = '04162b6d37967dd742863759243e8901449db3b48d6c26fee244856bf200f149'
-$checksum = '435e4609741ba285d4cd67d4c76035a3f37f8d3640b56879b00b3db6f862cdd6'
+$checksumde = '8efe82bb099de9ed47a28783e811dfadf3541e62143f84d1beb18c73e31135e9'
+$checksumfr = 'ce31fd3c70da01bc834da7c6304b47f81492443daf17f8c0bd22fd29cb889b98'
+$checksumjp = 'ede8cba8d47d5a12fe324ff9ef935fa7a0708e0fff50e7212ae0b520639ec5a4'
+$checksumzh = 'af92abf3bbf066f0a78629e488da8922e2cd7b5889f39291cbf81ff6faeafd50'
+$checksum = 'd1868103341be68bb1068483950f46cf33c2290f748c7fca4b634cdd220650b7'
 
 if ($german -contains $LCID)
 {
--- dotnetdeveloperbundle/tools/chocolateyinstall.ps1
+++ dotnetdeveloperbundle/tools/chocolateyinstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop';
 $toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$checksum = 'D091DD3F6AC011C6BBC52E20609DC106B0FFE24DE8EB04C8BD141074FD1FC954'
-$url = 'https://download.red-gate.com/installers/DotNETDeveloperBundle/2020-06-18/DotNETDeveloperBundle.exe'
+$checksum = '37B346EC982126ADE73743C28F3BCAFC4D71FE807F6B9FC9B4930807B1724274'
+$url = 'https://download.red-gate.com/installers/DotNETDeveloperBundle/2020-07-13/DotNETDeveloperBundle.exe'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
