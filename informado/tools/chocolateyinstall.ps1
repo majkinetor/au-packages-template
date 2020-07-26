5e3bdc0488a1bd04130816aa4f4d9e611dae77d5
Chocolatey <chocolatey@realdimensions.net>
2020-07-26 16:06:17 +0000

    AU: 1 updated - informado [skip ci]

--- informado/tools/chocolateyinstall.ps1
+++ informado/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@
 $InstallArgs = @{
     PackageName = $env:ChocolateyPackageName
     FileFullPath = Join-Path (Join-Path $env:ChocolateyInstall (Join-Path 'lib' $env:ChocolateyPackageName)) 'informado.exe'
-    URL = 'https://github.com/030/informado/releases/download/1.3.0/informado-windows'
-    Checksum = '7fc51f7775452b636fd1da49a506953235010644543957da3e7bc6856a728c809a0d33baad6ee34a7564ef6aba0bd6cd914d0f8e778b85e9d7c02cbfc027fee3'
+    URL = 'https://github.com/030/informado/releases/download/1.4.0/informado-windows'
+    Checksum = '96c98a740e527712a0ee2276ab414a2e844611d5b52e233913213ec4eb88f9598beb29fa29720779a0cd811b946e5e7b1fe5e349033fa60c82425a67854ae197'
     ChecksumType = 'sha512'
 }
 
