37b1be850b6dbe1fbe49ea8ea1aad0b8e00e7ebf
bullshit <coding@holowaty.me>
2020-07-30 20:23:42 +0200

    Adding PDF-XChange Pro

---
+++ PDFXchangePro/tools/chocolateyInstall.ps1
@@ -0,0 +1,124 @@
+﻿$ErrorActionPreference = 'Stop'; # stop on all errors
+$packageName = 'PDFXchangePro' 
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$version    = $env:ChocolateyPackageVersion
+$filename   = 'ProV8.x86.msi'
+$filename64 = 'ProV8.x64.msi'
+$primaryDownloadUrl = "http://downloads.pdf-xchange.com/$filename"
+$primaryDownloadUrl64 = "http://downloads.pdf-xchange.com/$filename64"
+$url        = "http://www.docu-track.co.uk/builds/$version/$filename"
+$url64      = "http://www.docu-track.co.uk/builds/$version/$filename64"
+$checksum   = '65CB3B5C8BD77E019F22EDA85F69BB46C4DDC3FB1ABBFC03067667105FD144F2'
+$checksum64 = 'A58C7453FA2E5290A54C7939BA7B32F040A9201BADEE871A1159F2E45584375A'
+$lastModified32 = New-Object -TypeName DateTimeOffset 2020, 7, 29, 1, 4, 24, 0 # Last modified time corresponding to this package version
+$lastModified64 = New-Object -TypeName DateTimeOffset 2020, 7, 29, 1, 4, 23, 0 # Last modified time corresponding to this package version
+
+# Tracker Software have fixed download URLs, but if the binary changes we can fall back to their alternate (but slower) download site
+# so the package doesn't break.
+function CheckDownload($url, $primaryDownloadUrl, [DateTimeOffset] $packageVersionLastModified)
+{
+    $headers = Get-WebHeaders -url $primaryDownloadUrl
+    $lastModifiedHeader = $headers.'Last-Modified'
+
+    $lastModified = [DateTimeOffset]::Parse($lastModifiedHeader, [Globalization.CultureInfo]::InvariantCulture)
+
+    Write-Verbose "Package LastModified: $packageVersionLastModified"
+    Write-Verbose "HTTP Last Modified  : $lastModified"
+
+    if ($lastModified -ne $packageVersionLastModified) {
+        Write-Warning "The download available at $primaryDownloadUrl has changed from what this package was expecting. Falling back to FTP for version-specific URL"
+        $url
+    } else {
+        Write-Verbose "Primary URL matches package expectation"
+        $primaryDownloadUrl
+    }
+}
+
+$url = CheckDownload $url $primaryDownloadUrl $lastModified32
+$url64 = CheckDownload $url64 $primaryDownloadUrl64 $lastModified64
+
+$packageArgs = @{
+  packageName   = $packageName
+  unzipLocation = $toolsDir
+  fileType      = 'MSI'
+  url           = $url
+  url64bit      = $url64
+  silentArgs = '/quiet /norestart'
+  validExitCodes= @(0, 3010, 1641)
+
+  softwareName  = 'PDF-XChange Pro'
+
+  checksum = $checksum
+  checksumType  = 'sha256' 
+  checksum64 = $checksum64
+  checksumType64= 'sha256' 
+}
+
+$arguments = @{}
+
+$packageParameters = $env:chocolateyPackageParameters
+
+if ($packageParameters) {
+    $match_pattern = "\/(?<option>([a-zA-Z]+)):(?<value>([`"'])?([a-zA-Z0-9- _\\:\.]+)([`"'])?)|\/(?<option>([a-zA-Z]+))"
+    $option_name = 'option'
+    $value_name = 'value'
+
+    if ($packageParameters -match $match_pattern ){
+        $results = $packageParameters | Select-String $match_pattern -AllMatches
+        $results.matches | ForEach-Object {
+        $arguments.Add(
+            $_.Groups[$option_name].Value.Trim(),
+            $_.Groups[$value_name].Value.Trim())
+    }
+    }
+    else
+    {
+        Throw "Package Parameters were found but were invalid (REGEX Failure)"
+    }
+
+    # http://help.tracker-software.com/EUM/default.aspx?pageid=PDFXEdit3:switches_for_msi_installers
+    $customArguments = @{}
+
+    if ($arguments.ContainsKey("NoDesktopShortcuts")) {
+        Write-Host "You want NoDesktopShortcuts"
+        $customArguments.Add("DESKTOP_SHORTCUTS", "0")
+    }
+
+    if ($arguments.ContainsKey("NoUpdater")) {
+        Write-Host "You want NoUpdater"
+        $customArguments.Add("NOUPDATER", "1")
+    }
+
+    if ($arguments.ContainsKey("NoViewInBrowsers")) {
+        Write-Host "You want NoViewInBrowsers"
+        $customArguments.Add("VIEW_IN_BROWSERS", "0")
+    }
+
+    if ($arguments.ContainsKey("NoSetAsDefault")) {
+        Write-Host "You want NoSetAsDefault"
+        $customArguments.Add("SET_AS_DEFAULT", "0")
+    }
+
+    if ($arguments.ContainsKey("NoProgramsMenuShortcuts")) {
+        Write-Host "You want NoProgramsMenuShortcuts"
+        $customArguments.Add("PROGRAMSMENU_SHORTCUTS", "0")
+    }
+
+    if ($arguments.ContainsKey("KeyFile")) {
+        if ($arguments["KeyFile"] -eq "") {
+          Throw 'KeyFile needs a colon-separated argument; try something like this: --params "/KeyFile:C:\Users\foo\Temp\PDFXChangeEditor.xcvault".'
+        } else {
+          Write-Host "You want a KeyFile named $($arguments["KeyFile"])"
+          $customArguments.Add("KEYFILE", $arguments["KeyFile"])
+        }
+    }
+
+} else {
+    Write-Debug "No Package Parameters Passed in"
+}
+
+if ($customArguments.Count) { 
+    $packageArgs.silentArgs += " " + (($customArguments.GetEnumerator() | ForEach-Object { "$($_.Name)=$($_.Value)" } ) -join " ")
+}
+
+Install-ChocolateyPackage @packageArgs
