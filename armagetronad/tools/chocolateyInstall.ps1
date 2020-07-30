ec54994772b75fbe614a0ac2d9a21cebe746599a
Chocolatey <chocolatey@realdimensions.net>
2020-07-30 08:06:12 +0000

    AU: 1 updated - armagetronad [skip ci] https://gist.github.com/eae7c31972662d6ea5f024c6c89a5e90/40698b70266c6ce10883df8b9ec8c7fb3da1212d

--- armagetronad/tools/chocolateyInstall.ps1
+++ armagetronad/tools/chocolateyInstall.ps1
@@ -5,11 +5,11 @@ $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $toolsDir
   fileType      = 'exe'
-  url           = 'https://sourceforge.net/projects/armagetronad/files/stable/0.2.8.3.4/armagetronad-0.2.8.3.4.gcc.win32.exe/download'
+  url           = 'https://sourceforge.net/projects/armagetronad/files/stable/0.2.9.0/armagetronad-0.2.9.0.gcc.win32.exe/download'
 
   softwareName  = 'Armagetron Advanced*'
 
-  checksum      = '5279281ab664f4a17dd7a1842c29da210d0026ab9ee5300bd714fe14c8356d15'
+  checksum      = '7fe67585fffb4fe110e43888852ef8d832ee5474f105271b2dc99a2cb875e050'
   checksumType  = 'sha256'
 
   silentArgs    = "/S"
