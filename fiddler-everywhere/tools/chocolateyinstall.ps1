839dfd01b7cb0dbe315669660411872970082710
Curtis Carter <lokiphasma@gmail.com>
2020-09-01 11:13:31 -0500

    Added Fiddler Everywhere

---
+++ fiddler-everywhere/tools/chocolateyinstall.ps1
@@ -0,0 +1,13 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$InstallArgs = @{
+    PackageName    = $env:ChocolateyPackageName
+    FileType       = 'exe'
+    SilentArgs     = '/S'
+    URL            = 'https://downloads.getfiddler.com/win/Fiddler%20Everywhere%201.0.2.exe'
+    Checksum       = '50b5bdd4f6bfc935f61b280b163cc1d29484c2bda9162520489003bc19e5e5a00b9562817ec7faa36e36bde259883b614b8838e349f91eabdb5a4a5803a99405'
+    ChecksumType   = 'sha512'
+}
+
+Install-ChocolateyPackage @InstallArgs
+
---
+++ fiddler-everywhere/update.ps1
@@ -0,0 +1,21 @@
+﻿import-module au
+
+$releases = "https://github.com/Tichau/FileConverter/releases"
+
+function global:au_SearchReplace {
+  @{
+    ".\tools\chocolateyInstall.ps1" = @{
+      "(Url\s*=\s*)('.*')"            = "`$1'$($Latest.URL)'"
+      "(Checksum\s*=\s*)('.*')"       = "`$1'$($Latest.Checksum32)'"
+      "(ChecksumType\s*=\s*)('.*')"   = "`$1'$($Latest.ChecksumType32)'"
+    }
+  }
+}
+
+function global:au_GetLatest {
+  $version = '1.0.2'
+  $url = "https://downloads.getfiddler.com/win/Fiddler%20Everywhere%20$version.exe"
+  return @{ Version = $version; URL = $url; ChecksumType32 = 'sha512'; }
+}
+
+Update-Package -ChecksumFor all
