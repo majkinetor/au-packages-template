31a676b96bb787a9c062bd568ccb25385b04a2b4
Chocolatey <chocolatey@realdimensions.net>
2020-09-15 08:06:33 +0000

    AU: 2 updated - kind oh-my-posh [skip ci]

--- kind/tools/chocolateyinstall.ps1
+++ kind/tools/chocolateyinstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop';
 
-$url64 = 'https://github.com/kubernetes-sigs/kind/releases/download/v0.8.1/kind-windows-amd64'
-$checksum64 = '2604d78bb2727a74bb2cf253acf3b132e54bcdcb6d5c5e22ce66d8f5c470b9464484011fdbbe7a7c6eb8730f390421e1752885202d89536b031afaaa12efb197'
+$url64 = 'https://github.com/kubernetes-sigs/kind/releases/download/v0.9.0/kind-windows-amd64'
+$checksum64 = '81d9844091bdd96c499b1c4f8d6dbff9a01aa328af16471730a6b9c122beb59b076e1451681d0eb90040adf5a01dcfeb686da3a447c47e56fa7e0766882cc683'
 $checksumType64 = 'sha512'
 $File = Join-Path (Join-Path $env:ChocolateyInstall (Join-Path 'lib' $env:ChocolateyPackageName)) 'kind.exe'
 $file = Get-ChocolateyWebFile -PackageName $env:ChocolateyPackageName `
