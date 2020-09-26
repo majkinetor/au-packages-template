d9ec191050ef0629b917129061139ed85eca32f7
Curtis Carter <lokiphasma@gmail.com>
2020-09-25 20:50:23 -0500

    Updated SideQuest Repo

--- sidequest/tools/chocolateyinstall.ps1
+++ sidequest/tools/chocolateyinstall.ps1
@@ -4,8 +4,8 @@ $InstallArgs = @{
     PackageName = $env:ChocolateyPackageName
     FileType = 'exe'
     SilentArgs = '/S'
-    URL64 = 'https://github.com/the-expanse/SideQuest/releases/download/v0.10.11/SideQuest-Setup-0.10.11-x64-win.exe'
-    Checksum64 = '55e6addd0a85ff36c47664d622152ce88ad8582f7c2326c8549bf115f59c641cdd4a7eea01c268cb27509e29a8458490d204a157b0dd1cf21105dfe8ecad3bf8'
+    URL64 = 'https://github.com/SideQuestVR/SideQuest/releases/download/v0.10.14/SideQuest-Setup-0.10.14-x64-win.exe'
+    Checksum64 = ''
     ChecksumType64 = 'sha512'
 }
 
--- sidequest/update.ps1
+++ sidequest/update.ps1
@@ -1,6 +1,6 @@
 ﻿import-module au
 
-$releases = "https://github.com/the-expanse/SideQuest/releases"
+$releases = "https://github.com/SideQuestVR/SideQuest/releases"
 
 function global:au_SearchReplace {
   @{
@@ -14,10 +14,10 @@ function global:au_SearchReplace {
 
 function global:au_GetLatest {
   $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
-  $regex   = '/the-expanse/SideQuest/releases/download/v[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\/SideQuest-Setup-[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}-x64-win\.exe$'
+  $regex   = '/SideQuestVR/SideQuest/releases/download/v[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\/SideQuest-Setup-[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}-x64-win\.exe$'
   $url     = $download_page.links | Where-Object href -match $regex | Select-Object -First 1 -expand href
   $version = $url -split '\/|-' | Select-Object -Last 1 -Skip 2
-  $url = "https://github.com/the-expanse/SideQuest/releases/download/v$version/SideQuest-Setup-$version-x64-win.exe"
+  $url = "https://github.com/SideQuestVR/SideQuest/releases/download/v$version/SideQuest-Setup-$version-x64-win.exe"
   return @{ Version = $version; URL64 = $url; ChecksumType64 = 'sha512';}
 }
 
