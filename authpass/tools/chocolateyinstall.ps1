5960f64f3ffe60aad91fe447ec646d00ca8ccaa1
Curtis Carter <lokiphasma@gmail.com>
2020-10-02 21:08:27 -0500

    Added authpass

---
+++ authpass/tools/chocolateyinstall.ps1
@@ -0,0 +1,12 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$args = @{ 
+  PackageName = $env:ChocolateyPackageName
+  FileType = 'exe'
+  SilentArgs = '/VERYSILENT'
+  Url64bit = 'https://github.com/authpass/authpass/releases/download/v1.7.8/AuthPass-setup-1.7.8_1552.exe'
+  Checksum64 = 'd0285be9f807e30a778052fb7f6ad10870fc537f10844b90020fd7cc90a059dd7aa54f02cd867ca585a1a1af56c408edc75d7a0080ba95b28248fc809775e358'
+  ChecksumType64 = 'sha512'
+}
+
+Install-ChocolateyPackage  @args
