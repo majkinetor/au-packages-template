a87c1f987f1b4e7afbc2596f6d8021f07a4bef28
Curtis Carter <lokiphasma@gmail.com>
2020-10-02 22:01:51 -0500

    Added ferdi

---
+++ ferdi/tools/chocolateyinstall.ps1
@@ -0,0 +1,12 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$args = @{ 
+  PackageName = $env:ChocolateyPackageName
+  FileType = 'exe'
+  SilentArgs = '/S'
+  Url64bit = 'https://github.com/getferdi/ferdi/releases/download/v5.5.0/Ferdi-Setup-5.5.0.exe'
+  Checksum64 = '3c7ca5709122056f19d824d9e3f7ee3ac890f423d7d260cf4e343f7e397473c661abfdb788fb0661c49013cf575d9b77ea672aad6841f47c53ab0472084d2fe9'
+  ChecksumType64 = 'sha512'
+}
+
+Install-ChocolateyPackage  @args
---
+++ ferdi/update.ps1
@@ -0,0 +1,26 @@
+﻿import-module au
+
+$releases = "https://github.com/getferdi/ferdi/releases"
+
+function global:au_SearchReplace {
+  @{
+    ".\tools\chocolateyInstall.ps1" = @{
+      "(Url64bit\s*=\s*)('.*')"      = "`$1'$($Latest.URL64)'"
+      "(Checksum64\s*=\s*)('.*')" = "`$1'$($Latest.Checksum64)'"
+      "(ChecksumType64\s*=\s*)('.*')" = "`$1'$($Latest.ChecksumType64)'"
+    }
+  }
+}
+
+function global:au_GetLatest {
+  $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
+  $regex   = '\/getferdi\/ferdi\/releases\/download\/v\d{1,4}\.\d{1,4}\.\d{1,4}.*\/Ferdi-Setup-.*\.exe$'
+  $url     = $download_page.links | Where-Object href -match $regex | Select-Object -First 1 -expand href
+  $version = $url -split '\/|v' | Select-Object -Last 1 -Skip 1
+  $url = "https://github.com$url"
+  $releaseNotes = "https://github.com/getferdi/ferdi/releases/tag/v$version"
+
+  return @{ Version = $version; URL64 = $url; ChecksumType64 = 'sha512'; ReleaseNotes = $releaseNotes}
+}
+
+Update-Package -ChecksumFor 64
