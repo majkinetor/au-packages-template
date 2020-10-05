f9e7a3a0ee03e464b9d9f57176edd7c749b86617
Curtis Carter <lokiphasma@gmail.com>
2020-10-04 21:56:33 -0500

    Added smmdb-client
    Added SMMDB-Client

---
+++ smmdb-client/tools/chocolateyinstall.ps1
@@ -0,0 +1,41 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$UnzipLocation = Join-Path $env:ChocolateyInstall (Join-Path 'lib' $env:ChocolateyPackageName)
+
+
+$ChocoWebFileArgs = @{
+    PackageName = $env:ChocolateyPackageName
+    FileFullPath = (Join-Path $UnzipLocation 'smmdb-client-windows.tar.gz')
+    Url = 'https://github.com/Tarnadas/smmdb-client/releases/download/0.1.2/smmdb-client-windows.tar.gz'
+    CheckSum = '282448ee0da7eaac0a6d04b43209a203c369d965adb9eb56a8f4b53c660d792746082ffb6f8f46f361d5d04e9c46eb196184d40382741b0827bfc08225856f4f'
+    CheckSumType = 'sha512'
+}
+
+Get-ChocolateyWebFile @ChocoWebFileArgs
+ 
+$UnGzArgs = @{
+    FileFullPath = (Join-Path $UnzipLocation 'smmdb-client-windows.tar.gz')
+    PackageName = $env:ChocolateyPackageName
+    Destination = $UnzipLocation
+  }
+  
+  Get-ChocolateyUnzip @UnGzArgs
+  
+  Remove-Item -Path (Join-Path $UnzipLocation 'smmdb-client-windows.tar.gz')
+  Remove-Item -Path (Join-Path $UnzipLocation 'smmdb-client-windows.tar.gz.txt')
+  
+  
+  $UnTarArgs = @{
+    FileFullPath = (Join-Path $UnzipLocation 'smmdb-client-windows.tar')
+    PackageName = $env:ChocolateyPackageName
+    Destination = $UnzipLocation
+  }
+  
+  Get-ChocolateyUnzip @UnTarArgs
+  
+  Remove-Item -Path (Join-Path $UnzipLocation 'smmdb-client-windows.tar')
+  Remove-Item -Path (Join-Path $UnzipLocation 'smmdb-client-windows.tar.txt')
+
+  "Run `smmdb` to start"
+  
+  
---
+++ smmdb-client/update.ps1
@@ -0,0 +1,23 @@
+﻿import-module au
+$releases = "https://github.com/Tarnadas/smmdb-client/releases"
+
+function global:au_SearchReplace {
+  @{
+    ".\tools\chocolateyInstall.ps1" = @{
+      "(URL\s*=\s*)('.*')"      = "`$1'$($Latest.URL)'"
+      "(CheckSum\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
+      "(CheckSumType\s*=\s*)('.*')" = "`$1'$($Latest.ChecksumType32)'"
+    }
+  }
+}
+
+function global:au_GetLatest {
+  $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
+  $regex   = "\/Tarnadas\/smmdb-client\/releases\/download\/\d{1,3}\.\d{1,3}\.\d{1,3}\/smmdb-client-windows.tar.gz$"
+  $url     = $download_page.links | Where-Object href -match $regex | Select-Object -First 1 -expand href
+  $version = $url -split '\/' | Select-Object -Last 1 -Skip 1
+  $url32 = "https://github.com$url"
+  return @{ Version = $version; URL = $url32; ChecksumType32 = 'sha512'; ReleaseNotes = $releaseNotes;}
+}
+
+Update-Package -ChecksumFor all
