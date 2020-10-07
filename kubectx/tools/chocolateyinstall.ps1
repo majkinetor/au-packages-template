f356235e786c66736769ee0169e49d133acf440a
Curtis Carter <lokiphasma@gmail.com>
2020-10-07 15:42:40 -0500

    Added Kubectx

---
+++ kubectx/tools/chocolateyinstall.ps1
@@ -0,0 +1,11 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$ZipInstallArgs = @{
+    PackageName = $env:ChocolateyPackageName 
+    UnzipLocation = Join-Path $env:ChocolateyInstall (Join-Path 'lib' $env:ChocolateyPackageName)
+    Url64 = ''
+    CheckSum64 = ''
+    CheckSumType64 = ''
+}
+
+Install-ChocolateyZipPackage @ZipInstallArgs
