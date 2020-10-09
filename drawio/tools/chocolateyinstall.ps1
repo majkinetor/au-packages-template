4ae4deab82f2bac960b6f16176b795dad362ed03
Chocolatey <chocolatey@realdimensions.net>
2020-10-09 00:07:03 +0000

    AU: 1 updated - drawio [skip ci]

--- drawio/tools/chocolateyinstall.ps1
+++ drawio/tools/chocolateyinstall.ps1
@@ -1,6 +1,6 @@
 ﻿$ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$drawioversion = '13.7.8'
+$drawioversion = '13.7.9'
 $url        = "https://github.com/jgraph/drawio-desktop/releases/download/v$drawioversion/draw.io-$drawioversion-windows-installer.exe"
 
 $packageArgs = @{
@@ -10,7 +10,7 @@ $packageArgs = @{
   #file          = "$toolsDir/draw.io-11.1.4-windows-installer.exe"
   url           = $url
   softwareName  = 'drawio*'
-  checksum      = '3cbca9eeb791ba563bf8a832227f6bec4ff366b3e9347e3ea23a2a6f645fa4cfaf8ac6dc36198844878ee31e8791ae3a539e190f37d06445554bd0efe8b17a41'
+  checksum      = '74a8be808050711e5606219c34104693722104f13c3822488ff0e0c9d2255ffe27f47e86a0cc430444f5b64300e0acbf29b98e3dc0cad11cab61095e1d9383a6'
   checksumType  = 'sha512'
   silentArgs   = '/S'
 }
