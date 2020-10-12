8f48a8ba1d9f089e870cda3ccf6b37e677d85177
Curtis Carter <lokiphasma@gmail.com>
2020-10-12 14:26:17 -0500

    Adding jcli back

---
+++ jcli/tools/chocolateyinstall.ps1
@@ -0,0 +1,17 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$UnzipLocation = Join-Path $env:ChocolateyInstall (Join-Path 'lib' $env:ChocolateyPackageName)
+
+
+$ChocolateyInstallZipArguments = @{
+    PackageName = $env:ChocolateyPackageName
+    UnzipLocation = Join-Path $env:ChocolateyInstall (Join-Path 'lib' $env:ChocolateyPackageName)
+    Url = ''
+    Url64 = ''
+    CheckSum = ''
+    CheckSum64 = ''
+    CheckSumType = ''
+    CheckSumType64 = ''
+}
+
+Install-ChocolateyZipPackage @ChocolateyInstallZipArguments
---
+++ jcli/update.ps1
@@ -0,0 +1,29 @@
+﻿import-module au
+$releases = "https://github.com/jenkins-zh/jenkins-cli/releases"
+
+function global:au_SearchReplace {
+  @{
+    ".\tools\chocolateyInstall.ps1" = @{
+      "(Url\s*=\s*)('.*')"            = "`$1'$($Latest.URL)'"
+      "(Url64\s*=\s*)('.*')"          = "`$1'$($Latest.URL64)'"
+      "(CheckSum\s*=\s*)('.*')"       = "`$1'$($Latest.Checksum32)'"     
+      "(CheckSum64\s*=\s*)('.*')"     = "`$1'$($Latest.Checksum64)'"
+      "(CheckSumType\s*=\s*)('.*')"   = "`$1'$($Latest.ChecksumType32)'"
+      "(CheckSumType64\s*=\s*)('.*')" = "`$1'$($Latest.ChecksumType64)'"
+    }
+  }
+}
+
+function global:au_GetLatest {
+  $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
+  $regex   = "\/jenkins-zh\/jenkins-cli\/tree\/v\d{1,3}\.\d{1,3}\.\d{1,3}"
+  $url     = $download_page.links | Where-Object href -match $regex | Select-Object -First 1 -expand href
+  $version = $url -split '\/' | Select-Object -Last 1
+  $version = $version -split 'v' | Select-Object -Last 1
+  $url32 = "https://github.com/jenkins-zh/jenkins-cli/releases/download/v$version/jcli-windows-386.zip"
+  $url64 = "https://github.com/jenkins-zh/jenkins-cli/releases/download/v$version/jcli-windows-amd64.zip"
+
+  return @{ Version = $version; URL = $url32; URL64 = $url64; ChecksumType64 = 'sha512'; ChecksumType32 = 'sha512';}
+}
+
+Update-Package -ChecksumFor all
