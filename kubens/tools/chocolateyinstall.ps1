1fc5183879a536d05c2ba0d6478498894d6eae46
Curtis Carter <lokiphasma@gmail.com>
2020-10-12 14:26:04 -0500

    Updating kubens

--- kubens/tools/chocolateyinstall.ps1
+++ kubens/tools/chocolateyinstall.ps1
@@ -3,7 +3,7 @@
 $ZipInstallArgs = @{
     PackageName = $env:ChocolateyPackageName 
     UnzipLocation = Join-Path $env:ChocolateyInstall (Join-Path 'lib' $env:ChocolateyPackageName)
-    Url64 = 'https://github.com/ahmetb/kubectx/releases/download/v0.9.1/kubectx_v0.9.1_windows_x86_64.zip'
+    Url64 = 'https://github.com/ahmetb/kubectx/releases/download/v0.9.1/kubens_v0.9.1_windows_x86_64.zip'
     CheckSum64 = ''
     CheckSumType64 = 'sha512'
 }
