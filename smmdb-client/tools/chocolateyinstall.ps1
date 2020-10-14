0a3d33be1c1004218b49fa28ac6df1bda736da31
Chocolatey <chocolatey@realdimensions.net>
2020-10-14 08:06:44 +0000

    AU: 1 updated - smmdb-client [skip ci]

--- smmdb-client/tools/chocolateyinstall.ps1
+++ smmdb-client/tools/chocolateyinstall.ps1
@@ -6,8 +6,8 @@ $UnzipLocation = Join-Path $env:ChocolateyInstall (Join-Path 'lib' $env:Chocolat
 $ChocoWebFileArgs = @{
     PackageName = $env:ChocolateyPackageName
     FileFullPath = (Join-Path $UnzipLocation 'smmdb-client-windows.tar.gz')
-    Url = 'https://github.com/Tarnadas/smmdb-client/releases/download/0.2.1/smmdb-client-windows.tar.gz'
-    CheckSum = '7eed82ff994916c4a4a128de96c0cc657ff5fcd23e63b513b58ac681a886c8eb1bf80195e3478df5d2ce1fe290cb712c350b7344df9a2833e1ec0e59ce7aa205'
+    Url = 'https://github.com/Tarnadas/smmdb-client/releases/download/0.2.2/smmdb-client-windows.tar.gz'
+    CheckSum = '66310a25afafaef7f02c694a321dbc7fe5e94261c1452ed3fae9772e51583f153f601e5d1589f0954d4c0cd55916529fe4be23ed251ff5e816d49003b636c7b2'
     CheckSumType = 'sha512'
 }
 
