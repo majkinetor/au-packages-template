24b217fca33bca5dbdd09e234309ce16e7115426
Chocolatey <chocolatey@realdimensions.net>
2020-10-22 16:06:41 +0000

    AU: 1 updated - buffalo [skip ci]

--- buffalo/tools/chocolateyinstall.ps1
+++ buffalo/tools/chocolateyinstall.ps1
@@ -6,10 +6,10 @@ $UnzipLocation = Join-Path $env:ChocolateyInstall (Join-Path 'lib' $env:Chocolat
 $ChocoWebFileArgs = @{
     PackageName = $env:ChocolateyPackageName
     FileFullPath = (Join-Path $UnzipLocation 'buffalo.tar.gz')
-    Url = 'https://github.com/gobuffalo/buffalo/releases/download/v0.16.15/buffalo_0.16.15_windows_i386.tar.gz'
-    Url64bit = 'https://github.com/gobuffalo/buffalo/releases/download/v0.16.15/buffalo_0.16.15_windows_x86_64.tar.gz'
-    CheckSum = '1a00a561116e5e351add98ad9565400f8bc192f457f4b9761d7e232715c30fa364f9bd7c8d15b6294c155337b6f5ac15bdfb056dec35e6e4ea25c32d5d603e60'
-    Checksum64 = '4fd7e15e90402b8295a8960dcec4f16e20332e4395b46a5db268f8218c0eee56dcbd05a52db2fd0cb536087251513acb6d492435d7ea99bbdccb33211863339a'
+    Url = 'https://github.com/gobuffalo/buffalo/releases/download/v0.16.16/buffalo_0.16.16_windows_i386.tar.gz'
+    Url64bit = 'https://github.com/gobuffalo/buffalo/releases/download/v0.16.16/buffalo_0.16.16_windows_x86_64.tar.gz'
+    CheckSum = 'd22647a3e75c11dee5889d121c787c6a136cb74fee488a909808c454397e762dddb7a4364f71acea6945cd603bc6bc6e8fa0a5222c059682f121dd9d681ea507'
+    Checksum64 = '700e99523b1eb943ec9188e868efbf4e0090807eda2ee7910d76a222b10b3afbc02ddbe2eac5b5b6e2d73671c4ea70cff5760b235a51f7c5d90b2f648357c736'
     CheckSumType = 'sha512'
     ChecksumType64 = 'sha512'
 }
