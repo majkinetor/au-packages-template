1ccb4856ebd305317799ac0651f6ff8bf3be5c79
Curtis Carter <lokiphasma@gmail.com>
2020-11-02 18:11:18 -0600

    Added bit-git
    Added Bit

---
+++ bit-git/tools/chocolateyinstall.ps1
@@ -0,0 +1,42 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$UnzipLocation = Join-Path $env:ChocolateyInstall (Join-Path 'lib' $env:ChocolateyPackageName)
+
+
+$ChocoWebFileArgs = @{
+    PackageName = $env:ChocolateyPackageName
+    FileFullPath = (Join-Path $UnzipLocation 'bit-git.tar.gz')
+    Url = ''
+    Url64bit = ''
+    CheckSum = ''
+    Checksum64 = ''
+    CheckSumType = ''
+    ChecksumType64 = ''
+}
+
+Get-ChocolateyWebFile @ChocoWebFileArgs
+ 
+$UnGzArgs = @{
+    FileFullPath = (Join-Path $UnzipLocation 'bit-git.tar.gz')
+    PackageName = $env:ChocolateyPackageName
+    Destination = $UnzipLocation
+  }
+  
+  Get-ChocolateyUnzip @UnGzArgs
+  
+  Remove-Item -Path (Join-Path $UnzipLocation 'bit-git.tar.gz')
+  Remove-Item -Path (Join-Path $UnzipLocation 'bit-git.tar.gz.txt')
+  
+  
+  $UnTarArgs = @{
+    FileFullPath = (Join-Path $UnzipLocation 'bit-git.tar')
+    PackageName = $env:ChocolateyPackageName
+    Destination = $UnzipLocation
+  }
+  
+  Get-ChocolateyUnzip @UnTarArgs
+  
+  Remove-Item -Path (Join-Path $UnzipLocation 'bit-git.tar')
+  Remove-Item -Path (Join-Path $UnzipLocation 'bit-git.tar.txt')
+  
+  
---
+++ bit-git/update.ps1
@@ -0,0 +1,29 @@
+﻿import-module au
+$releases = "https://github.com/chriswalz/bit/releases"
+
+function global:au_SearchReplace {
+  @{
+    ".\tools\chocolateyInstall.ps1" = @{
+      "(URL\s*=\s*)('.*')"      = "`$1'$($Latest.URL)'"
+      "(Url64bit\s*=\s*)('.*')"      = "`$1'$($Latest.URL64)'"
+      "(CheckSum\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
+      "(CheckSum64\s*=\s*)('.*')" = "`$1'$($Latest.Checksum64)'"
+      "(CheckSumType\s*=\s*)('.*')" = "`$1'$($Latest.ChecksumType32)'"
+      "(CheckSumType64\s*=\s*)('.*')" = "`$1'$($Latest.ChecksumType64)'"
+    }
+  }
+}
+
+function global:au_GetLatest {
+  $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
+  $regex   = "\/chriswalz\/bit\/releases\/download\/v\d{1,3}\.\d{1,3}\.\d{1,3}\/bit_\d{1,3}\.\d{1,3}\.\d{1,3}_windows_amd64.tar.gz$"
+  $url     = $download_page.links | Where-Object href -match $regex | Select-Object -First 1 -expand href
+  $version = $url -split '\/' | Select-Object -Last 1 -Skip 1
+  $version = $version -split 'v' | Select-Object -Last 1
+  $url32 = "$releases/download/v$version/bit_$($version)_windows_386.tar.gz"  
+  $url64 = "$releases/download/v$version/bit_$($version)_windows_amd64.tar.gz"
+  $ReleaseNotesVersion = $version -replace '\.', ''
+  return @{ Version = $version; URL = $url32; URL64 = $url64; ChecksumType32 = 'sha512'; ChecksumType64 = 'sha512'; ReleaseNotes = $releaseNotes;}
+}
+
+Update-Package -ChecksumFor all
