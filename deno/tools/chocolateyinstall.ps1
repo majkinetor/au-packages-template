926087079ac09585ff7c5627e5220c098de69f2d
Chocolatey <chocolatey@realdimensions.net>
2020-11-16 16:06:39 +0000

    AU: 2 updated - deno drawio [skip ci]

--- deno/tools/chocolateyinstall.ps1
+++ deno/tools/chocolateyinstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop';
 
-$url64 = 'https://github.com/denoland/deno/releases/download/v1.5.2/deno-x86_64-pc-windows-msvc.zip'
-$checksum64 = '5ff8cf0869e150e728657643f22dc82370afa9060426d7fc8df3aa3c111817fcb70f309a10fe50085849e5d72d1a0dd0174e3811dbc720f6b2baaa4dd85ed6f4'
+$url64 = 'https://github.com/denoland/deno/releases/download/v1.5.3/deno-x86_64-pc-windows-msvc.zip'
+$checksum64 = '7c125cf6c3aedf9f9e8dc37f7736cb50ad610b1bb13a297a14967c2a34112d2f1c330d83df0f027f24a8c7f67f2ea2d4161e48c79eacae196a60d0e517d37deb'
 $checksumType64 = 'sha512'
 $UnzipLocation = Join-Path $env:ChocolateyInstall (Join-Path 'lib' $env:ChocolateyPackageName)
 Install-ChocolateyZipPackage -PackageName $env:ChocolateyPackageName -Url64 $url64 -UnzipLocation $UnzipLocation -CheckSum64 $checksum64 -CheckSumType64 $checksumType64
--- drawio/tools/chocolateyinstall.ps1
+++ drawio/tools/chocolateyinstall.ps1
@@ -1,6 +1,6 @@
 ﻿$ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$drawioversion = '13.9.8'
+$drawioversion = '13.9.9'
 $url        = "https://github.com/jgraph/drawio-desktop/releases/download/v$drawioversion/draw.io-$drawioversion-windows-installer.exe"
 
 $packageArgs = @{
@@ -10,7 +10,7 @@ $packageArgs = @{
   #file          = "$toolsDir/draw.io-11.1.4-windows-installer.exe"
   url           = $url
   softwareName  = 'drawio*'
-  checksum      = '041a203ff2b47b29534c1deadf6cc14f9da302cb368643cfa365cb5e56011b5d90eed4a457407b47a3ce72d96ad25464f4d10a2a66334147ef2aef7a6390dc41'
+  checksum      = '00b08fda74d68c62867de01059f45a246a8294e534f36b40b6b5602bf2371ebebe800aa0260625a3ae7922a8c8e3289ba683c0a5d2db581e39c591e028e192ae'
   checksumType  = 'sha512'
   silentArgs   = '/S'
 }
