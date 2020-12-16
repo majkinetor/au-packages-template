38cca85891bcbf4de7e26a3ae1f3752cf21448ce
Chocolatey <chocolatey@realdimensions.net>
2020-12-16 16:06:38 +0000

    AU: 1 updated - dip [skip ci]

--- dip/tools/chocolateyinstall.ps1
+++ dip/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@
 $InstallArgs = @{
     PackageName = $env:ChocolateyPackageName
     FileFullPath = Join-Path (Join-Path $env:ChocolateyInstall (Join-Path 'lib' $env:ChocolateyPackageName)) 'dip.exe'
-    URL = 'https://github.com/030/dip/releases/download/2.0.1/dip-windows'
-    Checksum = '711254807ffa7986234b9a9139c9ddc58ac11d524f7ac210026b3b4229b79503a91b8a5b35e6a812f96512beeac6d664e00dfc14d13896b16a3c77aa4d4736f9'
+    URL = 'https://github.com/030/dip/releases/download/2.0.2/dip-windows'
+    Checksum = '5f5336acf6baa44ae2e1f426036254d505db96b4d2052f0e532e5360e5841c2fbd04a883e66b18431f5d8a09de736d4fddf45f2553aec19a5601e5e1f0d8d99e'
     ChecksumType = 'sha512'
 }
 
