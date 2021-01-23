db6f223beab342498b99bc6647882603e38c2a40
Curtis Carter <lokiphasma@gmail.com>
2021-01-23 13:15:33 -0600

    Added nethermind
    Added Nethermind Package

---
+++ nethermind/tools/chocolateyinstall.ps1
@@ -0,0 +1,12 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$ZipInstallArgs = @{
+    PackageName = $env:ChocolateyPackageName 
+    UnzipLocation = Join-Path $env:ChocolateyInstall (Join-Path 'lib' $env:ChocolateyPackageName)
+    Url64 = 'https://github.com/biome-sh/biome/releases/download/bio-1.5.75/bio-1.5.75-x86_64-windows.zip'
+    CheckSum64 = 'da00eb524d253f42fac1adbf271f4cac5c9d61f6f20a3be97f3b441618dc13cd2a6999cb0e5a153f664929989a3ec8a7bf4d153bd2d75ab338fb2d124ce6284c'
+    CheckSumType64 = 'sha512'
+}
+
+Install-ChocolateyZipPackage @ZipInstallArgs
+Write-Output "Run 'Nethermind.Launcher.exe' and select the desired configuration to get started"
---
+++ nethermind/update.ps1
@@ -0,0 +1,24 @@
+﻿import-module au
+
+$releases = "https://github.com/NethermindEth/Nethermind/releases"
+
+function global:au_SearchReplace {
+  @{
+    ".\tools\chocolateyInstall.ps1" = @{
+      "(Url64\s*=\s*)('.*')"      = "`$1'$($Latest.URL64)'"
+      "(CheckSum64\s*=\s*)('.*')" = "`$1'$($Latest.Checksum64)'"
+      "(CheckSumType64\s*=\s*)('.*')" = "`$1'$($Latest.ChecksumType64)'"
+    }
+  }
+}
+
+function global:au_GetLatest {
+  $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
+  $regex   = '\/NethermindEth\/nethermind\/releases\/download\/\d{1,3}\.\d{1,3}\.\d{1,3}.*\/nethermind-windows-amd64-\d{1,3}\.\d{1,3}\.\d{1,3}.*\.zip$'
+  $url     = $download_page.links | Where-Object href -match $regex | Select-Object -First 1 -expand href
+  $version = $url -split '\/' | Select-Object -Last 1 -Skip 1
+  $url = "https://github.com$url"
+  return @{ Version = $version; URL64 = $url; ChecksumType64 = 'sha512';}
+}
+
+Update-Package -ChecksumFor 64
