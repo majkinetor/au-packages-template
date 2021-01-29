a0db7aa0308ab6568b81358588bd5406e9b93553
Chocolatey <chocolatey@realdimensions.net>
2021-01-29 16:06:51 +0000

    AU: 2 updated - deno jcli [skip ci]

--- deno/tools/chocolateyinstall.ps1
+++ deno/tools/chocolateyinstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop';
 
-$url64 = 'https://github.com/denoland/deno/releases/download/v1.7.0/deno-x86_64-pc-windows-msvc.zip'
-$checksum64 = 'c45c25abfbb8ecb3dff914617dd55870efeaa43d7a90495f917287f45185d83637c4dc109776212427fb357f5d8a28f1778cd14e75e6cca76383481ec51b44ee'
+$url64 = 'https://github.com/denoland/deno/releases/download/v1.7.1/deno-x86_64-pc-windows-msvc.zip'
+$checksum64 = 'c6368396094fba43118deecfa1ea6633108879a99e8fb75eaaae2a2c888685afd67cd1c69c58adb5b508119c321b53bc0dc6e4214d7926a66fdda24c24c08ea1'
 $checksumType64 = 'sha512'
 $UnzipLocation = Join-Path $env:ChocolateyInstall (Join-Path 'lib' $env:ChocolateyPackageName)
 Install-ChocolateyZipPackage -PackageName $env:ChocolateyPackageName -Url64 $url64 -UnzipLocation $UnzipLocation -CheckSum64 $checksum64 -CheckSumType64 $checksumType64
--- jcli/tools/chocolateyinstall.ps1
+++ jcli/tools/chocolateyinstall.ps1
@@ -6,8 +6,8 @@ $UnzipLocation = Join-Path $env:ChocolateyInstall (Join-Path 'lib' $env:Chocolat
 $ChocolateyInstallZipArguments = @{
     PackageName = $env:ChocolateyPackageName
     UnzipLocation = Join-Path $env:ChocolateyInstall (Join-Path 'lib' $env:ChocolateyPackageName)
-    Url64 = 'https://github.com/jenkins-zh/jenkins-cli/releases/download/v0.0.32/jcli-windows-amd64.zip'
-    CheckSum64 = '67a7bdc072dd85ba26f894e624a7dcb14f349c46789ea654e6bb05bb2a648fd4d7e6252b8958792edf3c17832f8baeae369391c006e347840f8ecfcf718b88dd'
+    Url64 = 'https://github.com/jenkins-zh/jenkins-cli/releases/download/v0.0.33/jcli-windows-amd64.zip'
+    CheckSum64 = '918ae27b603fd6117e21a3278195fc735b1708acb2e46400b3df0a0ef2704660e950ff179491d7937f7a7d8f2816824b0254b25a9e8a3f9045c197b52c370ddf'
     CheckSumType64 = 'sha512'
 }
 
