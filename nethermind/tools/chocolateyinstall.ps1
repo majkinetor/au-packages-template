2687dc6cd32075ed99c5bc47157ac59b9ce835d1
Chocolatey <chocolatey@realdimensions.net>
2021-02-18 00:06:54 +0000

    AU: 1 updated - nethermind [skip ci]

--- nethermind/tools/chocolateyinstall.ps1
+++ nethermind/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@
 $ZipInstallArgs = @{
     PackageName = $env:ChocolateyPackageName 
     UnzipLocation = Join-Path $env:ChocolateyInstall (Join-Path 'lib' $env:ChocolateyPackageName)
-    Url64 = 'https://github.com/biome-sh/biome/releases/download/bio-1.5.75/bio-1.5.75-x86_64-windows.zip'
-    CheckSum64 = 'da00eb524d253f42fac1adbf271f4cac5c9d61f6f20a3be97f3b441618dc13cd2a6999cb0e5a153f664929989a3ec8a7bf4d153bd2d75ab338fb2d124ce6284c'
+    Url64 = 'https://github.com/NethermindEth/nethermind/releases/download/1.10.22/nethermind-windows-amd64-1.10.22-75f43f0-20210217.zip'
+    CheckSum64 = '902d70c1530b66ce5d39cf118775f258af0f9b74aa0c3370bd75b56923bca9379a61d461c65b55dc9aae883ec65ab816a94107a3878a5417944df7ecd0a1e4cb'
     CheckSumType64 = 'sha512'
 }
 
