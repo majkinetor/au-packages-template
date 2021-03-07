56d6b9b23749c64796d8c3f28c36f76c606518e4
adehad <26027314+adehad@users.noreply.github.com>
2021-03-07 15:39:00 +0000

    update to new release tag spec

--- oh-my-posh/update.ps1
+++ oh-my-posh/update.ps1
@@ -9,9 +9,9 @@ function global:au_SearchReplace {
 
 function global:au_GetLatest {
   $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
-  $regex   = '\/JanDeDobbeleer\/oh-my-posh\/releases\/tag\/\d{1,3}\.\d{1,3}\.\d{1,3}$'
+  $regex   = '\/JanDeDobbeleer\/oh-my-posh\/releases\/tag\/v\d{1,3}\.\d{1,3}\.\d{1,3}$'
   $url     = $download_page.links | Where-Object href -match $regex | Select-Object -First 1 -expand href
-  $version = $url -split '\/' | Select-Object -Last 1
+  $version = $url -split '\/v' | Select-Object -Last 1
   Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
   Save-Module -RequiredVersion $version -Name 'oh-my-posh' -Force -Path './'
   Write-Host "Saved Module"
