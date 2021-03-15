a0b0594ba2b51fc86805c415e7463557e076a88b
Chocolatey <chocolatey@realdimensions.net>
2021-03-15 00:06:30 +0000

    AU: 1 updated - oh-my-posh [skip ci]

--- oh-my-posh/tools/chocolateyinstall.ps1
+++ oh-my-posh/tools/chocolateyinstall.ps1
@@ -4,8 +4,8 @@ $InstallArgs = @{
   PackageName = $env:ChocolateyPackageName
   FileType = 'exe'
   SilentArgs = '/VERYSILENT'
-  Url64bit = 'https://github.com/JanDeDobbeleer/oh-my-posh/releases/download/v3.112.1/install.exe'
-  Checksum64 = '34f1d186259bdf99a8b9c2496a4cd09616293b19142618f1a06e2e675450aee68af19a0f8372ddb46af42cd87a16684b7e7f65c26c57d1b263d941f39d4ce7fe'
+  Url64bit = 'https://github.com/JanDeDobbeleer/oh-my-posh/releases/download/v3.113.0/install.exe'
+  Checksum64 = '39c6293f13de7654bf6bfcd4f5d589278d2e9cdbdb02bc31faec9304b21c250f70567a5cb2fdf1225300004b4bffafb53c538af912d555b14d84d0ca92ae5219'
   ChecksumType64 = 'sha512'
 }
 
