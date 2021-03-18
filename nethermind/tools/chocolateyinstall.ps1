d4df0b76b14d01f017983e1b2b75c3b83e13f431
Chocolatey <chocolatey@realdimensions.net>
2021-03-18 16:06:47 +0000

    AU: 2 updated - nethermind oh-my-posh [skip ci]

--- nethermind/tools/chocolateyinstall.ps1
+++ nethermind/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@
 $ZipInstallArgs = @{
     PackageName = $env:ChocolateyPackageName 
     UnzipLocation = Join-Path $env:ChocolateyInstall (Join-Path 'lib' $env:ChocolateyPackageName)
-    Url64 = 'https://github.com/NethermindEth/nethermind/releases/download/1.10.44/nethermind-windows-amd64-1.10.44-557fe6b-20210316.zip'
-    CheckSum64 = '38bd1ef6bc8a5db04bd0f9860c7ea57ecc1a110d02b3f0bc3e93388f6b4e5a6a2b985a55533e2c1a1b059f4dde68a144ade0f2734416dcb06dea22e360f9355e'
+    Url64 = 'https://github.com/NethermindEth/nethermind/releases/download/1.10.46/nethermind-windows-amd64-1.10.46-0e01637-20210318.zip'
+    CheckSum64 = 'd2fe318862b89f21ef107cc1bbaeb15f5f1ae2a2164647f73c967e189d268823d58631f33a120dde165d1365368c292551a518d44eaeae429bbc6af2a61bb9e0'
     CheckSumType64 = 'sha512'
 }
 
--- oh-my-posh/tools/chocolateyinstall.ps1
+++ oh-my-posh/tools/chocolateyinstall.ps1
@@ -4,8 +4,8 @@ $InstallArgs = @{
   PackageName = $env:ChocolateyPackageName
   FileType = 'exe'
   SilentArgs = '/VERYSILENT'
-  Url64bit = 'https://github.com/JanDeDobbeleer/oh-my-posh/releases/download/v3.115.0/install.exe'
-  Checksum64 = '96c473dfe8de736eb48dc8380c8fd378e452593aca7344d39fdf129ba95ea301db170fec74920cf6713bdeccb03df6ef84893b3c6a05280807f35bc299b61cee'
+  Url64bit = 'https://github.com/JanDeDobbeleer/oh-my-posh/releases/download/v3.115.1/install.exe'
+  Checksum64 = 'a2263463a6b3d5d6d65f6cb96bcd9b693e401a4c2a38f35fe57bb38e87ec4103dec2231d5c06139d7c7a588baa684a529a1dff61a6803532b94856b09ad60ba7'
   ChecksumType64 = 'sha512'
 }
 
