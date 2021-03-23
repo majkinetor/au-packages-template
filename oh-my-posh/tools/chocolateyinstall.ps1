d106b5b5d1978d357b33380c7e846addc5a3cad3
Curtis Carter <lokiphasma@gmail.com>
2021-03-23 17:10:08 -0500

    Fixes #17
    Added Automatic Additions to Powershell Profile
    Added Theme Parameter
    Added Uninstaller to remove from Powershell Profile
    Added logic to remove Profile lines from V2 Chocolatey Package

--- oh-my-posh/tools/chocolateyinstall.ps1
+++ oh-my-posh/tools/chocolateyinstall.ps1
@@ -1,12 +1,64 @@
 ﻿$ErrorActionPreference = 'Stop';
+$pp = Get-PackageParameters
 
 $InstallArgs = @{ 
-  PackageName = $env:ChocolateyPackageName
-  FileType = 'exe'
-  SilentArgs = '/VERYSILENT'
-  Url64bit = 'https://github.com/JanDeDobbeleer/oh-my-posh/releases/download/v3.120.0/install.exe'
-  Checksum64 = '9c3ab3e7f33b527c90340219863b4275a4270465dcb944d5deeffe1740eaad63d57fd7eb800d225fb94a496fdc7a6ff6dc56861fba530b60cda38ed4c158372d'
+  PackageName    = $env:ChocolateyPackageName
+  FileType       = 'exe'
+  SilentArgs     = '/VERYSILENT'
+  Url64bit       = 'https://github.com/JanDeDobbeleer/oh-my-posh/releases/download/v3.120.0/install.exe'
+  Checksum64     = '9c3ab3e7f33b527c90340219863b4275a4270465dcb944d5deeffe1740eaad63d57fd7eb800d225fb94a496fdc7a6ff6dc56861fba530b60cda38ed4c158372d'
   ChecksumType64 = 'sha512'
 }
 
 Install-ChocolateyPackage  @InstallArgs
+
+if ($PROFILE -and (Test-Path $PROFILE)) {
+  $oldProfile = @(Get-Content $PROFILE)
+
+  $newProfile = @()
+  foreach ($line in $oldProfile) {
+    if ($line -like 'Import-Module posh-git') {
+      Write-Host "posh-git no longer required to be imported separately '$line' can be removed from profile"
+    }
+    if ($line -like 'Import-Module oh-my-posh' -or $line -like 'Invoke-Expression (oh-my-posh --init --shell pwsh*') {
+      if ($pp['Theme']) {
+        # If a theme is set, Overwrite old line to set new theme
+        if (Test-Path "$env:LocalAppDataPrograms/oh-my-posh/themes/$($p['Theme'])).omp.json") {
+          $ohMyPoshProfileLine = "Invoke-Expression (oh-my-posh --init --shell pwsh --config ""$env:LocalAppDataPrograms/oh-my-posh/themes/$($p['Theme'])).omp.json"")"
+          Write-Host "Overwriting Old Oh-My-Posh line: $line with $ohMyPoshProfileLine"
+          $newProfile += $ohMyPoshProfileLine;
+        }
+        else {
+          Throw "Could not find Theme $pp['Theme'] @ $env:LocalAppDataPrograms/oh-my-posh/themes/$($p['Theme'])).omp.json";
+        }
+      }
+      else {        
+        $OhMyPoshInProfile = $true
+      }
+    }      
+    else {
+      $newProfile += $line
+    }
+      
+  }
+  if (-not $OhMyPoshInProfile) {
+    if ($pp['Theme']) {
+      if (Test-Path "$env:LocalAppDataPrograms/oh-my-posh/themes/$($p['Theme'])).omp.json") {
+        $newProfile += "Invoke-Expression (oh-my-posh --init --shell pwsh --config ""$env:LocalAppDataPrograms/oh-my-posh/themes/$($p['Theme'])).omp.json"")";
+      }
+      else {
+        Throw "Could not find Theme $pp['Theme'] @ $env:LocalAppDataPrograms/oh-my-posh/themes/$($p['Theme'])).omp.json";
+      }
+    }
+    else {
+      $newProfile += 'Invoke-Expression (oh-my-posh --init --shell pwsh)`n';
+
+    }
+  }
+
+  Set-Content -path $profile -value $newProfile -Force
+  Write-Host "oh-my-posh has been added to your profile. You may wish to append 'Set-PoshPrompt paradox' to set a theme"
+}
+else {
+  Write-Host 'No Powershell Profile was found. You may wish to create a Profile and append ''Invoke-Expression (oh-my-posh --init --shell pwsh --config "$env:LocalAppDataPrograms/oh-my-posh/themes/themename.omp.json")'' to enable oh-my-posh. ''Get-PoshThemes'' will list available themes for you'
+}
---
+++ oh-my-posh/tools/chocolateyuninstall.ps1
@@ -0,0 +1,25 @@
+﻿#Remove added lines from Profile
+if ($PROFILE -and (Test-Path $PROFILE)) {
+    $oldProfile = @(Get-Content $PROFILE)
+
+    $newProfile = @()
+    foreach($line in $oldProfile) {
+        if($line -like 'Import-Module posh-git') {
+            Write-Host "'Import-Module posh-git' was found in your profile but is not being removed."
+        }
+        if ($line -like 'Import-Module oh-my-posh' -or $line -like 'Invoke-Expression (oh-my-posh --init --shell pwsh*') {
+            continue
+        }
+        if($line -like 'Set-PoshPrompt*') {
+            Write-Host "'$line' was setting a theme for your profile but is being removed."
+            continue
+        }
+        $newProfile += $line
+    }
+  
+    Set-Content -path $profile -value $newProfile -Force
+    Write-Host "oh-my-posh has been removed from your Powershell profile."
+  }
+  else{
+    Write-Host "No Powershell Profile was found. You may need to undo changes to your Powershell profile manually."
+  }
