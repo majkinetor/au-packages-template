900e8bb9d1192e5718762cc36146e20816f8ad8c
Aesara B <aesara.b@hotmail.com>
2021-04-13 21:43:58 +0930

    Preview final version release

--- manual/temp/yacreader/tools/chocolateyuninstall.ps1
+++ manual/temp/yacreader/tools/chocolateyuninstall.ps1
@@ -1,19 +1,7 @@
-﻿# IMPORTANT: Before releasing this package, copy/paste the next 2 lines into PowerShell to remove all comments from this file:
-#   $f='c:\path\to\thisFile.ps1'
-#   gc $f | ? {$_ -notmatch "^\s*#"} | % {$_ -replace '(^.*?)\s*?[^``]#.*','$1'} | Out-File $f+".~" -en utf8; mv -fo $f+".~" $f
-
-## NOTE: In 80-90% of the cases (95% with licensed versions due to Package Synchronizer and other enhancements),
-## AutoUninstaller should be able to detect and handle registry uninstalls without a chocolateyUninstall.ps1.
-## See https://chocolatey.org/docs/commands-uninstall
-## and https://chocolatey.org/docs/helpers-uninstall-chocolatey-package
-
-## If this is an MSI, ensure 'softwareName' is appropriate, then clean up comments and you are done.
-## If this is an exe, change fileType, silentArgs, and validExitCodes
-
-$ErrorActionPreference = 'Stop'; # stop on all errors
+﻿$ErrorActionPreference = 'Stop';
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
-  softwareName  = 'YACReader v*'  #part or all of the Display Name as you see it in Programs and Features. It should be enough to be unique
+  softwareName  = 'YACReader v*'
   fileType      = 'exe'
   silentArgs    = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
   validExitCodes= @(0, 3010, 1605, 1614, 1641)
@@ -24,24 +12,13 @@ $uninstalled = $false
 
 if ($key.Count -eq 1) {
   $key | % { 
-    $packageArgs['file'] = "$($_.UninstallString)" #NOTE: You may need to split this if it contains spaces, see below
+    $packageArgs['file'] = "$($_.UninstallString)"
     
     if ($packageArgs['fileType'] -eq 'MSI') {
-      # The Product Code GUID is all that should be passed for MSI, and very 
-      # FIRST, because it comes directly after /x, which is already set in the 
-      # Uninstall-ChocolateyPackage msiargs (facepalm).
       $packageArgs['silentArgs'] = "$($_.PSChildName) $($packageArgs['silentArgs'])"
       
-      # Don't pass anything for file, it is ignored for msi (facepalm number 2) 
-      # Alternatively if you need to pass a path to an msi, determine that and 
-      # use it instead of the above in silentArgs, still very first
       $packageArgs['file'] = ''
     } else {
-      # NOTES:
-      # - You probably will need to sanitize $packageArgs['file'] as it comes from the registry and could be in a variety of fun but unusable formats
-      # - Split args from exe in $packageArgs['file'] and pass those args through $packageArgs['silentArgs'] or ignore them
-      # - Ensure you don't pass double quotes in $file (aka $packageArgs['file']) - otherwise you will get "Illegal characters in path when you attempt to run this"
-      # - Review the code for auto-uninstaller for all of the fun things it does in sanitizing - https://github.com/chocolatey/choco/blob/bfe351b7d10c798014efe4bfbb100b171db25099/src/chocolatey/infrastructure.app/services/AutomaticUninstallerService.cs#L142-L192
     }
 
     Uninstall-ChocolateyPackage @packageArgs
